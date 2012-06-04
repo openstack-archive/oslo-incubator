a6cae81c66c96ee6dceb26d1ddee79e728c5c4c1
Russell Bryant <rbryant@redhat.com>
2012-06-04 15:58:30 -0400

    Add gettext support.
    This patch adds gettext support for openstack-common modules.  In the
    process of pulling in the rpc code, I didn't want to remove the usage of
    _() that was already in there.  This allows it to stay in a way that
    doesn't conflict with gettext usage in the project using
    openstack-common.
    
    Change-Id: I9cad006f81244f0cce7b7f28659fa1daa5a4268a

---
+++ tests/unit/test_gettext.py
@@ -0,0 +1,30 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+import logging
+
+from openstack.common.gettextutils import gt as _
+
+
+LOG = logging.getLogger(__name__)
+
+
+class GettextTest(unittest.TestCase):
+
+    def test_gettext_does_not_blow_up(self):
+        LOG.info(_('test'))
