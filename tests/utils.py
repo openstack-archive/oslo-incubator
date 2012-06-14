fa73e8b172c756fa2b701011a1b7a358baf538e6
Andrew Bogott <abogott@wikimedia.org>
2012-06-13 22:47:26 -0500

    Added some testing tools; largely cribbed from glance.
    Change-Id: I90775bf99313e7dd16929059132ee058287b3585

---
+++ tests/utils.py
@@ -0,0 +1,65 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010-2011 OpenStack, LLC
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Common utilities used in testing"""
+
+import errno
+import functools
+import os
+import random
+import socket
+import stubout
+import subprocess
+import unittest
+
+import nose.plugins.skip
+
+from openstack.common import config
+from openstack.common import utils
+from openstack.common import cfg
+
+CONF = cfg.CONF
+
+
+class BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        super(BaseTestCase, self).setUp()
+        self.stubs = stubout.StubOutForTesting()
+
+    def tearDown(self):
+        super(BaseTestCase, self).tearDown()
+        CONF.reset()
+        self.stubs.UnsetAll()
+        self.stubs.SmartUnsetAll()
+
+    def config(self, **kw):
+        """
+        Override some configuration values.
+
+        The keyword arguments are the names of configuration options to
+        override and their values.
+
+        If a group argument is supplied, the overrides are applied to
+        the specified configuration option group.
+
+        All overrides are automatically cleared at the end of the current
+        test by the tearDown() method.
+        """
+        group = kw.pop('group', None)
+        for k, v in kw.iteritems():
+            CONF.set_override(k, v, group)
