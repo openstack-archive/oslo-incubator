8a967b16dc2ffd60bc19bee09f45e665ddcc9a81
Monty Taylor <mordred@inaugust.com>
2012-07-02 17:19:06 -0400

    Fix up openstack-common build.
    Ironically enough, even though the code to manage this is in
    openstack-common, openstack-common wasn't using it. This patch fixes doc
    generation, as well as setuptools-git integration and versioning.
    
    Change-Id: Ifb74ad539832d8cec4a536a96221eea8fb7527d2

---
+++ doc/source/index.rst
@@ -0,0 +1,17 @@
+OpenStack Common Code
+=====================
+
+Code shared for common tasks across OpenStack. Contents:
+
+.. toctree::
+   :maxdepth: 1
+
+   api/autoindex
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
-from setuptools import setup, find_packages
-from openstack.common.setup import parse_requirements
-from openstack.common.setup import parse_dependency_links
-from openstack.common.setup import write_requirements
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2012 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import setuptools
 
-version = '0.1'
+from openstack.common import setup
 
-requires = parse_requirements()
-depend_links = parse_dependency_links()
-write_requirements()
+requires = setup.parse_requirements()
+depend_links = setup.parse_dependency_links()
 
-setup(name='openstack.common',
-      version=version,
+setuptools.setup(name='openstack.common',
+      version=setup.get_post_version('openstack'),
       description="Common components for Openstack",
       long_description="Common components for Openstack "
                        "including paster templates.",
@@ -26,11 +38,14 @@ setup(name='openstack.common',
       author_email='openstack@lists.launchpad.net',
       url='http://www.openstack.org/',
       license='Apache Software License',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=setuptools.find_packages(exclude=['ez_setup',
+                                                 'examples', 'tests']),
       include_package_data=True,
+      cmdclass=setup.get_cmdclass(),
       zip_safe=True,
       install_requires=requires,
       dependency_links=depend_links,
+      setup_requires=['setuptools-git>=0.4'],
       entry_points="""
       # -*- Entry points: -*-
       """,
