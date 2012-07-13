openstack-archive/oslo-incubator

commit 372d11e990a544a645ce74ebfd56f19a0b422d28

Fix line length for pep8 compliance after cloning into other projects
There are a few lines that are close to the length limit so they pass
the PEP8 check under the common repository but fail the check after
the code is cloned into another project such as ceilometer. These
changes adjust the lines to give more space to allow the merge into
the other projects to go ahead without breaking the pep8 gating tests.

Change-Id: I33a05f5b51120114cc8ceffd0bf1b1ef462ed636
Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

openstack/common/rpc/impl_zmq.py
@@ -47,9 +47,12 @@ zmq_opts = [
                     'address.'),
 
     # The module.Class to use for matchmaking.
-    cfg.StrOpt('rpc_zmq_matchmaker',
-               default='openstack.common.rpc.matchmaker.MatchMakerLocalhost',
-               help='MatchMaker driver'),
+    cfg.StrOpt(
+        'rpc_zmq_matchmaker',
+        default=('openstack.common.rpc'
+                 'matchmaker.MatchMakerLocalhost'),
+        help='MatchMaker driver',
+        ),
 
     # The following port is unassigned by IANA as of 2012-05-21
     cfg.IntOpt('rpc_zmq_port', default=9501,

openstack/common/notifier/log_notifier.py
@@ -30,5 +30,6 @@ def notify(_context, message):
                            CONF.default_notification_level)
     priority = priority.lower()
     logger = logging.getLogger(
-            'openstack.common.notification.%s' % message['event_type'])
+            'openstack.common.notification.%s' %
+            message['event_type'])
     getattr(logger, priority)(jsonutils.dumps(message))

openstack/common/rpc/__init__.py
@@ -47,7 +47,9 @@ rpc_opts = [
                help='Seconds to wait before a cast expires (TTL). '
                     'Only supported by impl_zmq.'),
     cfg.ListOpt('allowed_rpc_exception_modules',
-                default=['openstack.common.exception', 'nova.exception'],
+                default=['openstack.common.exception',
+                         'nova.exception',
+                         ],
                 help='Modules of exceptions that are permitted to be recreated'
                      'upon receiving exception data from an rpc call.'),
     cfg.StrOpt('control_exchange',

commit b93bb12374d510b7bdb5371bada7667462084c8d

Fix ZeroMQ driver and tests
Fixes a number of bugs:

Adds test-options, taken from Nova.

Fixes breakage in flags introduced by 372d11e990...

zmq tests skipped on python 2.6 until we can pass reliably.

test_zmq listens only on 127.0.0.1

Configures RPC timeout in impl_zmq since no longer in fake_flags.

Restructures test_zmq's daemon launching/testing to be more robust.

Restores unit tests of zmq

Add pyzmq to test-options

Fix super RpcZmqBaseTestCase in test_zmq; bug 1021451

Remove opt registration from test_zmq; Related to bug 1021459

Fix zeromq options; Fixes bug 1021462 and bug 1021459.

Call our own register_opts
Brackets should be Parens in str formatting.
fixes bug 1021455.

Fix super RpcZmqBaseTestCase in test_zmq; Fixes bug 1021451

Change-Id: I6234c709346fadd6c9d9e736a51955c5efeb3ec6

openstack/common/rpc/impl_zmq.py
@@ -49,7 +49,7 @@ zmq_opts = [
     # The module.Class to use for matchmaking.
     cfg.StrOpt(
         'rpc_zmq_matchmaker',
-        default=('openstack.common.rpc'
+        default=('openstack.common.rpc.'
                  'matchmaker.MatchMakerLocalhost'),
         help='MatchMaker driver',
         ),
@@ -63,6 +63,11 @@ zmq_opts = [
 
     cfg.StrOpt('rpc_zmq_ipc_dir', default='/var/run/openstack',
                help='Directory for holding IPC sockets'),
+
+# NOTE(ewindisch): The host option is our identity/return-path for replies,
+#                  Nova defines this, but we won't have that in all
+#                  projects using RPC via openstack-common.
+#                  Should match Nova's CONF.host
     cfg.StrOpt('rpc_zmq_host', default=socket.gethostname(),
                help='Name of this node. Must be a valid hostname, FQDN, or '
                     'IP address')
@@ -719,3 +724,6 @@ def register_opts(conf):
         mm_impl = importutils.import_module(mm_module)
         mm_constructor = getattr(mm_impl, mm_class)
         matchmaker = mm_constructor()
+
+
+register_opts(cfg.CONF)

tox.ini
@@ -23,3 +23,9 @@ setenv = NOSE_WITH_COVERAGE=1
 
 [testenv:venv]
 commands = {posargs}
+
+[testenv:full]
+sitepackages = True
+deps = -r{toxinidir}/tools/pip-requires
+       -r{toxinidir}/tools/test-requires
+       -r{toxinidir}/tools/test-options

tests/unit/rpc/test_zmq.py
@@ -39,48 +39,83 @@ except ImportError:
     zmq = None
     impl_zmq = None
 
+
+pyzmq_ver = zmq.pyzmq_version().split('.') if zmq else (0, 0, 0)
+zmq_ver = zmq.zmq_version().split('.') if zmq else (0, 0, 0)
 LOG = logging.getLogger(__name__)
 FLAGS = cfg.CONF
 
 
 class _RpcZmqBaseTestCase(common.BaseRpcTestCase):
-#    @testutils.skip_if(zmq is None, "Test requires zmq")
-    @testutils.skip_if(True, "Zmq tests broken on jenkins")
+    # Version can be 2.1.11+, but 3.0 is not tested.
+    # pyzmq and libzmq versions don't need to match, but must be
+    # individually meet these minimum version requirements.
+    @testutils.skip_if(zmq is None, "Test requires pyzmq.")
+    @testutils.skip_if(pyzmq_ver < (2, 1, 11), "Test requires pyzmq>2.1.11")
+    @testutils.skip_if(zmq_ver < (2, 1, 11), "Test requires libzmq>2.1.11")
     def setUp(self, topic='test', topic_nested='nested'):
         if not impl_zmq:
             return None
 
         self.reactor = None
-        FLAGS.register_opts(rpc.rpc_opts)
         self.rpc = impl_zmq
-        self.rpc.register_opts(FLAGS)
-        FLAGS.set_default('rpc_zmq_matchmaker',
-                          'mod_matchmaker.MatchMakerLocalhost')
+
+        FLAGS.set_override('rpc_zmq_bind_address', '127.0.0.1')
+        FLAGS.set_override('rpc_zmq_host', '127.0.0.1')
+        FLAGS.set_override('rpc_response_timeout', 5)
+        FLAGS.set_override('rpc_zmq_matchmaker',
+                           'mod_matchmaker.MatchMakerLocalhost')
+
+        internal_ipc_dir = '/tmp/openstack-zmq.ipc.test'
 
         # We'll change this if we detect no daemon running.
         ipc_dir = FLAGS.rpc_zmq_ipc_dir
 
-        # Only launch the router if it isn't running independently.
+        # Only launch the router if it isn't running.
         if not os.path.exists(os.path.join(ipc_dir, "zmq_topic_zmq_replies")):
+
             LOG.info(_("Running internal zmq receiver."))
             # The normal ipc_dir default needs to run as root,
             # /tmp is easier within a testing environment.
-            FLAGS.set_default('rpc_zmq_ipc_dir', '/tmp/openstack-zmq.ipc.test')
+            FLAGS.set_override('rpc_zmq_ipc_dir',
+                               '/tmp/openstack-zmq.ipc.test')
 
             # Value has changed.
             ipc_dir = FLAGS.rpc_zmq_ipc_dir
+            self.setupReceiver(ipc_dir)
+        elif ipc_dir != internal_ipc_dir:
+            LOG.warning(_("Detected zmq-receiver socket."))
+            LOG.warning(_("Assuming nova-rpc-zmq-receiver is running."))
+            LOG.warning(_("Using system zmq receiver deamon."))
+
+        super(_RpcZmqBaseTestCase, self).setUp(
+            topic=topic, topic_nested=topic_nested)
+
+    def tearDown(self):
+        if not impl_zmq:
+            return None
+        if self.reactor:
+            try:
+                self.reactor.close()
+            finally:
+                try:
+                    utils.execute('rm', '-rf', FLAGS.rpc_zmq_ipc_dir)
+                except exception.Error:
+                    pass
+
+        super(_RpcZmqBaseTestCase, self).tearDown()
+
+    def setupReceiver(self, ipc_dir):
+        # Only launch the receiver if it isn't running independently.
+        # This is checked again, with the (possibly) new ipc_dir.
+        if not os.path.isdir(ipc_dir):
+            try:
+                os.mkdir(ipc_dir)
+            except OSError:
+                assert False, \
+                    _("Could not create IPC directory %s") % (ipc_dir, )
 
         try:
-            # Only launch the receiver if it isn't running independently.
-            # This is checked again, with the (possibly) new ipc_dir.
-            if os.path.exists(os.path.join(ipc_dir, "zmq_topic_zmq_replies")):
-                LOG.warning(_("Detected zmq-receiver socket. "
-                              "Assuming nova-rpc-zmq-receiver is running."))
-                return
-
-            if not os.path.isdir(ipc_dir):
-                os.mkdir(ipc_dir)
-
             self.reactor = impl_zmq.ZmqProxy(FLAGS)
             consume_in = "tcp://%s:%s" % \
                 (FLAGS.rpc_zmq_bind_address,
@@ -95,24 +130,6 @@ class _RpcZmqBaseTestCase(common.BaseRpcTestCase):
         except zmq.ZMQError:
             assert False, _("Could not create ZeroMQ receiver daemon. "
                             "Socket may already be in use.")
-        except OSError:
-            assert False, _("Could not create IPC directory %s") % (ipc_dir, )
-        finally:
-            super(_RpcZmqBaseTestCase, self).setUp(
-                topic=topic, topic_nested=topic_nested)
-
-    def tearDown(self):
-        if not impl_zmq:
-            return None
-        if self.reactor:
-            self.reactor.close()
-
-            try:
-                utils.execute('rm', '-rf', FLAGS.rpc_zmq_ipc_dir)
-            except exception.Error:
-                pass
-
-        super(_RpcZmqBaseTestCase, self).tearDown()
 
 
 class RpcZmqBaseTopicTestCase(_RpcZmqBaseTestCase):

tools/test-options
@@ -1,0 +1,1 @@
+pyzmq>=2.1.11