openstack-archive/oslo-incubator

commit 83044a72815ba2a7cec1e6a343c6eeb091c68a30

Adds support for bol and eol spaces to ini files
  * Fixes bug 1009639
  * Adds tests

Change-Id: Id00563dfcc6f143c3e86ec380d66cffc967b8c48

openstack/common/iniparser.py
@@ -52,7 +52,10 @@ class BaseParser(object):
         else:
             key, value = line[:colon], line[colon + 1:]
 
-        return key.strip(), [value.strip()]
+        value = value.strip()
+        if value[0] == value[-1] and value[0] == "\"" or value[0] == "'":
+            value = value[1:-1]
+        return key.strip(), [value]
 
     def parse(self, lineiter):
         key = None

tests/unit/test_iniparser.py
@@ -104,6 +104,15 @@ class ParserTestCase(unittest.TestCase):
         self.parser.parse(lines)
         self.assertTrue(self.parser.comment_called)
 
+    def test_assignment_space_single_quote(self):
+        lines = ["foo = ' bar '"]
+        self.parser.parse(lines)
+        self.assertEquals(self.parser.values, {'': {'foo': [' bar ']}})
+
+    def test_assignment_space_double_quote(self):
+        lines = ["foo = \" bar \""]
+        self.parser.parse(lines)
+        self.assertEquals(self.parser.values, {'': {'foo': [' bar ']}})
 
 class ExceptionTestCase(unittest.TestCase):
     def test_parseerror(self):

commit ace759e55d7c87dc66a500d598d1355dd3c0408f

cfg: allow empty config values
Fixes bug #1025522

Commit 83044a7 caused this to stop working in Quantum:

  api_extensions_path =

and could only be worked around with:

  api_extensions_path = ""

Change-Id: I8c1a57225a2c135e6baf567b8e71d61e974da4e2

openstack/common/iniparser.py
@@ -53,7 +53,8 @@ class BaseParser(object):
             key, value = line[:colon], line[colon + 1:]
 
         value = value.strip()
-        if value[0] == value[-1] and value[0] == "\"" or value[0] == "'":
+        if ((value and value[0] == value[-1]) and
+            (value[0] == "\"" or value[0] == "'")):
             value = value[1:-1]
         return key.strip(), [value]
 

tests/unit/test_iniparser.py
@@ -104,6 +104,11 @@ class ParserTestCase(unittest.TestCase):
         self.parser.parse(lines)
         self.assertTrue(self.parser.comment_called)
 
+    def test_empty_assignment(self):
+        lines = ["foo = "]
+        self.parser.parse(lines)
+        self.assertEquals(self.parser.values, {'': {'foo': ['']}})
+
     def test_assignment_space_single_quote(self):
         lines = ["foo = ' bar '"]
         self.parser.parse(lines)