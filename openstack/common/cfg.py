672f75b73c7db081660ac86b62d01ef49c2b8d1d
Giampaolo Lauria <lauria@us.ibm.com>
2012-07-24 10:59:45 -0400

    Patch set 2
    Change-Id: I3fc0609bbd3936ce09565052458fd15c633b662b

--- openstack/common/cfg.py
+++ openstack/common/cfg.py
@@ -464,10 +464,9 @@ def _is_opt_registered(opts, opt):
     :raises: DuplicateOptError if a naming conflict is detected
     """
     if opt.dest in opts:
-        if opts[opt.dest]['opt'] == opt:
-            return True
-        else:
+        if opts[opt.dest]['opt'] != opt:
             raise DuplicateOptError(opt.name)
+        return True
     else:
         return False
 
@@ -528,8 +527,8 @@ class Opt(object):
         else:
             self.deprecated_name = None
 
-    def __eq__(self, another):
-        return self.__dict__ == another.__dict__
+    def __ne__(self, another):
+        return vars(self) != vars(another)
 
     def _get_from_config_parser(self, cparser, section):
         """Retrieves the option value from a MultiConfigParser object.
