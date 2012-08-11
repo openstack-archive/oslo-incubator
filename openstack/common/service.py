12cad4e00d9376156950107506749d07a0c4777c
Angus Salkeld <asalkeld@redhat.com>
2012-08-11 22:20:29 +1000

    Basic service launching infrastructure
    Part of blueprint service-infrastructure
    
    Change-Id: I4e97db2877be976b0c681da9ff0d331fbfb306b0
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>

--- openstack/common/service.py
+++ openstack/common/service.py
@@ -19,6 +19,64 @@
 
 """Generic Node base class for all workers that run on hosts."""
 
+import sys
+import eventlet
+import greenlet
+
+
+class Launcher(object):
+    """Launch one or more services and wait for them to complete."""
+
+    def __init__(self):
+        """Initialize the service launcher.
+
+        :returns: None
+
+        """
+        self._services = []
+
+    @staticmethod
+    def run_server(server):
+        """Start and wait for a server to finish.
+
+        :param service: Server to run and wait for.
+        :returns: None
+
+        """
+        server.start()
+        server.wait()
+
+    def launch_server(self, server):
+        """Load and start the given server.
+
+        :param server: The server you would like to start.
+        :returns: None
+
+        """
+        gt = eventlet.spawn(self.run_server, server)
+        self._services.append(gt)
+
+    def stop(self):
+        """Stop all services which are currently running.
+
+        :returns: None
+
+        """
+        for service in self._services:
+            service.kill()
+
+    def wait(self):
+        """Waits until all services have been stopped, and then returns.
+
+        :returns: None
+
+        """
+        for service in self._services:
+            try:
+                service.wait()
+            except greenlet.GreenletExit:
+                pass
+
 
 class Service(object):
     """Service object for binaries running on hosts.
@@ -28,3 +86,32 @@ class Service(object):
     def __init__(self, host, manager, *args, **kwargs):
         self.host = host
         self.manager = manager
+
+    def start(self):
+        if self.manager:
+            self.manager.init_host()
+
+    def stop(self):
+        pass
+
+    def wait(self):
+        pass
+
+
+# NOTE(vish): the global launcher is to maintain the existing
+#             functionality of calling service.serve +
+#             service.wait
+_launcher = None
+
+
+def serve(server, workers=None):
+    global _launcher
+    if _launcher:
+        raise RuntimeError(_('serve() can only be called once'))
+
+    _launcher = Launcher()
+    _launcher.launch_server(server)
+
+
+def wait():
+    _launcher.wait()
