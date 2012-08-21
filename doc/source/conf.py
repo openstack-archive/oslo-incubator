71ff22ee842812ed693d200f55fd6aa8103bb598
Ray Chen <oldsharp@163.com>
2012-08-21 17:48:39 +0800

    Code clean up
    I fixed some PEP8 issues in nova/doc/ext/nova_todo.py and
    nova/doc/source/conf.py in the past few days, intending to be
    familiar with the bugfix workflow. But when I turned to the same
    code files in other projects such as Swift, Keystone,... etc., I
    found that they have the same problems, some even have issues
    that beyond the scope of PEP8. So I decide to do a little code
    clean up works on these files.
    In this file, I tried to do things as follow:
    1. PEP8 issues
    2. Remove unused import
    3. Add a LISENCE info at the beginning of the file.
    4. Move the 'project' variable to the appropriate position.
    
    Change-Id: Ifc2f8fdd2d3887e68b4d537d30d17b8c61cfba20

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2010-2012 OpenStack, LLC.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import sys, os
+# -- General configuration ----------------------------------------------------
 
-project = 'openstack-common'
-
-# -- General configuration -----------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+# Add any Sphinx extension module names here, as strings.
+# They can be extensions coming with Sphinx (named 'sphinx.ext.*')
+# or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
@@ -25,6 +36,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
+project = 'openstack-common'
 copyright = u'OpenStack LLC'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
@@ -37,7 +49,7 @@ add_module_names = True
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# -- Options for HTML output ---------------------------------------------------
+# -- Options for HTML output --------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
@@ -48,12 +60,13 @@ htmlhelp_basename = '%sdoc' % project
 
 
 # Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
+# (source start file, target name, title, author, documentclass
+# [howto/manual]).
 latex_documents = [
-  ('index',
-    '%s.tex' % project,
-    u'%s Documentation' % project,
-    u'OpenStack LLC', 'manual'),
+        ('index',
+         '%s.tex' % project,
+         u'%s Documentation' % project,
+         u'OpenStack LLC', 'manual'),
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
