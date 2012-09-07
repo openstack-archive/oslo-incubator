879470b99c0f8f42d50f955defaf43ab47b704cc
Eugene Kirpichov <ekirpichov@gmail.com>
2012-09-07 22:56:34 +0000

    Added a method for parsing host:port pairs.
    The method works correctly with ipv6.
    An alternative way would be to use urlparse, but urlparse
    only works correctly starting with Python 2.7, so to be
    compatible, we have to reimplement this part.
    
    The method will be used for parsing Glance API server
    addresses, RabbitMQ addresses and perhaps other things.
    
    Change-Id: Ie5014891c6abcd80681a370d5dd94cb0406f7f61

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -80,3 +80,20 @@ class UtilsTest(unittest.TestCase):
     #                running code.
     def test_execute_unknown_kwargs(self):
         self.assertRaises(exception.Error, utils.execute, hozer=True)
+
+    def test_parse_host_port(self):
+        self.assertEqual(('server01', 80),
+            utils.parse_host_port('server01:80'))
+        self.assertEqual(('server01', None),
+            utils.parse_host_port('server01'))
+        self.assertEqual(('server01', 1234),
+            utils.parse_host_port('server01', default_port=1234))
+        self.assertEqual(('::1', 80),
+            utils.parse_host_port('[::1]:80'))
+        self.assertEqual(('::1', None),
+            utils.parse_host_port('[::1]'))
+        self.assertEqual(('::1', 1234),
+            utils.parse_host_port('[::1]', default_port=1234))
+        self.assertEqual(('2001:db8:85a3::8a2e:370:7334', 1234),
+            utils.parse_host_port('2001:db8:85a3::8a2e:370:7334',
+              default_port=1234))
