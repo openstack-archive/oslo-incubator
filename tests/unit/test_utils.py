d4161b95885d43a1426be5e5f63c3141470abf3e
Zhongyue Luo <zhongyue.nah@intel.com>
2012-09-11 14:03:47 +0800

    Use pep8 v1.3.3
    Fixed indents which violate pep8 1.3.3
    Also added .mailmap file to coalesce commits
    
    Change-Id: Ia7a788a28e6c0fd9cb52fb7ea477cddd7d589e58

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -83,17 +83,17 @@ class UtilsTest(unittest.TestCase):
 
     def test_parse_host_port(self):
         self.assertEqual(('server01', 80),
-            utils.parse_host_port('server01:80'))
+                         utils.parse_host_port('server01:80'))
         self.assertEqual(('server01', None),
-            utils.parse_host_port('server01'))
+                         utils.parse_host_port('server01'))
         self.assertEqual(('server01', 1234),
-            utils.parse_host_port('server01', default_port=1234))
+                         utils.parse_host_port('server01', default_port=1234))
         self.assertEqual(('::1', 80),
-            utils.parse_host_port('[::1]:80'))
+                         utils.parse_host_port('[::1]:80'))
         self.assertEqual(('::1', None),
-            utils.parse_host_port('[::1]'))
+                         utils.parse_host_port('[::1]'))
         self.assertEqual(('::1', 1234),
-            utils.parse_host_port('[::1]', default_port=1234))
+                         utils.parse_host_port('[::1]', default_port=1234))
         self.assertEqual(('2001:db8:85a3::8a2e:370:7334', 1234),
-            utils.parse_host_port('2001:db8:85a3::8a2e:370:7334',
-              default_port=1234))
+                         utils.parse_host_port('2001:db8:85a3::8a2e:370:7334',
+                                               default_port=1234))
