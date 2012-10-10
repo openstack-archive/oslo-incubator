9a8c1d7e1a537e6580be2be1630baefc910de1de
Michael Still <mikal@stillhq.com>
2012-10-10 17:28:11 +1100

    Move nova's util.synchronized decorator to openstack common.
    In the end I needed to port utils.ensure_tree as well. Resolves
    bug 1063230.
    
    Change-Id: I6e6fa8201de2cac3f17e6c60d7b16f7df7c64116

---
+++ tests/unit/test_fileutils.py
@@ -0,0 +1,37 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2011 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+
+import os
+import shutil
+import tempfile
+
+from openstack.common import fileutils
+
+
+class EnsureTree(unittest.TestCase):
+    def test_ensure_tree(self):
+        tmpdir = tempfile.mkdtemp()
+        try:
+            testdir = '%s/foo/bar/baz' % (tmpdir,)
+            fileutils.ensure_tree(testdir)
+            self.assertTrue(os.path.isdir(testdir))
+
+        finally:
+            if os.path.exists(tmpdir):
+                shutil.rmtree(tmpdir)
