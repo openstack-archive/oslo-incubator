5525acd080911492d76a237ee385fc95e5ffc751
Michael Still <mikal@stillhq.com>
2012-10-10 17:28:11 +1100

    Move nova's util.synchronized decorator to openstack common.
    Resolves bug 1063230.
    
    Change-Id: I6e6fa8201de2cac3f17e6c60d7b16f7df7c64116

---
+++ tests/unit/test_lockutils.py
@@ -0,0 +1,60 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#    Copyright 2011 Justin Santa Barbara
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import shutil
+import tempfile
+import unittest
+
+import eventlet
+from eventlet import greenpool
+
+from openstack.common import lockutils
+
+
+class TestFileLocks(unittest.TestCase):
+    def test_concurrent_green_lock_succeeds(self):
+        """Verify spawn_n greenthreads with two locks run concurrently."""
+        tmpdir = tempfile.mkdtemp()
+
+        try:
+            self.completed = False
+
+            def locka(wait):
+                a = lockutils.InterProcessLock(os.path.join(tmpdir, 'a'))
+                with a:
+                    wait.wait()
+                self.completed = True
+
+            def lockb(wait):
+                b = lockutils.InterProcessLock(os.path.join(tmpdir, 'b'))
+                with b:
+                    wait.wait()
+
+            wait1 = eventlet.event.Event()
+            wait2 = eventlet.event.Event()
+            pool = greenpool.GreenPool()
+            pool.spawn_n(locka, wait1)
+            pool.spawn_n(lockb, wait2)
+            wait2.send()
+            eventlet.sleep(0)
+            wait1.send()
+            pool.waitall()
+
+            self.assertTrue(self.completed)
+
+        finally:
+            shutil.rmtree(tmpdir)
