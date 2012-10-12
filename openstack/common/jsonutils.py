ce796e9b2a734f1e047f5737c8a92aefbf94f859
long-wang <long.wang@bj.cs2c.com.cn>
2012-10-12 22:09:18 +0800

    add function to determine whether an string is json format.
    Change-Id: Ib3706813477a9211790e9a59987edde712f7a2db

--- openstack/common/jsonutils.py
+++ openstack/common/jsonutils.py
@@ -137,6 +137,16 @@ def loads(s):
 def load(s):
     return json.load(s)
 
+def is_json(s):
+    try:
+        #FIXME(wanglong): optimize this call in good method
+        #is there good method to determine that a string is 
+        #json format.
+        json.loads(s)
+        return True
+    except:
+        return False
+    
 
 try:
     import anyjson
--- tests/unit/test_jsonutils.py
+++ tests/unit/test_jsonutils.py
@@ -34,6 +34,10 @@ class JSONUtilsTestCase(unittest.TestCase):
     def test_load(self):
         x = StringIO.StringIO('{"a": "b"}')
         self.assertEqual(jsonutils.load(x), {'a': 'b'})
+        
+    def test_is_json(self):
+        self.assertTrue(jsonutils.is_json('{"a": "b"}'))
+        self.assertFalse(jsonutils.is_json('not a json'))
 
 
 class ToPrimitiveTestCase(unittest.TestCase):
