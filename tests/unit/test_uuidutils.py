d2b559250694c5e217f288c4fbed394393de7167
Zhongyue Luo <zhongyue.nah@intel.com>
2012-10-23 14:35:19 +0800

    Creates uuidutils module
    Fixes bug #1070198
    
    Implements UUID processing functions used across projects.
    
    Change-Id: Ife2c972cea727dc7e453008bcd5871607e883d5c

---
+++ tests/unit/test_uuidutils.py
@@ -0,0 +1,33 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2012 Intel Corporation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+import uuid
+
+from openstack.common import uuidutils
+
+
+class UUIDUtilsTest(unittest.TestCase):
+
+    def test_is_uuid_like(self):
+        self.assertTrue(uuidutils.is_uuid_like(str(uuid.uuid4())))
+
+    def test_is_uuid_like_id(self):
+        self.assertFalse(uuidutils.is_uuid_like(1234567))
+
+    def test_is_uuid_like_name(self):
+        self.assertFalse(uuidutils.is_uuid_like('zhongyueluo'))
