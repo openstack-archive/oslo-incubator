256a467d3ce5b626f50e24a7d9a053b133390586
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-10-26 11:22:24 -0400

    Add setup.py for registering namespace package
    Create a dummy package and setup.py file solely to be used
    to register the name "oslo" on PyPI.
    
    Change-Id: Ica9dd1cf05fda36c317223ead22a94f337f3ae21
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

---
+++ pypi/oslo/__init__.py
@@ -0,0 +1,3 @@
+"""Declare namespace package"""
+
+__import__('pkg_resources').declare_namespace(__name__)
---
+++ pypi/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2012 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+
+setuptools.setup(
+    name='oslo',
+    version='1',
+    description="Namespace for common components for Openstack",
+    long_description="Namespace for common components for Openstack",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: No Input/Output (Daemon)',
+        'Environment :: OpenStack',
+    ],
+    keywords='openstack',
+    author='OpenStack',
+    author_email='openstack@lists.launchpad.net',
+    url='http://www.openstack.org/',
+    license='Apache Software License',
+    zip_safe=True,
+    packages=setuptools.find_packages(exclude=['ez_setup',
+                                               'examples', 'tests']),
+    namespace_packages=['oslo'],
+)
