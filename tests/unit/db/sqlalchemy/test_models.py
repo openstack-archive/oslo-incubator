6b1797ed6d8a83d37790179ce388ccd3decd362b
Eric Windisch <eric@cloudscaling.com>
2012-12-04 10:40:10 -0500

    Import sqlalchemy session/models/utils
    Bring in session, base model, utilities and tests for sqlalchemy.
    
    Adds sqlalchemy to pip-requires and python-mysql to testing-requires.
    
    Partially implements blueprint common-db
    
    Change-Id: I3e0065cdac87e10c4e0742d66c293c72bb3acbb2

---
+++ tests/unit/db/sqlalchemy/test_models.py
@@ -0,0 +1,72 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 Cloudscaling Group, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import openstack.common.db.sqlalchemy.models as models
+from tests import utils as test_utils
+
+
+class ModelBaseTest(test_utils.BaseTestCase):
+
+    def test_modelbase_has_dict_methods(self):
+        dict_methods = ('__getitem__',
+                        '__setitem__',
+                        '__iter__',
+                        'get',
+                        'next',
+                        'update',
+                        'iteritems')
+        for method in dict_methods:
+            self.assertTrue(hasattr(models.ModelBase, method))
+
+    def test_modelbase_set(self):
+        mb = models.ModelBase()
+        mb['world'] = 'hello'
+        self.assertEqual(mb['world'], 'hello')
+
+    def test_modelbase_update(self):
+        mb = models.ModelBase()
+        h = {'a': '1', 'b': '2'}
+        mb.update(h)
+        for key in h.keys():
+            self.assertEqual(mb[key], h[key])
+
+    def test_modelbase_iteritems(self):
+        self.skipTest("Requires DB")
+        mb = models.ModelBase()
+        h = {'a': '1', 'b': '2'}
+        mb.update(h)
+        for key, value in mb.iteritems():
+            self.assertEqual(h[key], value)
+
+    def test_modelbase_iter(self):
+        self.skipTest("Requires DB")
+        mb = models.ModelBase()
+        h = {'a': '1', 'b': '2'}
+        mb.update(h)
+        i = iter(mb)
+
+        min_items = len(h)
+        found_items = 0
+        while True:
+            r = next(i, None)
+            if r is None:
+                break
+
+            self.assertTrue(r in h)
+            found_items += 1
+
+        self.assertEqual(min_items, found_items)
