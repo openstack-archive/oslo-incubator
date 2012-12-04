26b7e9ff041e41d933f1a18b8ee8c1d6a204e855
Eric Windisch <eric@cloudscaling.com>
2012-12-04 10:40:10 -0500

    Import sqlalchemy models/utils
    Bring in utilities and base model
    for SQLAlchemy code.
    
    partially implements blueprint common-db
    
    Change-Id: I3e0065cdac87e10c4e0742d66c293c72bb3acbb2

---
+++ openstack/common/db/common.py
@@ -0,0 +1,30 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2012 Cloudscaling Group, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack.common.gettextutils import _
+
+
+# Invalid imported from nova.exception
+class Invalid(Exception):
+        message = _("Unacceptable parameters.")
+        code = 400
+
+
+class InvalidSortKey(Invalid):
+        message = _("Sort key supplied was not valid.")
