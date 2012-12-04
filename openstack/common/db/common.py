caebff1c473cfa0103e9f556dd58f9caad528a4c
Eric Windisch <eric@cloudscaling.com>
2012-12-04 10:40:10 -0500

    Import sqlalchemy session/models/utils
    Bring in session, utilities, and base model
    for SQLAlchemy code. Includes tests.
    
    Brings sqlalchemy and python-mysqldb into
    pip-requires and testing-requires.
    
    Partially implements blueprint common-db
    
    Change-Id: I3e0065cdac87e10c4e0742d66c293c72bb3acbb2

---
+++ openstack/common/db/common.py
@@ -0,0 +1,54 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2012 Cloudscaling Group, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import openstack.common.exception as exception
+from openstack.common.gettextutils import _
+from openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+class InvalidSortKey(exception.Invalid):
+    message = _("Sort key supplied was not valid.")
+
+
+class InvalidUnicodeParameter(exception.Invalid):
+    message = _("Invalid Parameter: "
+                "Unicode is not supported by the current database.")
+
+
+class DBError(exception.OpenstackException):
+    """Wraps an implementation specific exception."""
+    def __init__(self, inner_exception=None):
+        self.inner_exception = inner_exception
+        super(DBError, self).__init__(str(inner_exception))
+
+
+def wrap_db_error(f):
+    def _wrap(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except UnicodeEncodeError:
+            raise InvalidUnicodeParameter()
+        except Exception, e:
+            LOG.exception(_('DB exception wrapped.'))
+            raise DBError(e)
+    _wrap.func_name = f.func_name
+    return _wrap
