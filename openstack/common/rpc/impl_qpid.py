openstack-archive/oslo-incubator

commit 86952852c1dadd54f1294ae0adba01cfe827b8ce

Qpid H/A cluster support
Implements blueprint qpid-roundrobin-reconnect

Change-Id: I01b65e6c33a238663667e9b6023b90172f1b77dd

openstack/common/rpc/impl_qpid.py
@@ -41,6 +41,9 @@ qpid_opts = [
     cfg.StrOpt('qpid_port',
                default='5672',
                help='Qpid broker port'),
+    cfg.ListOpt('qpid_hosts',
+                default=['$qpid_hostname:$qpid_port'],
+                help='Qpid HA cluster host:port pairs'),
     cfg.StrOpt('qpid_username',
                default='',
                help='Username for qpid connection'),
@@ -277,22 +280,21 @@ class Connection(object):
         self.conf = conf
 
         params = {
-            'hostname': self.conf.qpid_hostname,
-            'port': self.conf.qpid_port,
+            'qpid_hosts': self.conf.qpid_hosts,
             'username': self.conf.qpid_username,
             'password': self.conf.qpid_password,
         }
         params.update(server_params or {})
 
-        self.broker = params['hostname'] + ":" + str(params['port'])
+        self.brokers = params['qpid_hosts']
         self.username = params['username']
         self.password = params['password']
-        self.connection_create()
+        self.connection_create(self.brokers[0])
         self.reconnect()
 
-    def connection_create(self):
+    def connection_create(self, broker):
         # Create the connection - this does not open the connection
-        self.connection = qpid.messaging.Connection(self.broker)
+        self.connection = qpid.messaging.Connection(broker)
 
         # Check if flags are set and if so set them for the connection
         # before we call open
@@ -320,10 +322,14 @@ class Connection(object):
             except qpid.messaging.exceptions.ConnectionError:
                 pass
 
+        attempt = 0
         delay = 1
         while True:
+            broker = self.brokers[attempt % len(self.brokers)]
+            attempt += 1
+
             try:
-                self.connection_create()
+                self.connection_create(broker)
                 self.connection.open()
             except qpid.messaging.exceptions.ConnectionError, e:
                 msg_dict = dict(e=e, delay=delay)
@@ -333,9 +339,8 @@ class Connection(object):
                 time.sleep(delay)
                 delay = min(2 * delay, 60)
             else:
+                LOG.info(_('Connected to AMQP server on %s'), broker)
                 break
-
-        LOG.info(_('Connected to AMQP server on %s'), self.broker)
 
         self.session = self.connection.session()
 

commit 82ccc2f371cfe7dd4774dfb09837a38748df3f8c

Fix qpid cast_to_server regression.
This patches addresses a regression in the qpid driver for the
cast_to_server and fanout_cast_to_server methods.  The regression was
introduced in 86952852c1dadd54f1294ae0adba01cfe827b8ce.

There are test cases that cover this code, but unfortunately they are
not being run by jenkins because python-qpid is not available in pypi.

Change-Id: I89d4f81149f9a05c578b5ec544c5e34311484c6c

openstack/common/rpc/impl_qpid.py
@@ -279,6 +279,13 @@ class Connection(object):
         self.consumer_thread = None
         self.conf = conf
 
+        if server_params and 'hostname' in server_params:
+            # NOTE(russellb) This enables support for cast_to_server.
+            server_params['qpid_hosts'] = [
+                '%s:%d' % (server_params['hostname'],
+                           server_params.get('port', 5672))
+            ]
+
         params = {
             'qpid_hosts': self.conf.qpid_hosts,
             'username': self.conf.qpid_username,

tests/unit/rpc/test_qpid.py
@@ -289,9 +289,9 @@ class RpcQpidTestCase(unittest.TestCase):
                                  server_params['username'])
                 self.assertEqual(myself.connection.password,
                                  server_params['password'])
-                self.assertEqual(myself.broker,
-                                 server_params['hostname'] + ':' +
-                                 str(server_params['port']))
+                self.assertEqual(myself.brokers,
+                                 [server_params['hostname'] + ':' +
+                                 str(server_params['port'])])
 
         MyConnection.pool = rpc_amqp.Pool(FLAGS, MyConnection)
         self.stubs.Set(impl_qpid, 'Connection', MyConnection)