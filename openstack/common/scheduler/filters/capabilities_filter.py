35df92abbe2ffb77783e368003648eb15e61988a
Zhiteng Huang <zhiteng.huang@intel.com>
2012-12-06 14:21:50 +0800

    Add common filter/filter handler for filter scheduler
    Filter scheduler is being used for more than one core projects (Nova
    and Cinder as of writing), the implementation shared a lot of common
    code.  This patch moves base filter/filter handler class as well as
    common filter implementation for filter scheduler into oslo to reduce
    possible porting.
    
    implement bp: common-filters
    
    Change-Id: If0b1dee79c410c98e152230b55c1ec5dbcdef27c

---
+++ openstack/common/scheduler/filters/capabilities_filter.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2011 OpenStack, LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack.common import log as logging
+from openstack.common.scheduler import filters
+from openstack.common.scheduler.filters import extra_specs_ops
+
+
+LOG = logging.getLogger(__name__)
+
+
+class CapabilitiesFilter(filters.BaseHostFilter):
+    """HostFilter to work with resource (instance & volume) type records."""
+
+    def _satisfies_extra_specs(self, capabilities, resource_type):
+        """Check that the capabilities provided by the services
+        satisfy the extra specs associated with the instance type"""
+        extra_specs = resource_type.get('extra_specs', [])
+        if not extra_specs:
+            return True
+
+        for key, req in extra_specs.iteritems():
+            # Either not scope format, or in capabilities scope
+            scope = key.split(':')
+            if scope[0] == "capabilities":
+                del scope[0]
+            elif len(scope) > 1:
+                continue
+            cap = capabilities
+            for capability in scope:
+                cap = cap.get(capability, None)
+                if cap is None:
+                    return False
+            if not extra_specs_ops.match(cap, req):
+                return False
+        return True
+
+    def host_passes(self, host_state, filter_properties):
+        """Return a list of hosts that can create instance_type."""
+        resource_type = filter_properties.get('resource_type')
+        if not self._satisfies_extra_specs(host_state.capabilities,
+                                           resource_type):
+            LOG.debug("%(host_state)s fails resource_type extra_specs "
+                      "requirements", locals())
+            return False
+        return True
