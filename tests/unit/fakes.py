5b8a168cb942925a09cbee043adc7d62cd8d9857
Zhiteng Huang <zhiteng.huang@intel.com>
2012-12-10 15:33:30 +0800

    Add common base weigher/weigher handler for filter scheduler
    Filter scheduler is being used for more than one core projects (Nova
    and Cinder as of writing), the implementation shared a lot of common
    code. This patch is to move base weigher (weighing function), weigher
    handler for filter scheduler into oslo to reduce possible porting.
    
    implement bp: common-weights
    
    Change-Id: I2d1b37438663b53e035cc262875a283e5e2ee970

---
+++ tests/unit/fakes.py
@@ -0,0 +1,35 @@
+# Copyright 2012 Intel Inc, OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+Fakes For filter and weight tests.
+"""
+
+
+from openstack.common.scheduler import weights
+
+
+class FakeWeigher1(weights.BaseHostWeigher):
+    def __init__(self):
+        pass
+
+
+class FakeWeigher2(weights.BaseHostWeigher):
+    def __init__(self):
+        pass
+
+
+class FakeClass(object):
+    def __init__(self):
+        pass
