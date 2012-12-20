f257182e1607a48b770083f3c3593134eb399fec
He Jie Xu <xuhj@linux.vnet.ibm.com>
2012-12-20 21:33:44 +0800

    Add custom gettext install for fixes translate domain overwrite
    Fixes bug 974810
    
    Add install function in openstack common utils. That function support
    install gettext as fallback, then when one project reference another
    project won't overwrite the translation domain.
    
    All the openstack project should use this 'install' function in root
    __init__.py file instead of use 'gettext.install'
    
    Change-Id: Ie01455bda37945bb6ebfff6e2fd9b0ca6d056c50

--- tests/unit/test_gettext.py
+++ tests/unit/test_gettext.py
@@ -15,10 +15,14 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import unittest
+import __builtin__
+import gettext
 import logging
+import unittest2 as unitest
 
-from openstack.common.gettextutils import _
+import mock
+
+from openstack.common import gettextutils
 
 
 LOG = logging.getLogger(__name__)
@@ -26,5 +30,34 @@ LOG = logging.getLogger(__name__)
 
 class GettextTest(unittest.TestCase):
 
+    def setUp(self):
+        self._orig_gettext = None
+        if '_' in __builtin__.__dict__:
+            self._orig_gettext = __builtin__.__dict__['_']
+            del __builtin__.__dict__['_']
+
+    def tearDown(self):
+        if self._orig_gettext:
+            __builtin__.__dict__['_'] = self._orig_gettext
+
     def test_gettext_does_not_blow_up(self):
+        from openstack.common.gettextutils import _
         LOG.info(_('test'))
+
+    def test_install_gettext(self):
+        gettextutils.install('test_install_gettext')
+        self.assertIsInstance(__builtin__.__dict__['_'].__self__,
+                              gettext.NullTranslations)
+
+    def test_install_gettext_with_existed_translation(self):
+        gettextutils.install('test_install_gettext')
+        trans = __builtin__.__dict__['_'].__self__
+        gettextutils.install('test_install_gettext')
+        self.assertIs(__builtin__.__dict__['_'].__self__, trans)
+        self.assertIsInstance(__builtin__.__dict__['_'].__self__._fallback,
+                              gettext.NullTranslations)
+
+    def test_install_gettext_with_existed_non_translation(self):
+        __builtin__.__dict__['_'] = gettext.gettext
+        gettextutils.install('test_install_gettext')
+        self.assertEqual(__builtin__.__dict__['_'], gettext.gettext)
