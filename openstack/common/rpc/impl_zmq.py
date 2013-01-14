5aa2f870a6ba3ce6df6d5e7d3abedd69294c7404
Mark McLoughlin <markmc@redhat.com>
2013-01-14 08:37:11 +0000

    Allow rpc drivers to always be imported
    The qpid and zmq drivers current can't be import unless their
    respective libraries are installed and neither are currently listed
    in test-requires.
    
    This prevents us from being able to use nosetests --with-doctests
    because the import failure is seen as a test failure.
    
    Fix this issue by making the import always succeed and deferring
    failure until the module's APIs are actually used.
    
    This will also help Nova's sample config file generation since it
    will allow the generate-sample tool discover config options for these
    drivers even when the messaging libraries aren't installed.
    
    Change-Id: I30087fd33eef41d50d1a374cb5694dc15c43d1da

--- openstack/common/rpc/impl_zmq.py
+++ openstack/common/rpc/impl_zmq.py
@@ -22,7 +22,7 @@ import types
 import uuid
 
 import eventlet
-from eventlet.green import zmq
+import extras
 import greenlet
 
 from openstack.common import cfg
@@ -31,6 +31,7 @@ from openstack.common import importutils
 from openstack.common import jsonutils
 from openstack.common.rpc import common as rpc_common
 
+zmq = extras.try_import('eventlet.green.zmq')
 
 # for convenience, are not modified.
 pformat = pprint.pformat
@@ -206,7 +207,9 @@ class ZmqSocket(object):
 class ZmqClient(object):
     """Client for ZMQ sockets."""
 
-    def __init__(self, addr, socket_type=zmq.PUSH, bind=False):
+    def __init__(self, addr, socket_type=None, bind=False):
+        if socket_type is None:
+            socket_type = zmq.PUSH
         self.outq = ZmqSocket(addr, socket_type, bind=bind)
 
     def cast(self, msg_id, topic, data, serialize=True, force_envelope=False):
@@ -726,6 +729,9 @@ def cleanup():
 
 
 def _get_ctxt():
+    if not zmq:
+        raise ImportError("Failed to import eventlet.green.zmq")
+
     global ZMQ_CTX
     if not ZMQ_CTX:
         ZMQ_CTX = zmq.Context(CONF.rpc_zmq_contexts)
