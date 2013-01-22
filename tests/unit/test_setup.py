e05519bf6c2810c9eda86dedff76cfa30a066d55
Alessio Ababilov <aababilo@yahoo-inc.com>
2013-01-22 15:43:29 +0200

    Add tests for setup
    Test write_git_changelog, generate_authors, get_cmdclass, and
    parse_dependency_links. Also test private _run_shell_command
    function.
    
    Change-Id: I583d679afc1dc805c920b4beffc2f664833aabd0

--- tests/unit/test_setup.py
+++ tests/unit/test_setup.py
@@ -18,8 +18,12 @@
 import os
 import sys
 from tempfile import mkstemp
+from tempfile import mkdtemp
 import unittest
+import shutil
+import stubout
 
+from openstack.common import setup as common_setup
 from openstack.common.setup import *
 
 
@@ -60,6 +64,116 @@ class MailmapTestCase(unittest.TestCase):
                          parse_mailmap(self.mailmap))
 
 
+class FakePopen(object):
+    fake_stdout = ""
+    returncode = 0
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def communicate(self):
+        return (self.fake_stdout, "")
+
+
+class DiveToTmpDir():
+    def __enter__(self):
+        self.tmp_dir = mkdtemp(prefix='openstack', suffix='.setup-dir')
+        self.old_cwd = os.getcwd()
+        os.chdir(self.tmp_dir)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        os.chdir(self.old_cwd)
+        shutil.rmtree(self.tmp_dir)
+
+
+class GitLogsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.stubs = stubout.StubOutForTesting()
+
+    def tearDown(self):
+        self.stubs.UnsetAll()
+
+    def test_write_git_changelog(self):
+        FakePopen.fake_stdout = "Author: Foo Bar <email@bar.com>\n"
+
+        self.stubs.Set(os.path, "isdir",
+                       lambda path: True)
+        self.stubs.Set(subprocess, "Popen",
+                       FakePopen)
+
+        with DiveToTmpDir():
+            with open(".mailmap", 'w') as mm_fh:
+                mm_fh.write("Foo Bar <email@foo.com> <email@bar.com>\n")
+
+            write_git_changelog()
+            with open("ChangeLog", 'r') as ch_fh:
+                self.assertTrue("email@foo.com" in ch_fh.read())
+
+    def test_generate_authors(self):
+        author_old = "Foo Foo <email@foo.com>"
+        author_new = "Bar Bar <email@bar.com>"
+
+        def fake_run_shell_command(cmd):
+            if cmd.startswith("git log"):
+                return author_new
+            return None
+
+        self.stubs.Set(os.path, "isdir",
+                       lambda path: True)
+        self.stubs.Set(common_setup, "_run_shell_command",
+                       fake_run_shell_command)
+
+        with DiveToTmpDir():
+            with open("AUTHORS.in", 'w') as auth_fh:
+                auth_fh.write(author_old)
+            generate_authors()
+            with open("AUTHORS", 'r') as auth_fh:
+                authors = auth_fh.read()
+                self.assertTrue(author_old in authors)
+                self.assertTrue(author_new in authors)
+
+
+class GetCmdClassTest(unittest.TestCase):
+
+    def setUp(self):
+        self.stubs = stubout.StubOutForTesting()
+
+    def tearDown(self):
+        self.stubs.UnsetAll()
+
+    def test_get_cmdclass(self):
+        cmdclass = get_cmdclass()
+
+        self.assertTrue('sdist' in cmdclass)
+        build_sphinx = cmdclass.get('build_sphinx')
+        if build_sphinx:
+            from distutils.dist import Distribution
+            from sphinx.setup_command import BuildDoc
+            self.stubs.Set(BuildDoc, "run", lambda self: None)
+            distr = Distribution()
+            distr.packages = ("fake_package",)
+            distr.command_options["build_sphinx"] = {"source_dir": ["a", "."]}
+
+            build_doc = build_sphinx(distr)
+
+            with DiveToTmpDir():
+                os.mkdir("fake_package")
+                with open("fake_package/__init__.py", "w"):
+                    pass
+                with open("fake_package/fake_module.py", "w"):
+                    pass
+
+                build_doc.run()
+
+                self.assertTrue(
+                    os.path.exists("api/autoindex.rst"))
+                self.assertTrue(
+                    os.path.exists("api/fake_package.fake_module.rst"))
+
+
 class ParseRequirementsTest(unittest.TestCase):
 
     def setUp(self):
@@ -95,3 +209,31 @@ class ParseRequirementsTest(unittest.TestCase):
             fh.write("argparse")
         if sys.version_info >= (2, 7):
             self.assertEqual([], parse_requirements([self.tmp_file]))
+
+    def test_get_requirement_from_file_empty(self):
+        actual = get_reqs_from_files([])
+        self.assertEqual([], actual)
+
+
+class ParseDependencyLinksTest(unittest.TestCase):
+
+    def setUp(self):
+        (fd, self.tmp_file) = mkstemp(prefix='openstack', suffix='.setup')
+
+    def tearDown(self):
+        if os.path.exists(self.tmp_file):
+            os.remove(self.tmp_file)
+
+    def test_parse_dependency_normal(self):
+        with open(self.tmp_file, 'w') as fh:
+            fh.write("http://test.com\n")
+        self.assertEqual(
+            ['http://test.com'],
+            parse_dependency_links([self.tmp_file]))
+
+    def test_parse_dependency_with_git_egg_url(self):
+        with open(self.tmp_file, 'w') as fh:
+            fh.write("-e git://foo.com/zipball#egg=bar")
+        self.assertEqual(
+            ['git://foo.com/zipball#egg=bar'],
+            parse_dependency_links([self.tmp_file]))
