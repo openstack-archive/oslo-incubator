9517c03020719e5837aa344e1e31a11fb801f824
Eric Windisch <eric@cloudscaling.com>
2013-01-24 16:28:43 -0500

    Redis-based matchmaker
    Introduces a reference implementation
    of a matchmaker (based on redis) that
    supports dynamic host/topic registrations,
    host expiration, and hooks for consuming
    applications to acknowledge or neg-acknowledge
    topic.host service availability.
    
    Implements blueprint advanced-matchmaking
    
    Change-Id: I8608d2089fca118b0e369f2eb5c6aedacf6821fe

--- tests/unit/rpc/test_matchmaker.py
+++ tests/unit/rpc/test_matchmaker.py
@@ -14,13 +14,17 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import eventlet
 import logging
-
-from openstack.common.rpc import matchmaker
 from tests import utils
 
+from openstack.common import importutils
+from openstack.common.rpc import matchmaker
+
+redis = importutils.try_import('redis')
 
 LOG = logging.getLogger(__name__)
+MatchMakerException = matchmaker.MatchMakerException
 
 
 class _MatchMakerTestCase(object):
@@ -28,6 +32,7 @@ class _MatchMakerTestCase(object):
         queues = self.driver.queues(self.topic)
         matched_hosts = map(lambda x: x[1], queues)
 
+        LOG.info("Received result from matchmaker: %s", matched_hosts)
         for host in matched_hosts:
             self.assertTrue(host in self.hosts)
 
@@ -36,9 +41,33 @@ class _MatchMakerTestCase(object):
         queues = self.driver.queues("fanout~" + self.topic)
         matched_hosts = map(lambda x: x[1], queues)
 
-        LOG.info("Received result from matchmaker: %s", queues)
+        LOG.info("Received result from matchmaker: %s", matched_hosts)
         for host in self.hosts:
             self.assertTrue(host in matched_hosts)
+
+
+class _MatchMakerDynRegTestCase(object):
+    def test_registers_host(self):
+        """
+        Registers a host, ensures it is registered.
+        """
+        self.driver.register(self.topic, self.hosts[0])
+
+        match = self.driver.queues(self.topic)
+        self.assertEqual(match[0][1], self.hosts[0])
+
+    def test_unregister(self):
+        """
+        Tests that hosts unregister cleanly.
+        Registers a host, ensures it is registered,
+        then unregisters and ensures is no
+        longer registered.
+        """
+        # Can only unregister if registrations work.
+        self.test_registers_host()
+
+        self.driver.unregister(self.topic, self.hosts[0])
+        self.assertEqual(self.driver.queues(self.topic), [])
 
 
 class MatchMakerFileTestCase(utils.BaseTestCase, _MatchMakerTestCase):
@@ -58,3 +87,96 @@ class MatchMakerLocalhostTestCase(utils.BaseTestCase, _MatchMakerTestCase):
         self.driver = matchmaker.MatchMakerLocalhost()
         self.topic = "test"
         self.hosts = ['localhost']
+
+
+class MatchMakerRedisTestCase(utils.BaseTestCase, _MatchMakerTestCase):
+    def setUp(self):
+        super(MatchMakerRedisTestCase, self).setUp()
+
+        if not redis:
+            self.skipTest("Redis required for test.")
+
+        self.config(matchmaker_heartbeat_ttl=1)
+
+        self.topic = "test"
+        self.hosts = map(lambda x: 'mockhost-' + str(x), range(1, 10))
+
+        try:
+            self.driver = matchmaker.MatchMakerRedis()
+            # Just test the connection
+            self.driver.redis.ping()
+        except:
+            raise self.skipTest("Redis server not available.")
+
+        # Wipe all entries...
+        for host in self.hosts:
+            self.driver.unregister(self.topic, host)
+
+        for h in self.hosts:
+            self.driver.register(self.topic, h)
+
+        self.driver.start_heartbeat()
+
+    def tearDown(self):
+        super(MatchMakerRedisTestCase, self).tearDown()
+        if not redis:
+            self.skipTest("Redis required for test.")
+        self.driver.stop_heartbeat()
+
+
+class MatchMakerRedisHeartbeatTestCase(utils.BaseTestCase, _MatchMakerDynRegTestCase):
+    def setUp(self):
+        super(MatchMakerRedisHeartbeatTestCase, self).setUp()
+
+        if not redis:
+            self.skipTest("Redis required for test.")
+
+        self.config(matchmaker_heartbeat_ttl=1)
+        self.driver = matchmaker.MatchMakerRedis()
+        self.topic = "test"
+        self.hosts = map(lambda x: 'mockhost-' + str(x), range(1, 10))
+
+        try:
+            self.driver = matchmaker.MatchMakerRedis()
+            # Just test the connection
+            self.driver.redis.ping()
+        except:
+            raise self.skipTest("Redis server not available.")
+
+        # Wipe all entries...
+        for host in self.hosts:
+            self.driver.unregister(self.topic, host)
+
+    def test_expires_set(self):
+        """
+        Test that expirations are set.
+        """
+        self.driver.register(self.topic, self.hosts[0])
+
+        ttl = self.driver.redis.ttl('.'.join((self.topic, self.hosts[0])))
+        self.assertTrue(ttl > -1)
+
+    def test_expires_hosts(self):
+        """
+        Tests that hosts expire.
+        Registers a host, ensures it is registered,
+        then waits for it to expire. Ensures is no
+        longer registered.
+        """
+        self.driver.register(self.topic, self.hosts[0])
+
+        key_host = '.'.join((self.topic, self.hosts[0]))
+
+        ttl = self.driver.redis.ttl(key_host)
+        eventlet.sleep(ttl + 1)
+        ttl2 = self.driver.redis.ttl(key_host)
+
+        # Tests that host has actually expired.
+        self.assertEqual(ttl2, -1)
+
+    def test_expired_hosts_removed(self):
+        """
+        Test that expired hosts are removed from results.
+        """
+        self.test_expires_hosts()
+        self.assertEqual(self.driver.queues(self.topic), [])
