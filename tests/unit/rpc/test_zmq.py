164faed8c1c60b35103f2a165e7f048002d96bd7
Eric Windisch <eric@cloudscaling.com>
2013-02-12 01:13:17 -0500

    Sanitize input before creating IPC socket.
    Sockets are created by the zeromq driver
    for the topic specified by each incoming message.
    
    Because the topic is arbitrarily supplied by the sender,
    path separators in the topic must be illegal.
    
    Change-Id: Iccdb9b69e646bfe7665ee34c367fd4019db25f17

--- tests/unit/rpc/test_zmq.py
+++ tests/unit/rpc/test_zmq.py
@@ -21,8 +21,10 @@ Unit Tests for remote procedure calls using zeromq
 import eventlet
 eventlet.monkey_patch()
 
+import itertools
 import logging
 import os
+import re
 import socket
 
 import fixtures
@@ -100,6 +102,25 @@ class _RpcZmqBaseTestCase(common.BaseRpcTestCase):
     def _close_reactor(self):
         if self.reactor:
             self.reactor.close()
+
+
+    def test_cast_pathsep_topic(self):
+        """Comply with AMQP topic expectations"""
+        tmp_topic = self.topic_nested
+
+        # All OS path separators
+        badchars = itertools.ifilter(None,
+                                     set((os.sep, os.altsep, '/', '\\')))
+        for char in badchars:
+            self.topic_nested = char.join(('hello', 'world'))
+            try:
+                # TODO(ewindisch): Determine which exception is raised.
+                #                  pending bug #1121348
+                self.assertRaises(Exception, self._test_cast,
+                                  common.TestReceiver.echo, 42, {"value": 42},
+                                  fanout=False)
+            finally:
+                self.topic_nested = tmp_topic
 
 
 class RpcZmqBaseTopicTestCase(_RpcZmqBaseTestCase):
