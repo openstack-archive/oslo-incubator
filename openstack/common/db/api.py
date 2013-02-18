543ce60ec5c18a8733ae3c75e86e113fcbe2e165
Chris Behrens <cbehrens@codestud.com>
2013-02-18 02:35:34 +0000

    Move DB thread pooling to DB API loader
    Fixes bug 1128605
    
    The dbpool code in sqlalchemy session is the wrong place to implement
    thread pooling as it wraps each individual SQL call to run in its own
    thread.  When combined with SQL server locking, all threads can be eaten
    waiting on locks with none available to run a 'COMMIT'.
    
    The correct place to do thread pooling is around each DB API call.
    
    This patch removes dbpool from sqlalchemy and creates a common DB API
    loader for all openstack projects which implements the following
    configuration options:
    
    db_backend: Full path to DB API backend module (or a known short name if
                a project chooses to implement a mapping)
    dbapi_use_tpool: True or False whether to use thread pooling around all
                     DB API calls.
    
    Example usage for nova/db/api.py would be:
    
    """
    from nova.openstack.common.db import api as db_api
    
    _KNOWN_BACKENDS = {'sqlalchemy': 'nova.db.sqlalchemy.api'}
    
    IMPL = db_api.DBAPI(known_backends=_KNOWN_BACKENDS)
    """
    
    NOTE: Enabling thread pooling will be broken until this issue is
    resolved in eventlet (or until we find a way to patch ourselves) OR
    until we modify our eventlet.monkey_patch() calls to include
    'thread=False':
    
    https://bitbucket.org/eventlet/eventlet/issue/137/use-of-threading-locks-causes-deadlock
    
    Change-Id: Idf14563ea07cf8ccf2a77b3f53659d8528927fc7

---
+++ openstack/common/db/api.py
@@ -0,0 +1,87 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2013 Rackspace Hosting
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Multiple DB API backend support.
+
+Supported configuration options:
+
+`db_backend`: DB backend name or full module path to DB backend module.
+`dbapi_use_tpool`: Enable thread pooling of DB API calls.
+
+A DB backend module should implement a class named 'API' that will be
+instatiated once by the backend loader.  For backwards compatibility with
+the sqlalchemy backend in nova, support methods defined within the
+module outside of a class.
+"""
+
+from openstack.common import cfg
+from openstack.common import lockutils
+from openstack.common import importutils
+
+
+db_opts = [
+    cfg.StrOpt('db_backend',
+               default='sqlalchemy',
+               help='The backend to use for db'),
+    cfg.BoolOpt('dbapi_use_tpool',
+               default=False,
+               help='Enable the use of thread pooling for all DB API calls')
+]
+
+CONF = cfg.CONF
+CONF.register_opts(db_opts)
+
+
+class DBAPI(object):
+    def __init__(self, known_backends=None):
+        if known_backends is None:
+            known_backends = {}
+        self.__backend = None
+        self.__known_backends = known_backends
+
+    @lockutils.synchronized('dbapi_backend', 'oslo-')
+    def __get_backend(self):
+        if self.__backend:
+            # Another thread assigned it
+            return self.__backend
+        backend_name = CONF.db_backend
+        self.__use_tpool = CONF.dbapi_use_tpool
+        if self.__use_tpool:
+            from eventlet import tpool
+            self.__tpool = tpool
+        # Import the untranslated name if we don't have a
+        # mapping.
+        backend_path = self.__known_backends.get(backend_name,
+                                                 backend_name)
+        backend_mod = importutils.import_module(backend_path)
+        try:
+            self.__backend = backend_mod.API()
+        except AttributeError:
+            self.__backend = backend_mod
+        return self.__backend
+
+    def __getattr__(self, key):
+        backend = self.__backend or self.__get_backend()
+        attr = getattr(backend, key)
+        if not self.__use_tpool or not hasattr(attr, '__call__'):
+            return attr
+
+        def tpool_wrapper(*args, **kwargs):
+            return self.__tpool.execute(attr, *args, **kwargs)
+
+        tpool_wrapper.__name__ = attr.__name__
+        return tpool_wrapper
