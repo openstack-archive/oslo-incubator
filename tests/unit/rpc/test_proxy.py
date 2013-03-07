bf6a595eab938af2a6e9bc6abeb30f170272505f
Matthew Sherborne <msherborne@gmail.com>
2013-03-07 14:25:55 +1000

    Improves Logging for for rpc method failures
    Adds rpc call exception logging to the RPCProxy.
    
    This means that all rpc calls will now have decent exception logging including a traceback, the rpc method called, and the topic.
    
    It can be disabled per call by passing 'log_exceptions=False' to any of the proxy's public methods.
    
    Works towards: bug #1148516
    
    Change-Id: I29a3b1b97c6114c4479e2b71c1257c4d72131535

--- tests/unit/rpc/test_proxy.py
+++ tests/unit/rpc/test_proxy.py
@@ -22,16 +22,20 @@ import copy
 
 from openstack.common import context
 from openstack.common import lockutils
-from openstack.common.fixture import moxstubout
 from openstack.common import rpc
 from openstack.common.rpc import proxy
 from tests import utils
 
 
+class UniqueException(Exception):
+    pass
+
+
 class RpcProxyTestCase(utils.BaseTestCase):
 
     def _test_rpc_method(self, rpc_method, has_timeout=False, has_retval=False,
-                         server_params=None, supports_topic_override=True):
+                         server_params=None, supports_topic_override=True,
+                         raise_exception=False):
         topic = 'fake_topic'
         timeout = 123
         rpc_proxy = proxy.RpcProxy(topic, '1.0')
@@ -39,11 +43,15 @@ class RpcProxyTestCase(utils.BaseTestCase):
         msg = {'method': 'fake_method', 'args': {'x': 'y'}}
         expected_msg = {'method': 'fake_method', 'args': {'x': 'y'},
                         'version': '1.0'}
+        expected_log = ('RPC call Exception: Topic: "fake_topic" - '
+                        'Method: "fake_method" - '
+                        'Exception: "The spider got you"')
 
         expected_retval = 'hi' if has_retval else None
 
         self.fake_args = None
         self.fake_kwargs = None
+        self.fake_log_msg = 'NOT LOGGED'
 
         def _fake_rpc_method(*args, **kwargs):
             rpc._check_for_lock()
@@ -52,15 +60,40 @@ class RpcProxyTestCase(utils.BaseTestCase):
             if has_retval:
                 return expected_retval
 
-        self.stubs.Set(rpc, rpc_method, _fake_rpc_method)
+        def _fake_rpc_exploding_method(*args, **kwargs):
+            _fake_rpc_method(*args, **kwargs)
+            raise UniqueException("The spider got you")
+
+        def _fake_logger(msg):
+            self.fake_log_msg = msg
+
+        if raise_exception:
+            self.stubs.Set(rpc, rpc_method, _fake_rpc_exploding_method)
+            self.stubs.Set(proxy.LOG, 'exception', _fake_logger)
+        else:
+            self.stubs.Set(rpc, rpc_method, _fake_rpc_method)
 
         args = [ctxt, msg]
         if server_params:
             args.insert(1, server_params)
 
+        def do_call(*args, **kwargs):
+            """Actually performs the call and checks the result"""
+            self.fake_log_msg = 'NOT LOGGED'
+            reraised = False
+            try:
+                retval = getattr(rpc_proxy, rpc_method)(*args, **kwargs)
+            except UniqueException:
+                reraised = True
+            if raise_exception:
+                self.assertEqual(reraised, True)
+                self.assertEqual(self.fake_log_msg, expected_log)
+            else:
+                self.assertEqual(retval, expected_retval)
+                self.assertEqual(self.fake_log_msg, 'NOT LOGGED')
+
         # Base method usage
-        retval = getattr(rpc_proxy, rpc_method)(*args)
-        self.assertEqual(retval, expected_retval)
+        do_call(*args)
         expected_args = [ctxt, topic, expected_msg]
         if server_params:
             expected_args.insert(1, server_params)
@@ -68,8 +101,7 @@ class RpcProxyTestCase(utils.BaseTestCase):
             self.assertEqual(arg, expected_arg)
 
         # overriding the version
-        retval = getattr(rpc_proxy, rpc_method)(*args, version='1.1')
-        self.assertEqual(retval, expected_retval)
+        do_call(*args, version='1.1')
         new_msg = copy.deepcopy(expected_msg)
         new_msg['version'] = '1.1'
         expected_args = [ctxt, topic, new_msg]
@@ -80,8 +112,7 @@ class RpcProxyTestCase(utils.BaseTestCase):
 
         if has_timeout:
             # set a timeout
-            retval = getattr(rpc_proxy, rpc_method)(ctxt, msg, timeout=timeout)
-            self.assertEqual(retval, expected_retval)
+            do_call(ctxt, msg, timeout=timeout)
             expected_args = [ctxt, topic, expected_msg, timeout]
             for arg, expected_arg in zip(self.fake_args, expected_args):
                 self.assertEqual(arg, expected_arg)
@@ -89,13 +120,22 @@ class RpcProxyTestCase(utils.BaseTestCase):
         if supports_topic_override:
             # set a topic
             new_topic = 'foo.bar'
-            retval = getattr(rpc_proxy, rpc_method)(*args, topic=new_topic)
-            self.assertEqual(retval, expected_retval)
+            expected_log = ('RPC call Exception: Topic: "foo.bar" - '
+                            'Method: "fake_method" - '
+                            'Exception: "The spider got you"')
+            do_call(*args, topic=new_topic)
             expected_args = [ctxt, new_topic, expected_msg]
             if server_params:
                 expected_args.insert(1, server_params)
             for arg, expected_arg in zip(self.fake_args, expected_args):
                 self.assertEqual(arg, expected_arg)
+
+        # Call ourselves again, but this time each call will raise an
+        # exception
+        if raise_exception is False:
+            self._test_rpc_method(rpc_method, has_timeout, has_retval,
+                                  server_params, supports_topic_override,
+                                  raise_exception=True)
 
     def test_call(self):
         self._test_rpc_method('call', has_timeout=True, has_retval=True)
