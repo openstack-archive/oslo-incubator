58b39cac572112caf59e84f626097c77d2a1a30c
Matthew Sherborne <msherborne@gmail.com>
2013-03-07 14:25:55 +1000

    Improves Logging for for rpc method failures
    Adds rpc call exception logging to the RPCProxy.
    
    This means that all rpc calls will now have decent exception logging including a traceback, the rpc method called, and the topic.
    
    It can be disabled per call by passing 'log_exceptions=False' to any of the proxy's public methods.
    
    Works towards: bug #1148516
    
    Change-Id: I29a3b1b97c6114c4479e2b71c1257c4d72131535

--- openstack/common/rpc/proxy.py
+++ openstack/common/rpc/proxy.py
@@ -22,7 +22,10 @@ For more information about rpc API version numbers, see:
 """
 
 
+from openstack.common import log as logging
 from openstack.common import rpc
+
+LOG = logging.getLogger(__name__)
 
 
 class RpcProxy(object):
@@ -32,6 +35,10 @@ class RpcProxy(object):
     specify the topic and API version in a single place.  This is intended to
     be used as a base class for a class that implements the client side of an
     rpc API.
+
+    By default, it will log any exceptions raised by the rpc call, then re
+    raise them. If your call is expected to fail you can pass
+    log_exceptions=False to any method to disable logging.
     """
 
     def __init__(self, topic, default_version):
@@ -62,42 +69,109 @@ class RpcProxy(object):
     def make_msg(method, **kwargs):
         return {'method': method, 'args': kwargs}
 
-    def call(self, context, msg, topic=None, version=None, timeout=None):
+    def _base_wrapper(self, method, topic, msg, *args, **kwargs):
+        """
+        Wraps a method call and logs any exceptions then re-raises them
+
+        :param method: callable method
+        :param *args: passed straight to 'method'
+        :param *kwargs: passed straight to 'method'
+        """
+        try:
+            return method(*args, **kwargs)
+        except Exception as exc:
+            detail_dict = {'method': '', 'topic': topic, 'exc': str(exc)}
+            detail_dict.update(msg)
+            details = (_(
+                'Topic: "%(topic)s" - Method: "%(method)s" - '
+                'Exception: "%(exc)s"') % detail_dict)
+            LOG.exception(_(
+                'RPC call Exception: %s' % details))
+            raise
+
+    def _exc_log_wrapper(self, method, context, topic, msg, *args, **kwargs):
+        """
+        Wraps an rpc call and logs and reraises any exceptions the call raises
+
+        :param method: callable method to wrap
+        :param context: the context passed to method
+        :param topic: The rpc call topic
+        :param msg: The rpc call msg
+        :param *args: anything else passed straight to 'method'
+        :param *kwargs: anything else passed straight to 'method'
+        """
+        method_args = (context, topic, msg) + args
+        return self._base_wrapper(method, topic, msg, *method_args, **kwargs)
+
+    def _server_exc_log_wrapper(self, method, context, server_params, topic,
+                                msg, *args, **kwargs):
+        """
+        Wraps an rpc call aimed at a server, and logs and reraises any
+        exceptions the call raises.
+
+        :param method: callable method to wrap
+        :param context: the context passed to method
+        :param server_params: the server_params, passed straight to the method
+        :param topic: The rpc call topic
+        :param msg: The rpc call msg
+        :param *args: anything else passed straight to 'method'
+        :param *kwargs: anything else passed straight to 'method'
+        """
+        method_args = (context, server_params, topic, msg) + args
+        return self._base_wrapper(method, topic, msg,
+                                  *method_args, **kwargs)
+
+    def call(self, context, msg, topic=None, version=None,
+             timeout=None, log_exceptions=True):
         """rpc.call() a remote method.
 
         :param context: The request context
         :param msg: The message to send, including the method and args.
         :param topic: Override the topic for this message.
+        :param version: (Optional) Override the requested API version in this
+               message.
         :param timeout: (Optional) A timeout to use when waiting for the
                response.  If no timeout is specified, a default timeout will be
                used that is usually sufficient.
-        :param version: (Optional) Override the requested API version in this
-               message.
+        :param log_exceptions: by default rpc exceptions are logged, then
+               re-raised if you don't wish to log them, set this to False
 
         :returns: The return value from the remote method.
         """
         self._set_version(msg, version)
-        return rpc.call(context, self._get_topic(topic), msg, timeout)
+        if log_exceptions:
+            return self._exc_log_wrapper(
+                rpc.call, context, self._get_topic(topic), msg, timeout)
+        else:
+            return rpc.call(context, self._get_topic(topic), msg, timeout)
 
-    def multicall(self, context, msg, topic=None, version=None, timeout=None):
+    def multicall(self, context, msg, topic=None, version=None,
+                  timeout=None, log_exceptions=True):
         """rpc.multicall() a remote method.
 
         :param context: The request context
         :param msg: The message to send, including the method and args.
         :param topic: Override the topic for this message.
+        :param version: (Optional) Override the requested API version in this
+               message.
         :param timeout: (Optional) A timeout to use when waiting for the
                response.  If no timeout is specified, a default timeout will be
                used that is usually sufficient.
-        :param version: (Optional) Override the requested API version in this
-               message.
+        :param log_exceptions: by default rpc exceptions are logged, then
+               re-raised if you don't wish to log them, set this to False
 
         :returns: An iterator that lets you process each of the returned values
                   from the remote method as they arrive.
         """
         self._set_version(msg, version)
-        return rpc.multicall(context, self._get_topic(topic), msg, timeout)
+        if log_exceptions:
+            return self._exc_log_wrapper(
+                rpc.multicall, context, self._get_topic(topic), msg, timeout)
+        else:
+            rpc.multicall(context, self._get_topic(topic), msg, timeout)
 
-    def cast(self, context, msg, topic=None, version=None):
+    def cast(self, context, msg, topic=None, version=None,
+             log_exceptions=True):
         """rpc.cast() a remote method.
 
         :param context: The request context
@@ -105,14 +179,21 @@ class RpcProxy(object):
         :param topic: Override the topic for this message.
         :param version: (Optional) Override the requested API version in this
                message.
+        :param log_exceptions: by default rpc exceptions are logged, then
+               re-raised if you don't wish to log them, set this to False
 
         :returns: None.  rpc.cast() does not wait on any return value from the
                   remote method.
         """
         self._set_version(msg, version)
-        rpc.cast(context, self._get_topic(topic), msg)
+        if log_exceptions:
+            self._exc_log_wrapper(
+                rpc.cast, context, self._get_topic(topic), msg)
+        else:
+            rpc.cast(context, self._get_topic(topic), msg)
 
-    def fanout_cast(self, context, msg, topic=None, version=None):
+    def fanout_cast(self, context, msg, topic=None, version=None,
+                    log_exceptions=True):
         """rpc.fanout_cast() a remote method.
 
         :param context: The request context
@@ -120,15 +201,21 @@ class RpcProxy(object):
         :param topic: Override the topic for this message.
         :param version: (Optional) Override the requested API version in this
                message.
+        :param log_exceptions: by default rpc exceptions are logged, then
+               re-raised if you don't wish to log them, set this to False
 
         :returns: None.  rpc.fanout_cast() does not wait on any return value
                   from the remote method.
         """
         self._set_version(msg, version)
-        rpc.fanout_cast(context, self._get_topic(topic), msg)
+        if log_exceptions:
+            self._exc_log_wrapper(
+                rpc.fanout_cast, context, self._get_topic(topic), msg)
+        else:
+            rpc.fanout_cast(context, self._get_topic(topic), msg)
 
     def cast_to_server(self, context, server_params, msg, topic=None,
-                       version=None):
+                       version=None, log_exceptions=True):
         """rpc.cast_to_server() a remote method.
 
         :param context: The request context
@@ -138,15 +225,23 @@ class RpcProxy(object):
         :param topic: Override the topic for this message.
         :param version: (Optional) Override the requested API version in this
                message.
+        :param log_exceptions: by default rpc exceptions are logged, then
+               re-raised if you don't wish to log them, set this to False
 
         :returns: None.  rpc.cast_to_server() does not wait on any
                   return values.
         """
         self._set_version(msg, version)
-        rpc.cast_to_server(context, server_params, self._get_topic(topic), msg)
+        if log_exceptions:
+            self._server_exc_log_wrapper(
+                rpc.cast_to_server, context, server_params,
+                self._get_topic(topic), msg)
+        else:
+            rpc.cast_to_server(context, server_params, self._get_topic(topic),
+                               msg)
 
     def fanout_cast_to_server(self, context, server_params, msg, topic=None,
-                              version=None):
+                              version=None, log_exceptions=True):
         """rpc.fanout_cast_to_server() a remote method.
 
         :param context: The request context
@@ -156,10 +251,17 @@ class RpcProxy(object):
         :param topic: Override the topic for this message.
         :param version: (Optional) Override the requested API version in this
                message.
+        :param log_exceptions: by default rpc exceptions are logged, then
+               re-raised if you don't wish to log them, set this to False
 
         :returns: None.  rpc.fanout_cast_to_server() does not wait on any
                   return values.
         """
         self._set_version(msg, version)
-        rpc.fanout_cast_to_server(context, server_params,
-                                  self._get_topic(topic), msg)
+        if log_exceptions:
+            self._server_exc_log_wrapper(
+                rpc.fanout_cast_to_server, context, server_params,
+                self._get_topic(topic), msg)
+        else:
+            rpc.fanout_cast_to_server(context, server_params,
+                                      self._get_topic(topic), msg)
