ee61a81d287cd641dc0ab006019c6357ad800b24
Monty Taylor <mordred@inaugust.com>
2013-03-27 16:46:36 +0100

    Support turning Sphinx warnings into errors.
    The build team would like to be able to turn this on for projects.
    Also, we should make it configurable. And while we're making that
    configurable, we should also make autoindexing configurable.
    
    Change-Id: I763329372be91afe442e87c7f3c0934763b4afc0

--- openstack/common/setup.py
+++ openstack/common/setup.py
@@ -226,15 +226,21 @@ def get_cmdclass():
     # just ignore it
     try:
         from sphinx.setup_command import BuildDoc
+        from sphinx import application
+
+        class LocalSphinx(application.Sphinx):
+
+            def __init__(self, *args, **kwargs):
+                kwargs['warningiserror'] = True
+                super(LocalSphinx, self).__init__(*args, **kwargs)
 
         class LocalBuildDoc(BuildDoc):
 
             builders = ['html', 'man']
 
-            def generate_autoindex(self):
+            def generate_autoindex(self, option_dict):
                 print "**Autodocumenting from %s" % os.path.abspath(os.curdir)
                 modules = {}
-                option_dict = self.distribution.get_option_dict('build_sphinx')
                 source_dir = os.path.join(option_dict['source_dir'][1], 'api')
                 if not os.path.exists(source_dir):
                     os.makedirs(source_dir)
@@ -263,8 +269,13 @@ def get_cmdclass():
                         autoindex.write("   %s.rst\n" % module)
 
             def run(self):
-                if not os.getenv('SPHINX_DEBUG'):
-                    self.generate_autoindex()
+                option_dict = self.distribution.get_option_dict(
+                    'build_sphinx')
+                if ('autoindex' in option_dict and
+                        not os.getenv('SPHINX_DEBUG')):
+                    self.generate_autoindex(option_dict)
+                if 'warnerrors' in option_dict:
+                    application.Sphinx = LocalSphinx
 
                 for builder in self.builders:
                     self.builder = builder
