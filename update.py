1a2b732ac0774546bdfc008badf8459e78c131c8
Zhongyue Luo <zhongyue.nah@intel.com>
2013-04-08 01:07:47 +0800

    Finds dependencies of configured modules for update.py
    Looks up import lines in the configures modules and finds dependencies.
    Did not load modules since it seemed as an overkill
    
    Fixes bug #963509
    
    Change-Id: I690cdf6fb149bbe243add3a35b96f6045ebe10f4

--- update.py
+++ update.py
@@ -86,6 +86,10 @@ opts = [
 ]
 
 
+OSLO_IMPORT_PATTERN = re.compile(r"\s*from\sopenstack\.common(\simport\s|\.)"
+                                 "(\w+)($|.+)")
+
+
 def _parse_args(argv):
     conf = cfg.ConfigOpts()
     conf.register_cli_opts(opts)
@@ -222,6 +226,49 @@ def _create_module_init(base, dest_dir, *sub_paths):
         open(init_path, 'w').close()
 
 
+def _find_mods(srcfile):
+    with open(srcfile, 'r') as f:
+        for line in f:
+            result = OSLO_IMPORT_PATTERN.match(line)
+            if result:
+                yield result.group(2)
+
+
+def _build_dependency_tree():
+    dep_tree = {}
+    base_path = os.path.join('openstack', 'common')
+    for dirpath, _, filenames in os.walk(base_path):
+        for filename in [x for x in filenames if x.endswith('.py')]:
+            if dirpath == base_path:
+                mod_name = filename.split('.')[0]
+            else:
+                mod_name = dirpath.split(os.sep)[2]
+            if mod_name == '__init__':
+                continue
+            dep_list = dep_tree.setdefault(mod_name, [])
+            filepath = os.path.join(dirpath, filename)
+            for mod in _find_mods(filepath):
+                if mod != mod_name and mod not in dep_list:
+                    dep_list.append(mod)
+    return dep_tree
+
+
+def _dfs_dependency_tree(dep_tree, mod_name, mod_list=[]):
+    mod_list.append(mod_name)
+    for mod in dep_tree.get(mod_name, []):
+        if mod not in mod_list:
+            mod_list = _dfs_dependency_tree(dep_tree, mod, mod_list)
+    return mod_list
+
+
+def _complete_module_list(mod_list):
+    dep_tree = _build_dependency_tree()
+    for mod in mod_list:
+        mod_list.extend([x for x in _dfs_dependency_tree(dep_tree, mod)
+                         if x not in mod_list])
+    return mod_list
+
+
 def main(argv):
     conf = _parse_args(argv)
 
@@ -244,7 +291,7 @@ def main(argv):
     _create_module_init(conf.base, dest_dir)
     _create_module_init(conf.base, dest_dir, 'common')
 
-    for mod in conf.module + conf.modules:
+    for mod in _complete_module_list(conf.module + conf.modules):
         _copy_module(mod, conf.base, dest_dir)
 
 
