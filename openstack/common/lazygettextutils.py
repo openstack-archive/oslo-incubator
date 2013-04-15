2989c1fdfcf53208511fe8303e5c1c38d1a74fc3
Matt Odden <mrodden@us.ibm.com>
2013-04-15 13:17:11 +0000

    Add basic lazy gettext implementation
    This is the first part of an implementation towards
    a deferred localization functionality in OpenStack.
    This change adds a Message class for encapsulating a
    translatable message and its relevant data, as well
    as an example LogHandler of how such a class can be
    used in the logging context.
    
    bp delayed-message-translation
    
    Change-Id: I8485a346d32925327ea9185e0da3822e4e19c2f5

--- openstack/common/lazygettextutils.py
+++ openstack/common/lazygettextutils.py
@@ -1,7 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2012 Red Hat, Inc.
-# All Rights Reserved.
+# Copyright 2012 IBM Corp.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -16,49 +15,9 @@
 #    under the License.
 
 """
-gettext for openstack-common modules.
-
-Usual usage in an openstack.common module:
-
-    from openstack.common.gettextutils import _
-"""
-
-import copy
-import gettext
-import inspect
-import logging.handlers
-import os
-import re
-import UserString
-
-_localedir = os.environ.get('oslo'.upper() + '_LOCALEDIR')
-_t = gettext.translation('oslo', localedir=_localedir, fallback=True)
-
-
-def _(msg):
-    return _t.ugettext(msg)
-
-
-def install(domain):
-    """Install a _() function using the given translation domain.
-
-    Given a translation domain, install a _() function using gettext's
-    install() function.
-
-    The main difference from gettext.install() is that we allow
-    overriding the default localedir (e.g. /usr/share/locale) using
-    a translation-domain-specific environment variable (e.g.
-    NOVA_LOCALEDIR).
-    """
-    gettext.install(domain,
-                    localedir=os.environ.get(domain.upper() + '_LOCALEDIR'),
-                    unicode=True)
-
-
-"""
 Lazy gettext functionality.
 
-The following is an attempt to introduce a deferred way
+This module is an attempt to introduce a deferred way
 to do translations on messages in OpenStack. We attempt to
 override the standard _() function and % (format string) operation
 to build Message objects that can later be translated when we have
@@ -66,6 +25,12 @@ more information. Also included is an example LogHandler that
 translates Messages to an associated locale, effectively allowing
 many logs, each with their own locale.
 """
+
+import copy
+import gettext
+import inspect
+import logging.handlers
+import re
 
 
 def get_lazy_gettext(domain):
@@ -85,37 +50,14 @@ def get_lazy_gettext(domain):
     return _lazy_gettext
 
 
-class Message(UserString.UserString, object):
+class Message(object):
     """Class used to encapsulate translatable messages."""
     def __init__(self, msg='', domain='oslo'):
-        # _orig_str is the gettext msgid and should never change
-        self._orig_str = msg
-        self._left_extra_msg = ''
-        self._right_extra_msg = ''
+        self.message = msg
+        self.extra_msg = ''
         self.params = None
         self.locale = None
         self.domain = domain
-
-    @property
-    def message(self):
-        # NOTE(mrodden): this should always resolve to a string
-        # that best represents the state of the message currently
-        if self.locale:
-            lang = gettext.translation(self.domain,
-                                       languages=[self.locale],
-                                       fallback=True)
-        else:
-            # use system locale for translations
-            lang = gettext.translation(self.domain, fallback=True)
-
-        full_msg = ''.join([self._left_extra_msg,
-                            lang.ugettext(self._orig_str),
-                            self._right_extra_msg])
-
-        if self.params is not None:
-            return full_msg % self.params
-        else:
-            return full_msg
 
     def _serialize_other(self, other):
         """
@@ -138,11 +80,15 @@ class Message(UserString.UserString, object):
 
         return None
 
-    def _save_parameters(self, other):
-        if isinstance(other, basestring):
+    def __mod__(self, other):
+        # do a format string to catch and raise
+        # any possible KeyErrors from missing parameters
+        (self.message + self.extra_msg) % other
+
+        if isinstance(other, str):
             self.params = other
         elif isinstance(other, dict):
-            full_msg = self.message
+            full_msg = self.message + self.extra_msg
             # look for %(blah) fields in string;
             # ignore %% and deal with the
             # case where % is first character on the line
@@ -177,86 +123,52 @@ class Message(UserString.UserString, object):
 
         return self
 
-    # overrides to be more string-like
+    def __add__(self, other):
+        self.extra_msg += other.__str__()
+
     def __unicode__(self):
-        return unicode(self.message)
+        """Behave like a string when needed
+
+        Localize the message and inject parameters when
+        we are requested to behave like a string (unicode) type
+        """
+        if self.locale:
+            try:
+                lang = gettext.translation(self.domain,
+                                           languages=[self.locale])
+                full_msg = lang.ugettext(self.message) + self.extra_msg
+            except IOError:
+                # no locale found so default to using no translation
+                full_msg = self.message + self.extra_msg
+        else:
+            t = gettext.translation(self.domain, fallback=True)
+            full_msg = t.ugettext(self.message) + self.extra_msg
+        if self.params is not None:
+            return unicode(full_msg % self.params)
+        else:
+            return unicode(full_msg)
 
     def __str__(self):
-        return unicode(self.message).encode('utf-8')
+        """Behave like a string when needed
 
-    def __contains__(self, key):
-        return key in self.message
+        Localize the message and inject parameters when
+        we are requested to behave like a string (str) type
+        """
+        return unicode(self).encode('utf-8')
 
-    def __nonzero__(self):
-        return bool(self.message)
+    def __contains__(self, item):
+        return str(self).__contains__(item)
 
-    def __dir__(self):
-        return dir(unicode)
-
-    def __iter__(self):
-        return iter(self.message)
-
-    def __len__(self):
-        return len(self.message)
+    def iteritems(self):
+        # crude attempt at serialization
+        local = dict(self.__dict__)
+        return local.iteritems()
 
     def __getattr__(self, name):
-        if name == '__members__':
-            return self.__dir__()
-        return getattr(self.message, name)
-
-    def __getstate__(self):
-        return self._func, self._args
-
-    def __setstate__(self, tup):
-        self._func, self._args = tup
-
-    def __getitem__(self, key):
-        return self.message[key]
-
-    def __copy__(self):
-        return self
-
-    # operator overloads
-    def __add__(self, other):
-        self._right_extra_msg += other.__str__()
-        return self
-
-    def __radd__(self, other):
-        self._left_extra_msg += other.__str__()
-        return self
-
-    def __mod__(self, other):
-        # do a format string to catch and raise
-        # any possible KeyErrors from missing parameters
-        self.message % other
-        return self._save_parameters(other)
-
-    def __rmod__(self, other):
-        return other % self.message
-
-    def __mul__(self, other):
-        return self.message * other
-
-    def __rmul__(self, other):
-        return other * self.message
-
-    def __lt__(self, other):
-        return self.message < other
-
-    def __le__(self, other):
-        return self.message <= other
-
-    def __eq__(self, other):
-        return self.message == other
-
-    def __ne__(self, other):
-        return self.message != other
-
-    def __gt__(self, other):
-        return self.message > other
-
-    def __ge__(self, other):
-        return self.message >= other
+        supported = ['find']
+        if name in supported:
+            return getattr(unicode(self), name)
+        raise AttributeError()
 
 
 class LocaleHandler(logging.handlers.MemoryHandler):
