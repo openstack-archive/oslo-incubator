814e55fead7d2056e0270390003ccdc08b5a935e
Flaper Fesp <flaper87@gmail.com>
2013-04-29 16:58:23 +0200

    Reduce duplicated code related to policys
    This patch adds some logic that is currently duplicated throughout
    Openstack. As part of this de-duplication, the patch also modifies
    current implementation.
    
    Major Changes:
        * check, set_rules, reset, init are now part of the Enforcer class
        * check was renamed into enforce
        * init was renamed into load_rules
        * It is now possible to load multiple files and have per instance
          rules instead of global rules.
        * There's a global instance of the Enforcer class that can be used as
          main enforcer.
    
            from openstack.common import policy
            ENFORCER = policy.ENFORCER
            ENFORCER.enforce(rule, target, creds)
    
    Minor Changes:
        * Added do_raise to the enforce method
        * Enforcer instance is now passed to the Check call.
    
    NOTE: If / once this patch gets in, I'll update other projects and port
    them to the latest version.
    
    Change-Id: Ife909bdf3277ef33c2fb1eae16ae261fa6374c63

--- openstack/common/fileutils.py
+++ openstack/common/fileutils.py
@@ -19,6 +19,12 @@
 import errno
 import os
 
+from openstack.common.gettextutils import _
+from openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+_FILE_CACHE = {}
 
 def ensure_tree(path):
     """Create a directory (and any ancestor directories required)
@@ -33,3 +39,31 @@ def ensure_tree(path):
                 raise
         else:
             raise
+
+
+def read_cached_file(filename, reload_func=None, force_reload=False):
+    """Read from a file if it has been modified.
+
+    :param reload_func: optional function to be called with data when
+                        file is reloaded due to a modification.
+
+    :param force_reload: Whether to reload the file.
+    :returns: data from file
+
+    """
+    global _FILE_CACHE
+
+    if force_reload:
+        _FILE_CACHE.pop(filename, None)
+
+    mtime = os.path.getmtime(filename)
+    cache_info = _FILE_CACHE.setdefault(filename, {})
+
+    if not cache_info or mtime != cache_info.get('mtime'):
+        LOG.debug(_("Reloading cached file %s") % filename)
+        with open(filename) as fap:
+            cache_info['data'] = fap.read()
+        cache_info['mtime'] = mtime
+        if reload_func:
+            reload_func(cache_info['data'])
+    return cache_info['data']
