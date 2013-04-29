1091b4f3bed9b0ad8c23e5db6d4a5cee15fc786c
Flaper Fesp <flaper87@gmail.com>
2013-04-29 16:58:23 +0200

    Reduce duplicated code related to policies
    This patch adds some logic that is currently duplicated throughout
    Openstack. As part of this de-duplication, the patch also modifies
    current implementation.
    
    Major Changes:
        * check, set_rules, reset, init are now part of the Enforcer class
        * check was renamed into enforce
        * init was renamed into load_rules
        * It is now possible to load multiple files and have per instance
          rules instead of global rules.
        * There's a global instance of the Enforcer class that can be used as
          main enforcer.
    
            from openstack.common import policy
            ENFORCER = policy.ENFORCER
            ENFORCER.enforce(rule, target, creds)
    
    Minor Changes:
        * Added do_raise to the enforce method
        * Enforcer instance is now passed to the Check call.
    
    NOTE: If / once this patch gets in, I'll update other projects and port
    them to the latest version.
    
    Change-Id: Ife909bdf3277ef33c2fb1eae16ae261fa6374c63

--- tests/unit/test_fileutils.py
+++ tests/unit/test_fileutils.py
@@ -15,9 +15,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import __builtin__
 import os
 import shutil
 import tempfile
+
+import mox
 
 from openstack.common import fileutils
 from tests import utils
@@ -34,3 +37,48 @@ class EnsureTree(utils.BaseTestCase):
         finally:
             if os.path.exists(tmpdir):
                 shutil.rmtree(tmpdir)
+
+
+class TestCachedFile(utils.BaseTestCase):
+
+    def setUp(self):
+        super(TestCachedFile, self).setUp()
+        self.mox = mox.Mox()
+        self.addCleanup(self.mox.UnsetStubs)
+
+    def test_read_cached_file(self):
+        self.mox.StubOutWithMock(os.path, "getmtime")
+        os.path.getmtime(mox.IgnoreArg()).AndReturn(1)
+        self.mox.ReplayAll()
+
+        fileutils._FILE_CACHE = {
+            '/this/is/a/fake': {"data": 1123, "mtime": 1}
+        }
+        fresh, data = fileutils.read_cached_file("/this/is/a/fake")
+        fdata = fileutils._FILE_CACHE['/this/is/a/fake']["data"]
+        self.assertEqual(fdata, data)
+
+    def test_read_modified_cached_file(self):
+        self.mox.StubOutWithMock(os.path, "getmtime")
+        self.mox.StubOutWithMock(__builtin__, 'open')
+        os.path.getmtime(mox.IgnoreArg()).AndReturn(2)
+
+        fake_contents = "lorem ipsum"
+        fake_file = self.mox.CreateMockAnything()
+        fake_file.read().AndReturn(fake_contents)
+        fake_context_manager = self.mox.CreateMockAnything()
+        fake_context_manager.__enter__().AndReturn(fake_file)
+        fake_context_manager.__exit__(mox.IgnoreArg(),
+                                      mox.IgnoreArg(),
+                                      mox.IgnoreArg())
+
+        __builtin__.open(mox.IgnoreArg()).AndReturn(fake_context_manager)
+
+        self.mox.ReplayAll()
+        fileutils._FILE_CACHE = {
+            '/this/is/a/fake': {"data": 1123, "mtime": 1}
+        }
+
+        fresh, data = fileutils.read_cached_file("/this/is/a/fake")
+        self.assertEqual(data, fake_contents)
+        self.assertTrue(fresh)
