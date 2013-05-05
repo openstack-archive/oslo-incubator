5dcc43b118fd6ce65676861bee126b1de5e0a8d1
Matthew Sherborne <msherborne@gmail.com>
2013-05-05 16:14:38 +1000

    Break out common functionality for filters and weights
    In the context of openstack.common.scheduler:
    
    base_filter.BaseFilterManager and base_weight.BaseWeightManager had
    duplicated code.
    
    This patch breaks that code out into a shared base class:
    base_handler.BaseHandler
    
    Change-Id: Ie5232c6dd9876a75170beda71adde90fc6bb4153

--- tests/unit/scheduler/test_base_filter.py
+++ tests/unit/scheduler/test_base_filter.py
@@ -60,6 +60,7 @@ class FakeFilter1(BaseFakeFilter):
     * AND
     * It has a fake entry point defined (is returned by fake ExtensionManager)
     """
+    pass
 
 
 class FakeFilter2(BaseFakeFilter):
@@ -69,6 +70,7 @@ class FakeFilter2(BaseFakeFilter):
     * BUT
     * It has no entry point
     """
+    pass
 
 
 class FakeFilter3(base_filter.BaseFilter):
@@ -112,7 +114,7 @@ class TestBaseFilterHandler(utils.BaseTestCase):
 
     def setUp(self):
         super(TestBaseFilterHandler, self).setUp()
-        self.stubs.Set(base_filter.extension, 'ExtensionManager',
+        self.stubs.Set(base_filter.base_handler.extension, 'ExtensionManager',
                        FakeExtensionManager)
         self.handler = base_filter.BaseFilterHandler(BaseFakeFilter,
                                                      'fake_filters')
