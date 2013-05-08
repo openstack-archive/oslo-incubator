505d8f6debe4ced34aa518b42ff96812a45f8558
Flaper Fesp <flaper87@gmail.com>
2013-05-08 17:13:08 +0200

    Implement cache abstraction layer
    This patch implements a cache abstraction library that can be extended
    and supports multiple back-ends.
    
    The patch being submitted defines the library's API and implements a
    first in-memory back-end, which is the default one and can be used for
    development purposes.
    
    Configuration variables
    =======================
    
    The library introduces 2 configuration variables:
    
        * cache_backend: The back-end to use.
        * cache_prefix: A prefix to prepend to every key.
    
    Installing back-ends
    ====================
    
    Back-ends are loaded using stevedore, which means, they most be
    installed as entry_points under the `openstack.common.cache.backends`
    namespace. See `setup.cfg`.
    
    Back-ends Configuration
    =======================
    
    As for now, every back-end is expected to define the configuration
    parameters it needs that will later accessed through the `GroupAttr`
    instance passed to it. Before a back-end instance is created, the
    `register_opts` classmethod will be called.
    
    Things I'm not sure about
    =========================
    
        * Is there a better way to let back-ends register their configuration
        parameters?
    
        * Is a separate group really needed? (I personally prefer to keep
        those configs isolated from app's ones)
    
        * Is the whole locking stuff really needed? What about just using
        .get / .pop and catch errors?
    
    NOTE: Some of the ideas where taken from Django's cache library and others
    from Oslo's memcached module.
    
    Implements blueprint cache-backend-abstraction
    
    Change-Id: Ic2a84ecef949fb28a7d05fe1b2c54ca91da3983c

---
+++ openstack/common/cache/cache.py
@@ -0,0 +1,57 @@
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Cache library.
+
+Supported configuration options:
+
+`cache_backend`: Name of the cache back-end to use.
+"""
+
+from oslo.config import cfg
+from stevedore import driver
+
+_cache_options = [
+    cfg.StrOpt('cache_backend',
+               default='memory',
+               help='The cache driver to use, default value is `memory`.'),
+    cfg.StrOpt('cache_prefix',
+               default=None,
+               help='Prefix to use in every cache key'),
+]
+
+
+def get_cache(conf):
+    """Loads the cache back-end
+
+    This function loads the cache back-end
+    specified in the given configuration.
+
+    :param conf: Configuration instance to use
+    """
+    cache_group = cfg.OptGroup(name='cache',
+                               title='Cache options')
+
+    conf.register_group(cache_group)
+    conf.register_opts(_cache_options, group=cache_group)
+
+    cache_conf = conf.cache
+    kwargs = dict(cache_prefix=cache_conf.cache_prefix)
+
+    backend = cache_conf.cache_backend
+    mgr = driver.DriverManager('openstack.common.cache.backends', backend,
+                               invoke_on_load=True,
+                               invoke_args=[cache_conf],
+                               invoke_kwds=kwargs)
+    return mgr.driver
