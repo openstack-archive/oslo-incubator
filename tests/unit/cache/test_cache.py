ddfd8385bdca0f95f772e2f075c663118487e7ae
Flaper Fesp <flaper87@gmail.com>
2013-05-08 17:13:08 +0200

    Implement cache abstraction layer
    This patch implements a cache abstraction library that can be extended
    and supports multiple back-ends.
    
    The patch being submitted defines the library's API and implements a
    first in-memory back-end, which is the default one and can be used for
    development purposes.
    
    Configuration variables
    =======================
    
    The library introduces 1 optional configuration variable:
    
        * cache_url: The cache url. For example:
            memory://?default_ttl=2
    
    `cache.register_oslo_config` needs to be called explicitly with a
    ConfigOpts instance in order to register this parameter. The library can
    be used without oslo.config.
    
    Installing back-ends
    ====================
    
    Back-ends are loaded using stevedore, which means, they most be
    installed as entry_points under the `openstack.common.cache.backends`
    namespace. See `setup.cfg`.
    
    NOTE: Some of the ideas where taken from Django's cache library and others
    from Oslo's memcached module.
    
    Implements blueprint cache-backend-abstraction
    
    Change-Id: Ic2a84ecef949fb28a7d05fe1b2c54ca91da3983c

---
+++ tests/unit/cache/test_cache.py
@@ -0,0 +1,37 @@
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo.config import cfg
+
+from openstack.common.cache._backends import memory
+from openstack.common.cache import cache
+from tests import utils
+
+
+class TestCacheModule(utils.BaseTestCase):
+
+    def test_oslo_config(self):
+        conf = cfg.ConfigOpts()
+        cache.register_oslo_configs(conf)
+        driver = cache.get_cache(conf.cache_url)
+        self.assertIsInstance(driver, memory.MemoryBackend)
+
+    def test_get_cache(self):
+        driver = cache.get_cache()
+        self.assertIsInstance(driver, memory.MemoryBackend)
+
+    def test_get_cache_options(self):
+        driver = cache.get_cache('memory://?default_ttl=4')
+        self.assertIsInstance(driver, memory.MemoryBackend)
+        self.assertEqual(driver._default_ttl, 4)
