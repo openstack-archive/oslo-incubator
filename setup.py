2a6c8bd690099e4f11e53a263e9abebc45625b0d
Monty Taylor <mordred@inaugust.com>
2013-05-11 15:18:04 -0400

    Migrate to pbr.
    Also, remove the files associated with this that should not be pulled
    from oslo-incubator any longer.
    
    Closes bug 1179007
    
    Change-Id: Id2e882837bdde51a60562e8cdd207c7ff0c88a6a

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
-# -*- encoding: utf-8 -*-
-# Copyright (c) 2012 OpenStack Foundation.
-# Copyright (c) 2012-2013 Hewlett-Packard Development Company, L.P.
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,59 +16,6 @@
 
 import setuptools
 
-from openstack.common import setup
-
-requires = setup.parse_requirements()
-depend_links = setup.parse_dependency_links()
-package = 'openstack.common'
-
-filters = [
-    "AvailabilityZoneFilter = "
-    "openstack.common.scheduler.filters."
-    "availability_zone_filter:AvailabilityZoneFilter",
-    "CapabilitiesFilter = "
-    "openstack.common.scheduler.filters."
-    "capabilities_filter:CapabilitiesFilter",
-    "JsonFilter = "
-    "openstack.common.scheduler.filters.json_filter:JsonFilter",
-]
-
-weights = [
-    "FakeWeigher1 = tests.unit.fakes:FakeWeigher1",
-    "FakeWeigher2 = tests.unit.fakes:FakeWeigher2",
-]
-
 setuptools.setup(
-    name=package,
-    version=setup.get_version(package, '2013.1'),
-    description="Common components for Openstack",
-    long_description="Common components for Openstack "
-                     "including paster templates.",
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6',
-        'Environment :: No Input/Output (Daemon)', ],
-    keywords='openstack',
-    author='OpenStack',
-    author_email='openstack@lists.launchpad.net',
-    url='http://www.openstack.org/',
-    license='Apache Software License',
-    packages=setuptools.find_packages(exclude=['ez_setup',
-                                               'examples', 'tests']),
-    include_package_data=True,
-    cmdclass=setup.get_cmdclass(),
-    zip_safe=True,
-    install_requires=requires,
-    dependency_links=depend_links,
-    entry_points={
-        "openstack.common.scheduler.filters": filters,
-        "openstack.common.tests.fakes.weights": weights,
-        "console_scripts": [
-            'oslo-rootwrap = openstack.common.rootwrap.cmd:main',
-            'oslo-rpc-zmq-receiver = openstack.common.rpc.zmq_receiver:main',
-        ]
-    },
-    namespace_packages=['openstack'],
-)
+    setup_requires=['d2to1>=0.2.10,<0.3', 'pbr>=0.5,<0.6'],
+    d2to1=True)
