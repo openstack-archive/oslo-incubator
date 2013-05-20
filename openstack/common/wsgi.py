4018a8e34528da36ab216b2fd757ddc6272933d1
Oleg Bondarev <obondarev@mirantis.com>
2013-05-20 13:08:09 +0400

    Make serial/deserializers in wsgi configurable
    fixes bug 1165666
    
    Change-Id: I038f245059f685cc886ad4cbcd10aecd9a4ee59c

--- openstack/common/wsgi.py
+++ openstack/common/wsgi.py
@@ -22,7 +22,6 @@ from __future__ import print_function
 import eventlet
 eventlet.patcher.monkey_patch(all=False, socket=True)
 
-import datetime
 import errno
 import socket
 import sys
@@ -32,19 +31,15 @@ import eventlet.wsgi
 from oslo.config import cfg
 import routes
 import routes.middleware
-import six
 import webob.dec
 import webob.exc
-from xml.dom import minidom
-from xml.parsers import expat
 
 from openstack.common import exception
 from openstack.common.gettextutils import _
-from openstack.common import jsonutils
+from openstack.common import importutils
 from openstack.common import log as logging
 from openstack.common import service
 from openstack.common import sslutils
-from openstack.common import xmlutils
 
 socket_opts = [
     cfg.IntOpt('backlog',
@@ -56,8 +51,35 @@ socket_opts = [
                     "server socket. Not supported on OS X."),
 ]
 
+# NOTE: In order to use default body serial/deserializers in the project
+# wsgi_body_serializers module should be added to the project's
+# openstack-common.conf (so that update.py copy it along with wsgi module)
+serializer_opts = [
+    cfg.StrOpt('json_serializer',
+               default='openstack.common.wsgi_body_serializers.'
+                       'JSONDictSerializer',
+               help='JSON body serializer to be used in Response '
+                    '(class path)'),
+    cfg.StrOpt('xml_serializer',
+               default='openstack.common.wsgi_body_serializers.'
+                       'XMLDictSerializer',
+               help='XML body serializer to be used in Response '
+                    '(class path)'),
+    cfg.StrOpt('json_deserializer',
+               default='openstack.common.wsgi_body_serializers.'
+                       'JSONDeserializer',
+               help='JSON body deserializer to be used in Request '
+                    '(class path)'),
+    cfg.StrOpt('xml_deserializer',
+               default='openstack.common.wsgi_body_serializers.'
+                       'XMLDeserializer',
+               help='XML body deserializer to be used in Request '
+                    '(class path)'),
+]
+
 CONF = cfg.CONF
 CONF.register_opts(socket_opts)
+CONF.register_opts(serializer_opts)
 
 LOG = logging.getLogger(__name__)
 
@@ -446,118 +468,6 @@ class DictSerializer(ActionDispatcher):
         return ""
 
 
-class JSONDictSerializer(DictSerializer):
-    """Default JSON request body serialization"""
-
-    def default(self, data):
-        def sanitizer(obj):
-            if isinstance(obj, datetime.datetime):
-                _dtime = obj - datetime.timedelta(microseconds=obj.microsecond)
-                return _dtime.isoformat()
-            return six.text_type(obj)
-        return jsonutils.dumps(data, default=sanitizer)
-
-
-class XMLDictSerializer(DictSerializer):
-
-    def __init__(self, metadata=None, xmlns=None):
-        """
-        :param metadata: information needed to deserialize xml into
-                         a dictionary.
-        :param xmlns: XML namespace to include with serialized xml
-        """
-        super(XMLDictSerializer, self).__init__()
-        self.metadata = metadata or {}
-        self.xmlns = xmlns
-
-    def default(self, data):
-        # We expect data to contain a single key which is the XML root.
-        root_key = data.keys()[0]
-        doc = minidom.Document()
-        node = self._to_xml_node(doc, self.metadata, root_key, data[root_key])
-
-        return self.to_xml_string(node)
-
-    def to_xml_string(self, node, has_atom=False):
-        self._add_xmlns(node, has_atom)
-        return node.toprettyxml(indent='    ', encoding='UTF-8')
-
-    #NOTE (ameade): the has_atom should be removed after all of the
-    # xml serializers and view builders have been updated to the current
-    # spec that required all responses include the xmlns:atom, the has_atom
-    # flag is to prevent current tests from breaking
-    def _add_xmlns(self, node, has_atom=False):
-        if self.xmlns is not None:
-            node.setAttribute('xmlns', self.xmlns)
-        if has_atom:
-            node.setAttribute('xmlns:atom', "http://www.w3.org/2005/Atom")
-
-    def _to_xml_node(self, doc, metadata, nodename, data):
-        """Recursive method to convert data members to XML nodes."""
-        result = doc.createElement(nodename)
-
-        # Set the xml namespace if one is specified
-        # TODO(justinsb): We could also use prefixes on the keys
-        xmlns = metadata.get('xmlns', None)
-        if xmlns:
-            result.setAttribute('xmlns', xmlns)
-
-        #TODO(bcwaldon): accomplish this without a type-check
-        if type(data) is list:
-            collections = metadata.get('list_collections', {})
-            if nodename in collections:
-                metadata = collections[nodename]
-                for item in data:
-                    node = doc.createElement(metadata['item_name'])
-                    node.setAttribute(metadata['item_key'], str(item))
-                    result.appendChild(node)
-                return result
-            singular = metadata.get('plurals', {}).get(nodename, None)
-            if singular is None:
-                if nodename.endswith('s'):
-                    singular = nodename[:-1]
-                else:
-                    singular = 'item'
-            for item in data:
-                node = self._to_xml_node(doc, metadata, singular, item)
-                result.appendChild(node)
-        #TODO(bcwaldon): accomplish this without a type-check
-        elif type(data) is dict:
-            collections = metadata.get('dict_collections', {})
-            if nodename in collections:
-                metadata = collections[nodename]
-                for k, v in data.items():
-                    node = doc.createElement(metadata['item_name'])
-                    node.setAttribute(metadata['item_key'], str(k))
-                    text = doc.createTextNode(str(v))
-                    node.appendChild(text)
-                    result.appendChild(node)
-                return result
-            attrs = metadata.get('attributes', {}).get(nodename, {})
-            for k, v in data.items():
-                if k in attrs:
-                    result.setAttribute(k, str(v))
-                else:
-                    node = self._to_xml_node(doc, metadata, k, v)
-                    result.appendChild(node)
-        else:
-            # Type is atom
-            node = doc.createTextNode(str(data))
-            result.appendChild(node)
-        return result
-
-    def _create_link_nodes(self, xml_doc, links):
-        link_nodes = []
-        for link in links:
-            link_node = xml_doc.createElement('atom:link')
-            link_node.setAttribute('rel', link['rel'])
-            link_node.setAttribute('href', link['href'])
-            if 'type' in link:
-                link_node.setAttribute('type', link['type'])
-            link_nodes.append(link_node)
-        return link_nodes
-
-
 class ResponseHeadersSerializer(ActionDispatcher):
     """Default response headers serialization"""
 
@@ -573,8 +483,9 @@ class ResponseSerializer(object):
 
     def __init__(self, body_serializers=None, headers_serializer=None):
         self.body_serializers = {
-            'application/xml': XMLDictSerializer(),
-            'application/json': JSONDictSerializer(),
+            'application/xml': importutils.import_object(CONF.xml_serializer),
+            'application/json': importutils.import_object(
+                CONF.json_serializer),
         }
         self.body_serializers.update(body_serializers or {})
 
@@ -628,8 +539,10 @@ class RequestDeserializer(object):
         self.supported_content_types = supported_content_types
 
         self.body_deserializers = {
-            'application/xml': XMLDeserializer(),
-            'application/json': JSONDeserializer(),
+            'application/xml': importutils.import_object(
+                CONF.xml_deserializer),
+            'application/json': importutils.import_object(
+                CONF.json_deserializer),
         }
         self.body_deserializers.update(body_deserializers or {})
 
@@ -718,83 +631,3 @@ class TextDeserializer(ActionDispatcher):
 
     def default(self, datastring):
         return {}
-
-
-class JSONDeserializer(TextDeserializer):
-
-    def _from_json(self, datastring):
-        try:
-            return jsonutils.loads(datastring)
-        except ValueError:
-            msg = _("cannot understand JSON")
-            raise exception.MalformedRequestBody(reason=msg)
-
-    def default(self, datastring):
-        return {'body': self._from_json(datastring)}
-
-
-class XMLDeserializer(TextDeserializer):
-
-    def __init__(self, metadata=None):
-        """
-        :param metadata: information needed to deserialize xml into
-                         a dictionary.
-        """
-        super(XMLDeserializer, self).__init__()
-        self.metadata = metadata or {}
-
-    def _from_xml(self, datastring):
-        plurals = set(self.metadata.get('plurals', {}))
-
-        try:
-            node = xmlutils.safe_minidom_parse_string(datastring).childNodes[0]
-            return {node.nodeName: self._from_xml_node(node, plurals)}
-        except expat.ExpatError:
-            msg = _("cannot understand XML")
-            raise exception.MalformedRequestBody(reason=msg)
-
-    def _from_xml_node(self, node, listnames):
-        """Convert a minidom node to a simple Python type.
-
-        :param listnames: list of XML node names whose subnodes should
-                          be considered list items.
-
-        """
-
-        if len(node.childNodes) == 1 and node.childNodes[0].nodeType == 3:
-            return node.childNodes[0].nodeValue
-        elif node.nodeName in listnames:
-            return [self._from_xml_node(n, listnames) for n in node.childNodes]
-        else:
-            result = dict()
-            for attr in node.attributes.keys():
-                result[attr] = node.attributes[attr].nodeValue
-            for child in node.childNodes:
-                if child.nodeType != node.TEXT_NODE:
-                    result[child.nodeName] = self._from_xml_node(child,
-                                                                 listnames)
-            return result
-
-    def find_first_child_named(self, parent, name):
-        """Search a nodes children for the first child with a given name"""
-        for node in parent.childNodes:
-            if node.nodeName == name:
-                return node
-        return None
-
-    def find_children_named(self, parent, name):
-        """Return all of a nodes children who have the given name"""
-        for node in parent.childNodes:
-            if node.nodeName == name:
-                yield node
-
-    def extract_text(self, node):
-        """Get the text field contained by the given node"""
-        if len(node.childNodes) == 1:
-            child = node.childNodes[0]
-            if child.nodeType == child.TEXT_NODE:
-                return child.nodeValue
-        return ""
-
-    def default(self, datastring):
-        return {'body': self._from_xml(datastring)}
--- tests/unit/test_wsgi.py
+++ tests/unit/test_wsgi.py
@@ -21,7 +21,6 @@ import urllib2
 
 import mock
 import routes
-import six
 import webob
 
 from openstack.common import exception
@@ -163,103 +162,11 @@ class DictSerializerTest(utils.BaseTestCase):
         self.assertEqual(serializer.serialize({}, 'NonExistantAction'), '')
 
 
-class XMLDictSerializerTest(utils.BaseTestCase):
-
-    def test_xml(self):
-        input_dict = dict(servers=dict(a=(2, 3)))
-        expected_xml = """<servers xmlns="asdf">
-                           <a>(2,3)</a>
-                         </servers>"""
-        serializer = wsgi.XMLDictSerializer(xmlns="asdf")
-        result = serializer.serialize(input_dict)
-        result = result.replace('\n', '').replace(' ', '')
-        expected_xml = expected_xml.replace('\n', '').replace(' ', '')
-        self.assertEqual(result, expected_xml)
-
-
-class JSONDictSerializerTest(utils.BaseTestCase):
-
-    def test_json(self):
-        input_dict = dict(servers=dict(a=(2, 3)))
-        expected_json = '{"servers":{"a":[2,3]}}'
-        serializer = wsgi.JSONDictSerializer()
-        result = serializer.serialize(input_dict)
-        result = result.replace('\n', '').replace(' ', '')
-        self.assertEqual(result, expected_json)
-
-    def test_object_unicode(self):
-        class TestUnicode:
-            def __unicode__(self):
-                return six.u('TestUnicode')
-        input_dict = dict(cls=TestUnicode())
-        expected_str = '{"cls":"TestUnicode"}'
-        serializer = wsgi.JSONDictSerializer()
-        result = serializer.serialize(input_dict)
-        result = result.replace('\n', '').replace(' ', '')
-        self.assertEqual(result, expected_str)
-
-
 class TextDeserializerTest(utils.BaseTestCase):
 
     def test_dispatch_default(self):
         deserializer = wsgi.TextDeserializer()
         self.assertEqual(deserializer.deserialize({}, 'update'), {})
-
-
-class JSONDeserializerTest(utils.BaseTestCase):
-
-    def test_json(self):
-        data = """{"a": {
-                "a1": "1",
-                "a2": "2",
-                "bs": ["1", "2", "3", {"c": {"c1": "1"}}],
-                "d": {"e": "1"},
-                "f": "1"}}"""
-        as_dict = {
-            'body': {
-                'a': {
-                    'a1': '1',
-                    'a2': '2',
-                    'bs': ['1', '2', '3', {'c': {'c1': '1'}}],
-                    'd': {'e': '1'},
-                    'f': '1',
-                },
-            },
-        }
-        deserializer = wsgi.JSONDeserializer()
-        self.assertEqual(deserializer.deserialize(data), as_dict)
-
-
-class XMLDeserializerTest(utils.BaseTestCase):
-
-    def test_xml(self):
-        xml = """
-            <a a1="1" a2="2">
-              <bs><b>1</b><b>2</b><b>3</b><b><c c1="1"/></b></bs>
-              <d><e>1</e></d>
-              <f>1</f>
-            </a>
-            """.strip()
-        as_dict = {
-            'body': {
-                'a': {
-                    'a1': '1',
-                    'a2': '2',
-                    'bs': ['1', '2', '3', {'c': {'c1': '1'}}],
-                    'd': {'e': '1'},
-                    'f': '1',
-                },
-            },
-        }
-        metadata = {'plurals': {'bs': 'b', 'ts': 't'}}
-        deserializer = wsgi.XMLDeserializer(metadata=metadata)
-        self.assertEqual(deserializer.deserialize(xml), as_dict)
-
-    def test_xml_empty(self):
-        xml = '<a></a>'
-        as_dict = {"body": {"a": {}}}
-        deserializer = wsgi.XMLDeserializer()
-        self.assertEqual(deserializer.deserialize(xml), as_dict)
 
 
 class RequestHeadersDeserializerTest(utils.BaseTestCase):
