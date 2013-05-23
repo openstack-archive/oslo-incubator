e613ad019da411183645ed24092a44098972586e
Michael Wilson <geekinutah@gmail.com>
2013-05-23 10:27:08 -0600

    Don't reconnect to exclusive fanout consumers.
    See bug #1180166, basically we shouldn't be trying to reconnect to
    exclusive queues.
    
    Change-Id: I71ac5a150ea8c4d97a34404cb821ce8e8dfa152d

--- openstack/common/rpc/impl_qpid.py
+++ openstack/common/rpc/impl_qpid.py
@@ -93,6 +93,7 @@ class ConsumerBase(object):
         self.callback = callback
         self.receiver = None
         self.session = None
+        self.topic = None
 
         addr_opts = {
             "create": "always",
@@ -118,10 +119,17 @@ class ConsumerBase(object):
 
         self.address = "%s ; %s" % (node_name, jsonutils.dumps(addr_opts))
 
-        self.reconnect(session)
+        self.connect(session)
+
+    def connect(self, session):
+        """Declare the reciever on connect."""
+        self._declare_receiver(session)
 
     def reconnect(self, session):
         """Re-declare the receiver after a qpid reconnect."""
+        self._declare_receiver(session)
+
+    def _declare_receiver(self, session):
         self.session = session
         self.receiver = session.receiver(self.address)
         self.receiver.capacity = 1
@@ -156,6 +164,9 @@ class ConsumerBase(object):
 
     def get_receiver(self):
         return self.receiver
+
+    def get_node_name(self):
+        return self.address.split(';')[0]
 
 
 class DirectConsumer(ConsumerBase):
@@ -213,6 +224,18 @@ class FanoutConsumer(ConsumerBase):
             {"durable": False, "type": "fanout"},
             "%s_fanout_%s" % (topic, uuid.uuid4().hex),
             {"exclusive": True})
+
+    def reconnect(self, session):
+        topic = self.get_node_name()
+        params = {
+            'session': session,
+            'topic': topic,
+            'callback': self.callback,
+        }
+
+        self.__init__(conf=None, **params)
+
+        super(FanoutConsumer, self).reconnect(session)
 
 
 class Publisher(object):
