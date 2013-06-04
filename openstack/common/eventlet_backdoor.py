852a68adbfffe7b570511dc68a10fc7b847ab8c8
Raymond Pekowski <pekowski@gmail.com>
2013-06-04 16:59:24 -0500

    Improve usability when backdoor_port is nonzero
    Users who may not know that configuring a backdoor_port with 0
    allows multiple services to be enabled for the eventlet backdoor
    or who simply want a more predictable port assignment might like
    this patch. If the specified port is in use, it is incremented
    until a free port is found. This is a backdoor_port collision
    recovery scheme as opposed to the collision failure scheme
    that exists today.
    
    This related to I95fdb5ca: Add support for backdoor_port to be
    returned with a rpc call.
    
    Change-Id: I7ec346db3575995fa15483b617eea34c1e003bb0

--- openstack/common/eventlet_backdoor.py
+++ openstack/common/eventlet_backdoor.py
@@ -18,8 +18,11 @@
 
 from __future__ import print_function
 
+import errno
 import gc
+import os
 import pprint
+import socket
 import sys
 import traceback
 
@@ -28,14 +31,20 @@ import eventlet.backdoor
 import greenlet
 from oslo.config import cfg
 
+from openstack.common import log as logging
+
 eventlet_backdoor_opts = [
     cfg.IntOpt('backdoor_port',
                default=None,
-               help='port for eventlet backdoor to listen')
+               help='(depricated) port for eventlet backdoor to listen'),
+    cfg.IntOpt('backdoor_starting_port',
+               default=None,
+               help='starting port for eventlet backdoor to try to listen')
 ]
 
 CONF = cfg.CONF
 CONF.register_opts(eventlet_backdoor_opts)
+LOG = logging.getLogger(__name__)
 
 
 def _dont_use_this():
@@ -69,8 +78,20 @@ def initialize_if_enabled():
         'pnt': _print_nativethreads,
     }
 
-    if CONF.backdoor_port is None:
-        return None
+    msg1 = 'backdoor_port is depricated and replaced with ' + \
+           'backdoor_starting_port. Please update your configuration file.'
+    if CONF.backdoor_starting_port is None:
+        if CONF.backdoor_port is None:
+            return None
+        else:
+            msg2 = 'Setting backdoor_starting_port to the value of ' + \
+                   'backdoor_port.'
+            LOG.info(_('%(msg1)s %(msg2)s') % {'msg1': msg1, 'msg2': msg2})
+        CONF.backdoor_starting_port = CONF.backdoor_port
+    else:
+        if CONF.backdoor_port is not None:
+            msg2 = 'Ignoring backdoor_port.'
+            LOG.info(_('%(msg1)s %(msg2)s') % {'msg1': msg1, 'msg2': msg2})
 
     # NOTE(johannes): The standard sys.displayhook will print the value of
     # the last expression and set it to __builtin__._, which overwrites
@@ -82,8 +103,21 @@ def initialize_if_enabled():
             pprint.pprint(val)
     sys.displayhook = displayhook
 
-    sock = eventlet.listen(('localhost', CONF.backdoor_port))
+    try_port = CONF.backdoor_starting_port
+    while True:
+        try:
+            sock = eventlet.listen(('localhost', try_port))
+            break
+        except socket.error as exc:
+            if exc.errno == errno.EADDRINUSE:
+                try_port += 1
+            else:
+                raise
+    # In the case of backdoor_port being zero, a port number is assigned by
+    # listen().  In any case, pull the port number out here.
     port = sock.getsockname()[1]
+    LOG.info(_('Using %(port)s for the backdoor port of process %(pid)d') %
+             {'port': port, 'pid': os.getpid()})
     eventlet.spawn_n(eventlet.backdoor.backdoor_server, sock,
                      locals=backdoor_locals)
     return port
