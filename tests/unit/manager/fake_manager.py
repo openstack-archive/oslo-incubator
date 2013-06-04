acae23bd0f500832b639651ebc3ed6dfe6df603c
Svetlana Shturm <sshturm@mirantis.com>
2013-06-04 07:54:53 +0100

    Move common settings from services manager.
    Some services have common settings in base manager.
    For example nova.manager, cinder.manager...
    
    In fact cinder uses its own periodic tasks, but if we
    change this part (replacement own periodic tasks by oslo periodic)
    we can use main manager for both services.
    
    Blueprint: oslo-base-manager
    
    Change-Id: I24a49b8eccde2f703b7406dd27a5c32428da868f

---
+++ tests/unit/manager/fake_manager.py
@@ -0,0 +1,31 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2011 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+Though openstack.commom.manager.base_service_manager.BaseManager is just a
+helper class for creation origin service manager, some methods should be
+overwritten during definition for successfull work.
+For example, get_project_name should return 'nova' for nova service in loading
+plugins. In cinder it can be missed.
+"""
+
+from openstack.common.manager import base_service_manager
+
+
+class FakeManager(base_service_manager.BaseManager):
+
+    def get_project_name(self):
+        return 'fake_project'
