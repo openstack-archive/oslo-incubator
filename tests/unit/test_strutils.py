85e10b726585ee586b8c5b292567deea4d511544
Zhongyue Luo <zhongyue.nah@intel.com>
2013-06-08 12:02:41 +0800

    Adds testcases to test_strutils.to_bytes()
    The 'default' arg was intended for returning a default value
    when the input has a valid unit name but no magnitude.
    This patch adds cases to make sure refactoring on to_bytes()
    doesn't break original behavior.
    
    Change-Id: I1e31d3f3cbb81be656a3e67ae2479e0afd0d4b21

--- tests/unit/test_strutils.py
+++ tests/unit/test_strutils.py
@@ -185,16 +185,19 @@ class StrUtilsTest(utils.BaseTestCase):
             '0KB': 0,
             '0TB': 0,
             '': 0,
+            'B': 0,
+            'k': 0,
+            'Mb': 0,
+            'tB': 0,
         }
         for (in_value, expected_value) in working_examples.items():
             b_value = strutils.to_bytes(in_value)
             self.assertEquals(expected_value, b_value)
-            if in_value:
-                in_value = "-" + in_value
-                b_value = strutils.to_bytes(in_value)
-                self.assertEquals(expected_value * -1, b_value)
+            in_value = "-" + in_value
+            b_value = strutils.to_bytes(in_value)
+            self.assertEquals(expected_value * -1, b_value)
         breaking_examples = [
-            'junk1KB', '1023BBBB',
+            'KxB', 'mx', 'junk1KB', '1023BBBB',
         ]
         for v in breaking_examples:
             self.assertRaises(TypeError, strutils.to_bytes, v)
