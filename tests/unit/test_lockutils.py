d3529914fbcd2f9387aac993f161b34da8dfbf88
Flaper Fesp <flaper87@gmail.com>
2013-06-10 14:25:58 +0200

    Make lock_file_prefix optional
    Currently, the lock_file_prefix says it should end with an hypen if
    specified. It seems that it was intended to be optional. Since that
    behavior makes sense, this patch changes the synchronized's signature
    and makes lock_file_prefix optional.
    
    With this patch, the prefix will not be required to end with an hypen
    anymore, such sign will be added to the prefix if specified and if not
    already present.
    
    This patch doesn't change synchronized's behavior at all, which means it
    doesn't break backward compatibility.
    
    Change-Id: I8841da1e8d105aac4a86b247228a9c935e00afc5

--- tests/unit/test_lockutils.py
+++ tests/unit/test_lockutils.py
@@ -211,3 +211,25 @@ class LockTestCase(utils.BaseTestCase):
         self.config(lock_path=lock_dir)
 
         self.assertTrue(bar(lock_dir, lock_pfix, lock_name))
+
+    def test_optional_prefix(self):
+        lock_dir = tempfile.mkdtemp()
+
+        @lockutils.synchronized('lock', 'test-', True, lock_dir)
+        def test():
+            path = os.path.join(lock_dir, "test-lock")
+            self.assertTrue(os.path.exists(path))
+
+        @lockutils.synchronized('lock', external=True, lock_path=lock_dir)
+        def test_without_prefix():
+            path = os.path.join(lock_dir, "lock")
+            self.assertTrue(os.path.exists(path))
+
+        @lockutils.synchronized('lock', 'hypen', True, lock_dir)
+        def test_without_hypen():
+            path = os.path.join(lock_dir, "hypen-lock")
+            self.assertTrue(os.path.exists(path))
+
+        test()
+        test_without_hypen()
+        test_without_prefix()
