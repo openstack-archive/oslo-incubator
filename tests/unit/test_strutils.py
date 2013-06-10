8bfd3e7c4a6877ed68af930a7565d5393e2a7912
Alessio Ababilov <aababilov@griddynamics.com>
2013-06-10 10:49:59 +0300

    Add slugify to strutils
    This function will be used in apiclient library.
    
    Change-Id: I19f976eda896e7bede07510aafebe4931e512351

--- tests/unit/test_strutils.py
+++ tests/unit/test_strutils.py
@@ -198,3 +198,16 @@ class StrUtilsTest(utils.BaseTestCase):
         ]
         for v in breaking_examples:
             self.assertRaises(TypeError, strutils.to_bytes, v)
+
+    def test_slugify(self):
+        slugify = strutils.slugify
+        self.assertRaises(TypeError, slugify, True)
+        self.assertEqual("hello", slugify("hello"))
+        self.assertEqual("two-words", slugify("Two Words"))
+        self.assertEqual("ma-any-spa-ce-es", slugify("Ma-any\t spa--ce- es"))
+        self.assertEqual("excamation", slugify("exc!amation!"))
+        self.assertEqual("ampserand", slugify("&ampser$and"))
+        self.assertEqual("ju5tnum8er", slugify("ju5tnum8er"))
+        self.assertEqual("perche", slugify("perch\xc3\xa9"))
+        self.assertEqual(six.u("strange"), slugify('\x80strange',
+                                                   errors='ignore'))
