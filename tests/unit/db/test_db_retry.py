5c4f0518ed88b3913ad877e26fcc4b99830c127a
Victor Sergeyev <vsergeyev@mirantis.com>
2013-06-20 17:13:43 +0300

    Automatic retry db.api query if db connection lost
    Current patch allows us to retry db.api method call if
    database connection was lost temporally.
    
    It can be done in several ways:
    1) - We can use enable_db_retry() to decorate some functions, if we
         want to apply db-retry feature only to them.
    2) - We can allow db-retry feature in config (`use_db_reconnect` option
         in `database` group). It's will allow db-retry for all db.api
         methods. If we need to disable this feature for some functions
         (suppose, we afraid, that it's can be unsafe) we can use
         disable_db_retry() decorator.
    
    If function suppose to be retried and it's got `db-gone-away` error,
    it's will calls in a loop until it succeeds, or until maximum retries
    count will be reached.
    Maximum retries count and timeout between retries attempts are configurable.
    
    We use SQLA engine.dialect.is_disconnect() function to determine
    broken db connection so it's should work for all supported SQLA
    backends (MySQL, PostgreSQL, DB2 and SQLite supported as well).
    
    Added wrapper for session and query methods. This wrapper catch
    exceptions caused by broken db connection and re-raise it as
    DBConnectionError exception.
    
    Note, that it's safely to use db-retry feature only if all db.api
    queries use single transaction.
    
    At the moment this feature is experimental, so disabled by default.
    
    Previous attepmt - https://review.openstack.org/#/c/10797/
    'Proof-of-concept' patch in Nova https://review.openstack.org/#/c/35610/
    
    blueprint db-reconnect
    
    Co-authored-by: Devananda van der Veen <devananda.vdv@gmail.com>
    
    Change-Id: Ifd848ea70a62fb9f7ccbc8bfd22a10ab9d3135c9

---
+++ tests/unit/db/test_db_retry.py
@@ -0,0 +1,143 @@
+# Copyright 2013 Mirantis Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+from openstack.common.db import api
+from openstack.common.db import exception
+from openstack.common import importutils
+
+from tests import utils as test_utils
+
+sqla = importutils.import_module('sqlalchemy')
+exc = sqla.exc
+
+
+class TestDBAPI(object):
+    def _api_raise(self, *args, **kwargs):
+        """Simulate raising a database-has-gone-away error
+
+        This method creates a fake OperationalError with an ID matching
+        a valid MySQL "database has gone away" situation. It also decrements
+        the error_counter so that we can artificially keep track of
+        how many times this function is called by the wrapper. When
+        error_counter reaches zero, this function returns True, simulating
+        the database becoming available again and the query succeeding.
+        """
+
+        if self.error_counter > 0:
+            self.error_counter -= 1
+            orig = exc.DBAPIError(False, False, False)
+            orig.args = [2006, 'Test raise operational error']
+            e = exception.DBConnectionError(orig)
+            raise e
+        else:
+            return True
+
+    def api_raise_default(self, *args, **kwargs):
+        return self._api_raise(*args, **kwargs)
+
+    @api.enable_db_retry
+    def api_raise_enable_retry(self, *args, **kwargs):
+        return self._api_raise(*args, **kwargs)
+
+    @api.disable_db_retry
+    def api_raise_disable_retry(self, *args, **kwargs):
+        return self._api_raise(*args, **kwargs)
+
+
+class DBReconnectTestCase(test_utils.BaseTestCase):
+    def setUp(self):
+        super(DBReconnectTestCase, self).setUp()
+
+        self.test_db_api = TestDBAPI()
+        self.dbapi = api.DBAPI()
+        self.dbapi._DBAPI__backend = self.test_db_api
+        self.dbapi._DBAPI__use_tpool = False
+
+    def test_raise_connection_error(self):
+        self.test_db_api.error_counter = 5
+        self.assertRaises(exception.DBConnectionError, self.dbapi._api_raise)
+
+    def test_retry_one(self):
+        self.config(group='database', use_db_reconnect=True)
+        self.config(group='database', db_retry_interval=1)
+        try:
+            func = self.dbapi.api_raise_default
+            self.test_db_api.error_counter = 1
+            self.assertEqual(True, func(), 'Single retry did not succeed.')
+        except Exception:
+            self.fail('Single retry raised an un-wrapped error.')
+
+        self.assertEqual(
+            0, self.test_db_api.error_counter,
+            'Counter not decremented, retry logic probably failed.')
+
+    def test_retry_two(self):
+        self.config(group='database', use_db_reconnect=True)
+        self.config(group='database', db_inc_retry_interval=False)
+        self.config(group='database', db_retry_interval=1)
+
+        try:
+            func = self.dbapi.api_raise_default
+            self.test_db_api.error_counter = 2
+            self.assertEqual(True, func(), 'Multiple retry did not succeed.')
+        except Exception:
+            self.fail('Multiple retry raised an un-wrapped error.')
+
+        self.assertEqual(
+            0, self.test_db_api.error_counter,
+            'Counter not decremented, retry logic probably failed.')
+
+    def test_retry_until_failure(self):
+        self.config(group='database', use_db_reconnect=True)
+        self.config(group='database', db_inc_retry_interval=False)
+        self.config(group='database', db_max_retries=3)
+        self.config(group='database', db_retry_interval=1)
+
+        func = self.dbapi.api_raise_default
+        self.test_db_api.error_counter = 5
+        self.assertRaises(
+            exception.DBError, func,
+            'Retry of permanent failure did not throw DBError exception.')
+
+        self.assertNotEqual(
+            0, self.test_db_api.error_counter,
+            'Retry did not stop after sql_max_retries iterations.')
+
+    def test_retry_enabled_by_decorator(self):
+        self.config(group='database', db_retry_interval=1)
+        try:
+            func = self.dbapi.api_raise_enable_retry
+            self.test_db_api.error_counter = 1
+            self.assertEqual(True, func(), 'Single retry did not succeed.')
+        except Exception:
+            self.fail('Single retry raised an un-wrapped error.')
+
+        self.assertEqual(
+            0, self.test_db_api.error_counter,
+            'Counter not decremented, retry logic probably failed.')
+
+    def test_retry_disabled_by_decorator(self):
+        self.config(group='database', use_db_reconnect=True)
+        self.test_db_api.error_counter = 2
+        self.assertRaises(
+            exception.DBConnectionError,
+            self.dbapi.api_raise_disable_retry,
+        )
+
+        self.assertEqual(
+            1, self.test_db_api.error_counter,
+            'Counter decremented, retry disable logic probably failed.')
