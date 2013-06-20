35db0a1cc18a827d61310c3e524195be2c1597f8
Victor Sergeyev <vsergeyev@mirantis.com>
2013-06-20 17:13:43 +0300

    Automatic retry db.api query if db connection lost
    Current patch allows us to retry db.api method call if
    database connection was lost temporally.
    
    It can be done in several ways:
    1) - We can use enable_db_retry() to decorate some functions, if we
         want to apply db-retry feature only to them.
    2) - We can allow db-retry feature in config (`use_db_reconnect` option
         in `database` group). It's will allow db-retry for all db.api
         methods. If we need to disable this feature for some functions
         (suppose, we afraid, that it's can be unsafe) we can use
         disable_db_retry() decorator.
    
    If function suppose to be retried and it's got `db-gone-away` error,
    it's will calls in a loop until it succeeds, or until maximum retries
    count will be reached.
    Maximum retries count and timeout between retries attempts are configurable.
    
    We use SQLA engine.dialect.is_disconnect() function to determine
    broken db connection so it's should work for all supported SQLA
    backends (MySQL, PostgreSQL, DB2 and SQLite supported as well).
    
    Added wrapper for session and query methods. This wrapper catch
    exceptions caused by broken db connection and re-raise it as
    DBConnectionError exception.
    
    Note, that it's safely to use db-retry feature only if all db.api
    queries use single transaction.
    
    At the moment this feature is experimental, so disabled by default.
    
    Previous attepmt - https://review.openstack.org/#/c/10797/
    'Proof-of-concept' patch in Nova https://review.openstack.org/#/c/35610/
    
    blueprint db-reconnect
    
    Co-authored-by: Devananda van der Veen <devananda.vdv@gmail.com>
    
    Change-Id: Ifd848ea70a62fb9f7ccbc8bfd22a10ab9d3135c9

--- openstack/common/db/api.py
+++ openstack/common/db/api.py
@@ -37,12 +37,15 @@ A bug for eventlet has been filed here:
 https://bitbucket.org/eventlet/eventlet/issue/137/
 """
 import functools
+import time
 
 from oslo.config import cfg
 
+from openstack.common.db import exception
+from openstack.common.gettextutils import _  # noqa
 from openstack.common import importutils
 from openstack.common import lockutils
-
+from openstack.common import log as logging
 
 db_opts = [
     cfg.StrOpt('backend',
@@ -55,11 +58,92 @@ db_opts = [
                 deprecated_name='dbapi_use_tpool',
                 deprecated_group='DEFAULT',
                 help='Enable the experimental use of thread pooling for '
-                     'all DB API calls')
+                     'all DB API calls'),
+    cfg.BoolOpt('use_db_reconnect',
+                default=False,
+                help='Enable the experimental use of database reconnect '
+                     'on connection lost'),
+    cfg.IntOpt('db_retry_interval',
+               default=1,
+               help='seconds between db connection retries'),
+    cfg.BoolOpt('db_inc_retry_interval',
+                default=True,
+                help='Whether to increase interval between db connection '
+                     'retries, up to db_max_retry_interval'),
+    cfg.IntOpt('db_max_retry_interval',
+               default=10,
+               help='max seconds between db connection retries, if '
+                    'db_inc_retry_interval is enabled'),
+    cfg.IntOpt('db_max_retries',
+               default=20,
+               help='maximum db connection retries before error is raised. '
+                    '(setting -1 implies an infinite retry count)'),
 ]
 
 CONF = cfg.CONF
 CONF.register_opts(db_opts, 'database')
+
+LOG = logging.getLogger(__name__)
+
+
+def disable_db_retry(f):
+    'Disable db-retry for decorated function, if db-retry enabled in config.'
+    f.__dict__['skip_retry'] = True
+    return f
+
+
+def enable_db_retry(f):
+    'Enable db-retry for decorated function, if db-retry disabled in config.'
+    # NOTE(vsergeyev): We should avoid situation when db.api method decorated
+    #                  by _wrap_db_retry() decorator twice. It can happens if
+    #                  we decorated singly function and set `use_db_reconnect`
+    #                  option to True.
+    if CONF.database.use_db_reconnect:
+        return f
+    return _wrap_db_retry(f)
+
+
+def _wrap_db_retry(f):
+    '''Enable retry for db.api method, if DBConnectionError() raised
+
+    Enable db-retry for decorated db.api methods. If we enabled
+    `use_db_reconnect` in config, this decorator will be applied to
+    all db.api functions.
+    Decorator catchs DBConnectionError() and retries function in a
+    loop until it succeeds, or until maximum retries count will be reached.
+    '''
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        next_interval = CONF.database.db_retry_interval
+        remaining = CONF.database.db_max_retries
+
+        while True:
+            try:
+                return f(*args, **kwargs)
+            except exception.DBConnectionError as e:
+                if remaining == 0:
+                    LOG.exception(_('DB exceeded retry limit.'))
+                    raise exception.DBError(e)
+                if remaining != -1:
+                    remaining -= 1
+                    LOG.exception(_('DB connection error.'))
+                # NOTE(vsergeyev): We are using patched time module, so this is
+                #                  effectively yields the execution context to
+                #                  another green thread.
+                time.sleep(next_interval)
+                if CONF.database.db_inc_retry_interval:
+                    next_interval = min(
+                        next_interval * 2,
+                        CONF.database.db_max_retry_interval
+                    )
+    return wrapper
+
+
+def wrap_tpool(f, thread_pool):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        return thread_pool.execute(f, *args, **kwargs)
+    return wrapper
 
 
 class DBAPI(object):
@@ -96,11 +180,17 @@ class DBAPI(object):
     def __getattr__(self, key):
         backend = self.__backend or self.__get_backend()
         attr = getattr(backend, key)
-        if not self.__use_tpool or not hasattr(attr, '__call__'):
+
+        if not hasattr(attr, '__call__'):
             return attr
+        # NOTE(vsergeyev): If `use_db_reconnect` option is set to True, retry
+        #                  all DB API methods on disconnect, unless retries
+        #                  are disabled explicitly for this method by the
+        #                  means of disable_db_retry() decorator
+        if CONF.database.use_db_reconnect and not getattr(attr,
+                                                          'skip_retry', False):
+            attr = _wrap_db_retry(attr)
+        if self.__use_tpool:
+            attr = wrap_tpool(attr, self.__tpool)
 
-        def tpool_wrapper(*args, **kwargs):
-            return self.__tpool.execute(attr, *args, **kwargs)
-
-        functools.update_wrapper(tpool_wrapper, attr)
-        return tpool_wrapper
+        return attr
