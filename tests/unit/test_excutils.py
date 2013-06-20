282db519cc49bced00e1f82bf42de00610defef8
Raymond Pekowski <pekowski@gmail.com>
2013-06-20 15:56:54 +0000

    Make AMQP based RPC consumer threads more robust
    bug 1189711 Should RPC consume_in_thread() be more fault tolerant?
    
    There are unprotected holes in the thread kicked off by RPC
    consume_in_thread such that an exception will kill the thread.
    This exists for both the service (TopicConsumer) and the new
    reply proxy (DirectConsumer) consumers. This patch plugs
    those holes as close to the base of the consumer thread as
    possible by catching all non-caught exceptions and retrying
    with sleeps between retries and some pacing of the log
    output to prevent log flooding.
    
    Change-Id: I0d6ec8a5e3a310314da201656ee862bb40b41616

--- tests/unit/test_excutils.py
+++ tests/unit/test_excutils.py
@@ -14,6 +14,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import inspect
+import logging
+import mox
+import time
+
 from openstack.common import excutils
 from tests import utils
 
@@ -47,3 +52,115 @@ class SaveAndReraiseTest(utils.BaseTestCase):
             e = _e
 
         self.assertEqual(str(e), msg)
+
+
+class ForeverRetryUncaughtExceptionsTest(utils.BaseTestCase):
+
+    @excutils.forever_retry_uncaught_exceptions
+    def exception_generator(self):
+        if self.exception_count == len(self.exception_tuple):
+            return
+        self.exception_count += 1
+        raise Exception(self.exception_tuple[self.exception_count - 1])
+
+    def my_time_sleep(self, arg):
+        pass
+
+    def common_exc_retrier_init(self):
+        self.stubs.Set(time, 'sleep', self.my_time_sleep)
+        self.mox.StubOutWithMock(logging, 'exception')
+        self.mox.StubOutWithMock(time, 'time')
+        self.exception_count = 0
+
+    def test_exc_retrier_1exc_gives_1log(self):
+        self.common_exc_retrier_init()
+        time.time().AndReturn(1)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        self.mox.ReplayAll()
+        self.exception_tuple = ('unexpected',)
+        self.exception_generator()
+        self.addCleanup(self.stubs.UnsetAll)
+
+    def test_exc_retrier_same_10exc_1min_gives_1log(self):
+        self.common_exc_retrier_init()
+        time.time().AndReturn(1)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        for i in range(2,11):
+            time.time().AndReturn(i)
+        self.mox.ReplayAll()
+        self.exception_tuple = ('unexpected',) * 10
+        self.exception_generator()
+        self.addCleanup(self.stubs.UnsetAll)
+
+    def test_exc_retrier_same_2exc_2min_gives_2logs(self):
+        self.common_exc_retrier_init()
+        time.time().AndReturn(1)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        time.time().AndReturn(65)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        self.mox.ReplayAll()
+        self.exception_tuple = ('unexpected',) * 2
+        self.exception_generator()
+        self.addCleanup(self.stubs.UnsetAll)
+
+    def test_exc_retrier_same_10exc_2min_gives_2logs(self):
+        self.common_exc_retrier_init()
+        time.time().AndReturn(1)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        time.time().AndReturn(12)
+        time.time().AndReturn(23)
+        time.time().AndReturn(34)
+        time.time().AndReturn(45)
+        time.time().AndReturn(106)
+        logging.exception(mox.In('Unexpected exception occurred 5 time(s)'))
+        time.time().AndReturn(117)
+        time.time().AndReturn(128)
+        time.time().AndReturn(139)
+        time.time().AndReturn(150)
+        self.mox.ReplayAll()
+        self.exception_tuple = ('unexpected',) * 10
+        self.exception_generator()
+        self.addCleanup(self.stubs.UnsetAll)
+
+    def test_exc_retrier_mixed_4exc_1min_gives_2logs(self):
+        self.common_exc_retrier_init()
+        time.time().AndReturn(1)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        time.time().AndReturn(10)
+        time.time().AndReturn(20)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        time.time().AndReturn(30)
+        self.mox.ReplayAll()
+        self.exception_tuple = ('unexpected 1', 'unexpected 1',
+                                'unexpected 2', 'unexpected 2')
+        self.exception_generator()
+        self.addCleanup(self.stubs.UnsetAll)
+
+    def test_exc_retrier_mixed_4exc_2min_gives_2logs(self):
+        self.common_exc_retrier_init()
+        time.time().AndReturn(1)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        time.time().AndReturn(10)
+        time.time().AndReturn(100)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        time.time().AndReturn(110)
+        self.mox.ReplayAll()
+        self.exception_tuple = ('unexpected 1', 'unexpected 1',
+                                'unexpected 2', 'unexpected 2')
+        self.exception_generator()
+        self.addCleanup(self.stubs.UnsetAll)
+
+    def test_exc_retrier_mixed_4exc_2min_gives_3logs(self):
+        self.common_exc_retrier_init()
+        time.time().AndReturn(1)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        time.time().AndReturn(10)
+        time.time().AndReturn(100)
+        logging.exception(mox.In('Unexpected exception occurred 2 time(s)'))
+        time.time().AndReturn(110)
+        logging.exception(mox.In('Unexpected exception occurred 1 time(s)'))
+        self.mox.ReplayAll()
+        self.exception_tuple = ('unexpected 1', 'unexpected 1',
+                                'unexpected 1', 'unexpected 2')
+        self.exception_generator()
+        self.addCleanup(self.stubs.UnsetAll)
