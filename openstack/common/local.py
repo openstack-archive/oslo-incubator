7ce6eda78477422280641fee968c12acc7337eb1
Lance Bragstad <ldbragst@us.ibm.com>
2013-06-28 02:53:24 +0000

    Modify local.py to not be dependent on Eventlet
    This checks to see if Eventlet is being used for thread local
    storage. If not then use threading.local instead of Eventlet's
    corolocal.local. This will allow projects to not be dependent on
    Eventlet by pulling in modules from Oslo that require local.py.
    
    Change-Id: Ib544be1485823f6c619312fdee5a04031f48bbb4

--- openstack/common/local.py
+++ openstack/common/local.py
@@ -17,14 +17,56 @@
 
 """Greenthread local storage of variables using weak references"""
 
+import thread
 import weakref
 
-from eventlet import corolocal
+# NOTE(ldbragst): Define store, weak_store, strong_store as global like the
+# previous implementation
+# NOTE(mikal): the name "store" should be deprecated in the future
+store = None
+# A "weak" store uses weak references and allows an object to fall out of scope
+# when it falls out of scope in the code that uses the thread local storage. A
+# "strong" store will hold a reference to the object so that it never falls out
+# of scope.
+weak_store = None
+strong_store = None
+
+# NOTE(ldbragst): Here we are checking if we are using Eventlet,
+# if not then use threading.local for thread storage.
+try:
+    from eventlet import patcher
+    if patcher.is_monkey_patched(thread):
+        from eventlet import corolocal
+        # NOTE(ldbragst): If we are using Eventlet, then define a class
+        # that uses corolocal.local for weak_store and strong_store.
 
 
-class WeakLocal(corolocal.local):
+        class EventletWeakLocal(WeakLocal, corolocal.local):
+            pass
+
+
+        store = eventlet.corolocal.local
+        strong_store = eventlet.corolocal.local
+        weak_store = EventletWeakLocal()
+
+    else:
+        # NOTE(ldbragst): We aren't using Eventlet so import threading.local
+        # from Python standard library.
+        from threading import local
+        store = threading.local
+        strong_store = threading.local
+except ImportError:
+    from threading import local
+    store = threading.local
+    strong_store = threading.local
+
+
+class WeakLocal(object):
+    """This mixin class will be inherited by ThreadingWeakLocal and
+       EventletWeakLocal
+    """
     def __getattribute__(self, attr):
-        rval = corolocal.local.__getattribute__(self, attr)
+        rval = super(WeakLocal, self).__getattribute__(self, attr)
         if rval:
             # NOTE(mikal): this bit is confusing. What is stored is a weak
             # reference, not the value itself. We therefore need to lookup
@@ -34,15 +76,12 @@ class WeakLocal(corolocal.local):
 
     def __setattr__(self, attr, value):
         value = weakref.ref(value)
-        return corolocal.local.__setattr__(self, attr, value)
+        return super(WeakLocal, self).__setattr__(self, attr, value)
 
 
-# NOTE(mikal): the name "store" should be deprecated in the future
-store = WeakLocal()
+class ThreadingWeakLocal(WeakLocal, strong_store):
+    # NOTE(ldbragst): Only set weak_store to ThreadingWeakLocal if it is not
+    # previously defined from instantiating EventletWeakLocal class.
+    if not weak_store:
+        weak_store = ThreadingWeakLocal()
 
-# A "weak" store uses weak references and allows an object to fall out of scope
-# when it falls out of scope in the code that uses the thread local storage. A
-# "strong" store will hold a reference to the object so that it never falls out
-# of scope.
-weak_store = WeakLocal()
-strong_store = corolocal.local
