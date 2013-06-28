462fd2fddca1f9af6081af99e003859770fdb26b
Lance Bragstad <ldbragst@us.ibm.com>
2013-06-28 02:53:24 +0000

    Modify local.py to not be dependent on Eventlet
    This checks to see if Eventlet is being used for thread local
    storage. If not then use threading.local instead of Eventlet's
    corolocal.local. This will allow projects to not be dependent on
    Eventlet by pulling in modules from Oslo that require local.py.
    
    Change-Id: Ib544be1485823f6c619312fdee5a04031f48bbb4

--- openstack/common/local.py
+++ openstack/common/local.py
@@ -17,32 +17,73 @@
 
 """Greenthread local storage of variables using weak references"""
 
+import threading
 import weakref
 
-from eventlet import corolocal
+try:
+    import eventlet
+    from eventlet import corolocal
+    from eventlet import patcher
+except ImportError:
+    eventlet = None
 
 
-class WeakLocal(corolocal.local):
-    def __getattribute__(self, attr):
-        rval = corolocal.local.__getattribute__(self, attr)
-        if rval:
-            # NOTE(mikal): this bit is confusing. What is stored is a weak
-            # reference, not the value itself. We therefore need to lookup
-            # the weak reference and return the inner value here.
-            rval = rval()
-        return rval
+class LocalStoreBuilder(object):
+    """This object carries the logic to dynamically instantiate weak_local
+       depending on the implementation. This helps our test cases by allowing
+       us to specify which implementation to build on by passing in modules
+       stored as variables.
+    """
 
-    def __setattr__(self, attr, value):
-        value = weakref.ref(value)
-        return corolocal.local.__setattr__(self, attr, value)
+    def __init__(self, threading_mod, eventlet_mod=None):
 
+        # NOTE(ldbragst): If eventlet_mod is passed in we can assume that it
+        # it was imported properly and that we can set strong_store to
+        # the corolocal.local implementation, and build weak_store
+        # accordingly.
+        if eventlet_mod:
+            self.strong_store = corolocal.local
+            self.weak_store = self._build_weak_store()
+        else:
+            # NOTE(ldbragst): Here we need to address the unit test concerns
+            # by knowing that just because eventlet_mod = None doesn't mean
+            # that it isn't installed on the system. So if it is installed,
+            # grab the original threading implementation for the Python
+            # standard library, using threading.local for strong_store and
+            # build weak_store accordingly.
+            if patcher.is_monkey_patched(threading_mod):
+                threading_mod = patcher.original(threading_mod)
+            self.strong_store = threading_mod.local
+            self.weak_store = self._build_weak_store()
 
-# NOTE(mikal): the name "store" should be deprecated in the future
-store = WeakLocal()
+    def _build_weak_store(self):
+        """Private method that builds a weak_store class dynamically depending
+           on self.strong_store, which can either be Eventlet based or
+           Python standard library threading.local.
+        """
 
-# A "weak" store uses weak references and allows an object to fall out of scope
-# when it falls out of scope in the code that uses the thread local storage. A
-# "strong" store will hold a reference to the object so that it never falls out
-# of scope.
-weak_store = WeakLocal()
-strong_store = corolocal.local
+        def __getattribute__(self, attr):
+            rval = self.strong_store.__getattribute__(self, attr)  # noqa
+            if rval:
+                # NOTE(mikal): this bit is confusing. What is stored is a weak
+                # reference, not the value itself. We therefore need to lookup
+                # the weak reference and return the inner value here.
+                rval = rval()
+            return rval
+
+        def __setattr__(self, attr, value):
+            value = weakref.ref(value)
+            return self.strong_store.__setattr__(self, attr, value)  # noqa
+
+        # NOTE(ldbragst): dynamically building weak_store accoring to
+        # self.strong_store
+        weak_store = type('WeakLocal', (self.strong_store,),
+                          dict(strong_store=self.strong_store))
+        weak_store.__getattribute__ = __getattribute__
+        weak_store.__setattr__ = __setattr__
+        return weak_store
+
+builder = LocalStoreBuilder(threading, eventlet)
+store = builder.weak_store
+weak_store = builder.weak_store
+strong_store = builder.strong_store
