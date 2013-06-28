a2196a24af0027afb8783de6203d0a64e9664c26
Lance Bragstad <ldbragst@us.ibm.com>
2013-06-28 02:53:24 +0000

    Modify local.py to not be dependent on Eventlet
    This checks to see if Eventlet is being used for thread local
    storage. If not then use threading.local instead of Eventlet's
    corolocal.local. This will allow projects to not be dependent on
    Eventlet by pulling in modules from Oslo that require local.py.
    
    Change-Id: Ib544be1485823f6c619312fdee5a04031f48bbb4

--- tests/unit/test_local.py
+++ tests/unit/test_local.py
@@ -16,6 +16,7 @@
 #    under the License.
 
 import eventlet
+import threading
 
 from openstack.common import local
 from tests import utils
@@ -31,10 +32,41 @@ class LocalStoreTestCase(utils.BaseTestCase):
     v2 = Dict(a='2')
     v3 = Dict(a='3')
 
+    def setUp(self):
+        super(LocalStoreTestCase, self).setUp()
+
+        self.eventlet_builder = local.LocalStoreBuilder(eventlet)  # noqa
+        self.threading_builder = local.LocalStoreBuilder()         # noqa
+
+    def test_local_store_builder_eventlet(self):
+        """Make sure LocalStoreBuilder object builds an eventlet local store
+           object.
+        """
+        self.assertIsInstance(type(self.eventlet_builder), object)
+
+    def test_local_store_builder_threading(self):
+        """Make sure LocalStoreBuilder object builds an eventlet local store
+           object.
+        """
+        self.assertIsInstance(type(self.threading_builder), object)
+
+    def test_local_store_builder_strong_store_eventlet(self):
+        """Test to make sure our LocalStoreBuilder object's strong_store is in
+           fact the same as eventlet.corolocal.local when using Eventlet.
+        """
+        self.assertEqual(self.eventlet_builder.strong_store,
+                         eventlet.corolocal.local)
+
+    def test_local_store_builder_strong_store_threading(self):
+        """Test to make sure our LocalStoreBuilder object's strong_store is in
+           fact the same as threading.local when using threading from Python
+           standard library.
+        """
+        self.assertEqual(self.threading_builder.strong_store, threading.local)
+
     def test_thread_unique_storage(self):
         """Make sure local store holds thread specific values."""
         expected_set = []
-        local.store.a = self.v1
 
         def do_something():
             local.store.a = self.v2
@@ -46,6 +78,33 @@ class LocalStoreTestCase(utils.BaseTestCase):
 
         eventlet.spawn(do_something).wait()
         eventlet.spawn(do_something2).wait()
+        local.store.a = self.v1
+        expected_set.append(getattr(local.store, 'a'))
+
+        self.assertTrue(self.v1 in expected_set)
+        self.assertTrue(self.v2 in expected_set)
+        self.assertTrue(self.v3 in expected_set)
+
+    def test_threading_unique_storage(self):
+        """Test thread storage using threading library from the Python
+           standard library, instead of Eventlet
+        """
+        expected_set = []
+
+        def do_something():
+            local.store.a = self.v2
+            expected_set.append(getattr(local.store, 'a'))
+
+        def do_something2():
+            local.store.a = self.v3
+            expected_set.append(getattr(local.store, 'a'))
+
+        t1 = threading.Thread(target=do_something)
+        t1.start()
+        t2 = threading.Thread(target=do_something2)
+        t2.start()
+
+        local.store.a = self.v1
         expected_set.append(getattr(local.store, 'a'))
 
         self.assertTrue(self.v1 in expected_set)
