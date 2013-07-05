ac087d7c243f7a6e0ecda1b4026cc8a5e6b111e7
Julia Varlamova <jvarlamova@mirantis.com>
2013-07-05 15:11:56 +0400

    Add tests for cinder/common/sqlalchemyutils.py
    Add test cases:
    - test_paginate_query_no_pagination_no_sort_dirs - tests
    sorting only
    - test_paginate_query_no_pagination - tests sorting only
    - test_paginate_query_attribute_error
    - test_paginate_query_assertion_error
    - test_paginate_query_assertion_error_2
    - test_paginate_query - tests both sorting and pagination
    - test_paginate_query_value_error - is skipped now
    
    Change-Id: Ibec636bf23872d221bac17f6514fc97bd09c4e86

--- tests/unit/db/sqlalchemy/test_utils.py
+++ tests/unit/db/sqlalchemy/test_utils.py
@@ -15,6 +15,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import sqlalchemy
+import testtools
+
 from sqlalchemy.dialects import mysql
 from sqlalchemy import Boolean, Index, Integer, DateTime, String
 from sqlalchemy import MetaData, Table, Column
@@ -25,12 +28,126 @@ from sqlalchemy.types import UserDefinedType, NullType
 from openstack.common.db.sqlalchemy import utils
 from openstack.common import exception
 from tests.unit.db.sqlalchemy import test_migrations
+from tests import utils as testutils
 
 
 class CustomType(UserDefinedType):
     """Dummy column type for testing unsupported types."""
     def get_col_spec(self):
         return "CustomType"
+
+
+class FakeModel(object):
+    def __init__(self, values):
+        self.values = values
+
+    def __getattr__(self, name):
+        try:
+            value = self.values[name]
+        except KeyError:
+            raise AttributeError(name)
+        return value
+
+    def __getitem__(self, key):
+        if key in self.values:
+            return self.values[key]
+        else:
+            raise NotImplementedError()
+
+    def __repr__(self):
+        return '<FakeModel: %s>' % self.values
+
+
+class TestPaginateQuery(testutils.BaseTestCase):
+    def setUp(self):
+        super(TestPaginateQuery, self).setUp()
+        self.query = self.mox.CreateMockAnything()
+        self.mox.StubOutWithMock(sqlalchemy, 'asc')
+        self.mox.StubOutWithMock(sqlalchemy, 'desc')
+        self.marker = FakeModel({
+            'user_id': 'user',
+            'project_id': 'p',
+            'snapshot_id': 's',
+        })
+        self.model = FakeModel({
+            'user_id': 'user',
+            'project_id': 'project',
+            'snapshot_id': 'snapshot',
+        })
+
+    def test_paginate_query_no_pagination_no_sort_dirs(self):
+        sqlalchemy.asc('user').AndReturn('asc_3')
+        self.query.order_by('asc_3').AndReturn(self.query)
+        sqlalchemy.asc('project').AndReturn('asc_2')
+        self.query.order_by('asc_2').AndReturn(self.query)
+        sqlalchemy.asc('snapshot').AndReturn('asc_1')
+        self.query.order_by('asc_1').AndReturn(self.query)
+        self.query.limit(5).AndReturn(self.query)
+        self.mox.ReplayAll()
+        utils.paginate_query(self.query, self.model, 5,
+                             ['user_id', 'project_id', 'snapshot_id'])
+
+    def test_paginate_query_no_pagination(self):
+        sqlalchemy.asc('user').AndReturn('asc')
+        self.query.order_by('asc').AndReturn(self.query)
+        sqlalchemy.desc('project').AndReturn('desc')
+        self.query.order_by('desc').AndReturn(self.query)
+        self.query.limit(5).AndReturn(self.query)
+        self.mox.ReplayAll()
+        utils.paginate_query(self.query, self.model, 5,
+                             ['user_id', 'project_id'],
+                             sort_dirs=['asc', 'desc'])
+
+    def test_paginate_query_attribute_error(self):
+        sqlalchemy.asc('user').AndReturn('asc')
+        self.query.order_by('asc').AndReturn(self.query)
+        self.mox.ReplayAll()
+        self.assertRaises(utils.InvalidSortKey,
+                          utils.paginate_query, self.query,
+                          self.model, 5, ['user_id', 'non-existent key'])
+
+    def test_paginate_query_assertion_error(self):
+        self.mox.ReplayAll()
+        self.assertRaises(AssertionError,
+                          utils.paginate_query, self.query,
+                          self.model, 5, ['user_id'],
+                          marker=self.marker,
+                          sort_dir='asc', sort_dirs=['asc'])
+
+    def test_paginate_query_assertion_error_2(self):
+        self.mox.ReplayAll()
+        self.assertRaises(AssertionError,
+                          utils.paginate_query, self.query,
+                          self.model, 5, ['user_id'],
+                          marker=self.marker,
+                          sort_dir=None, sort_dirs=['asc', 'desk'])
+
+    def test_paginate_query(self):
+        sqlalchemy.asc('user').AndReturn('asc_1')
+        self.query.order_by('asc_1').AndReturn(self.query)
+        sqlalchemy.desc('project').AndReturn('desc_1')
+        self.query.order_by('desc_1').AndReturn(self.query)
+        self.mox.StubOutWithMock(sqlalchemy.sql, 'and_')
+        sqlalchemy.sql.and_(False).AndReturn('some_crit')
+        sqlalchemy.sql.and_(True, False).AndReturn('another_crit')
+        self.mox.StubOutWithMock(sqlalchemy.sql, 'or_')
+        sqlalchemy.sql.or_('some_crit', 'another_crit').AndReturn('some_f')
+        self.query.filter('some_f').AndReturn(self.query)
+        self.query.limit(5).AndReturn(self.query)
+        self.mox.ReplayAll()
+        utils.paginate_query(self.query, self.model, 5,
+                             ['user_id', 'project_id'],
+                             marker=self.marker,
+                             sort_dirs=['asc', 'desc'])
+
+    @testtools.skip('The bug is not fixed: ValueError never raises')
+    def test_paginate_query_value_error(self):
+        sqlalchemy.asc('user').AndReturn('asc_1')
+        self.query.order_by('asc_1').AndReturn(self.query)
+        self.mox.ReplayAll()
+        self.assertRaises(ValueError, utils.paginate_query,
+                          self.query, self.model, 5, ['user_id', 'project_id'],
+                          marker=self.marker, sort_dirs=['asc', 'mixed'])
 
 
 class TestMigrationUtils(test_migrations.BaseMigrationTestCase):
