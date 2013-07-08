824abd2826cbc28183cc87df11818ea1c39e2b98
Solly Ross <sross@redhat.com>
2013-07-08 19:40:34 -0400

    Introduces Openstack-Specific Report Utilities
    Introduces classes representing various Openstack-specific
    views, data models, and generators.  This commit also introduces
    the folder for generators, since the only predefined generators are
    those which are Openstack-specific.
    
    Change-Id: Ice56cb63b2a9807b15642d7cfa4f24a507c70266
    Implements: bp guru-meditation-report

--- openstack/common/report/views/text/generic.py
+++ openstack/common/report/views/text/generic.py
@@ -76,38 +76,75 @@ class KeyValueView(object):
             key =
               serialize(item)
 
-    :param str indent_str: the string used to represent
-                           one "indent"
+    :param str indent_str: the string used to represent one "indent"
+    :param str key_sep: the separator to use between keys and values
+    :param str dict_sep: the separator to use after a dictionary root key
+    :param str list_sep: the separator to use after a list root key
+    :param str anon_dict: the "key" to use when there is a dict in a list
+                          (does not automatically use the dict separator)
+    :param before_dict: content to place on the line(s) before the a dict
+                        root key (use None to avoid inserting an extra line)
+    :type before_dict: str or None
+    :param before_list: content to place on the line(s) before the a list
+                        root key (use None to avoid inserting an extra line)
+    :type before_list: str or None
     """
 
-    def __init__(self, indent_str='  '):
+    def __init__(self,
+                 indent_str='  ',
+                 key_sep=' = ',
+                 dict_sep=' = ',
+                 list_sep=' = ',
+                 anon_dict='[dict]',
+                 before_dict=None,
+                 before_list=None):
         self.indent_str = indent_str
+        self.key_sep = key_sep
+        self.dict_sep = dict_sep
+        self.list_sep = list_sep
+        self.anon_dict = anon_dict
+        self.before_dict = before_dict
+        self.before_list = before_list
 
     def __call__(self, model):
         def serialize(root, rootkey, indent):
             res = []
             if rootkey is not None:
-                res.append((self.indent_str * indent) + rootkey + " = ")
+                res.append((self.indent_str * indent) + rootkey)
 
             if isinstance(root, col.Mapping):
                 if rootkey is None and indent > 0:
-                    res.append((self.indent_str * indent) + '[dict]')
+                    res.append((self.indent_str * indent) + self.anon_dict)
+                elif rootkey is not None:
+                    res[0] += self.dict_sep
+                    if self.before_dict is not None:
+                        res.insert(0, self.before_dict)
 
                 for key in root:
                     res.extend(serialize(root[key], key, indent + 1))
             elif (isinstance(root, col.Sequence) and
                     not isinstance(root, basestring)):
+                if rootkey is not None:
+                    res[0] += self.list_sep
+                    if self.before_list is not None:
+                        res.insert(0, self.before_list)
+
                 for val in root:
                     res.extend(serialize(val, None, indent + 1))
             else:
                 str_root = str(root)
                 if '\n' in str_root:
+                    # we are in a submodel
+                    if rootkey is not None:
+                        res[0] += self.dict_sep
+
                     list_root = [(self.indent_str * (indent + 1)) + line
                                  for line in str_root.split('\n')]
                     res.extend(list_root)
                 else:
+                    # just a normal key or list entry
                     try:
-                        res[0] += str_root
+                        res[0] += self.key_sep + str_root
                     except IndexError:
                         res = [(self.indent_str * indent) + str_root]
 
