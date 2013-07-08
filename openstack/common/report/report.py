07325b189db5e75896be4a7d6080e4b2e36eecce
Solly Ross <sross@redhat.com>
2013-07-08 14:10:54 -0400

    Introduces Report Views
    Introduces classes representing generic view types for each of the
    standard report serialization types: text (human-readable), JSON, and
    XML.  Each category of view has at least a basic key-value view.
    Additionally, a view which utilizes the Jinja templating system
    (http://jinja.pocoo.org/) is available for easy creation of more
    complex views.  Finally, a method of specifying several views for
    a model, each associated with a different serialization type, was
    introduced.
    
    Change-Id: I30b127d67587127e5f08317fe06fa81246897ed6
    Implements: bp guru-meditation-report

--- openstack/common/report/report.py
+++ openstack/common/report/report.py
@@ -21,6 +21,8 @@ reports and report sections.  All reports take the
 form of a report class containing various report sections.
 """
 
+import openstack.common.report.views.text.header as header_views
+
 
 class BasicReport(object):
     """A Basic Report
@@ -84,7 +86,8 @@ class ReportSection(object):
     on the resulting model.
 
     .. seealso::
-       Class :class:`BasicReport`,
+
+       Class :class:`BasicReport`
            :func:`BasicReport.add_section`
 
     :param view: the top-level view for this section
@@ -99,3 +102,90 @@ class ReportSection(object):
 
     def __str__(self):
         return self.view(self.generator())
+
+
+class ReportOfType(BasicReport):
+    """A Report of a Certain Type
+
+    A ReportOfType has a predefined type associated with it.
+    This type is automatically propogated down to the each of
+    the sections upon serialization by wrapping the generator
+    for each section.
+
+    .. seealso::
+
+       Class :class:`openstack.common.report.models.with_default_view.ModelWithDefaultView` # noqa
+          (the entire class)
+
+       Class :class:`openstack.common.report.models.base.ReportModel`
+           :func:`openstack.common.report.models.base.ReportModel.set_current_view_type` # noqa
+
+    :param str tp: the type of the report
+    """
+
+    def __init__(self, tp):
+        self.output_type = tp
+        super(ReportOfType, self).__init__()
+
+    def add_section(self, view, generator, index=None):
+        def with_type(gen):
+            def newgen():
+                res = gen()
+                try:
+                    res.set_current_view_type(self.output_type)
+                except AttributeError:
+                    pass
+
+                return res
+            return newgen
+
+        super(ReportOfType, self).add_section(
+            view,
+            with_type(generator),
+            index
+        )
+
+
+class TextReport(ReportOfType):
+    """A Human-Readable Text Report
+
+    This class defines a report that is designed to be read by a human
+    being.  It has nice section headers, and a formatted title.
+
+    :param str name: the title of the report
+    """
+
+    def __init__(self, name):
+        super(TextReport, self).__init__('text')
+        self.name = name
+        # add a title with a generator that creates an empty result model
+        self.add_section(name, lambda: ('|' * 72) + "\n\n")
+
+    def add_section(self, heading, generator, index=None):
+        """Add a section to the report
+
+        This method adds a section with the given title, and
+        generator to the report.  An index may be specified to
+        insert the section at a given location in the list;
+        If no index is specified, the section is appended to the
+        list.  The view is called on the model which results from
+        the generator when the report is run.  A generator is simply
+        a method or callable object which takes no arguments and
+        returns a :class:`openstack.common.report.models.base.ReportModel`
+        or similar object.
+
+        The model is told to serialize as text (if possible) at serialization
+        time by wrapping the generator.  The view model's attached view
+        (if any) is wrapped in a
+        :class:`openstack.common.report.views.text.header.TitledView`
+
+        :param str heading: the title for the section
+        :param generator: the method or class which generates the model
+        :param index: the index at which to insert the section
+                      (or None to append)
+        :type index: int or None
+        """
+
+        super(TextReport, self).add_section(header_views.TitledView(heading),
+                                            generator,
+                                            index)
