747f7dd395c6eed3d46b0dd62f8071885330bd55
Chuck Short <chuck.short@canonical.com>
2013-07-12 15:42:52 -0400

    python3: Import translation layer
    Python3 reorganized the standard library and moved several functions
    to different modules. Six provides a consistent interface
    to them through the fake six.moves module.
    
    However, the urlparse, urllib2, etc modules have been combined
    into one module which Six does not support so do it the old
    fashioned way.
    
    Change-Id: I9a865ef1fc4de6f94cc3ccf26db0483f2bdc13df
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

---
+++ tests/unit/test_compat.py
@@ -0,0 +1,47 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 Canonical Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+from openstack.common.py3kcompat import urlutils
+from tests import utils
+
+
+class CompatTestCase(utils.BaseTestCase):
+    def test_urlencode(self):
+        fake = 'fake'
+        result = urlutils.urlencode({'Fake': fake})
+        self.assertEquals(result, 'Fake=fake')
+
+    def test_urlquote(self):
+        url = "/~fake"
+        result = urlutils.quote(url)
+        self.assertEquals(result, '/%7Efake')
+
+    def test_urlparse(self):
+        url = 'http://www.yahoo.com'
+        result = urlutils.urlparse(url)
+        self.assertEquals(result.scheme, 'http')
+
+    def test_urlsplit(self):
+        url = 'http://www.yahoo.com'
+        result = urlutils.urlsplit(url)
+        self.assertEquals(result.scheme, 'http')
+
+    def test_urlunsplit(self):
+        url = "http://www.yahoo.com"
+        result = urlutils.urlunsplit(urlutils.urlsplit(url))
+        self.assertEquals(result, 'http://www.yahoo.com')
