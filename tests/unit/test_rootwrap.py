d7e3c7954afb2ae56aba87913db70d36990d6fcb
Julien Danjou <julien@danjou.info>
2013-07-29 17:05:49 +0200

    rootwrap: improve Python 3 compatibility
    Change-Id: I83a7aa7e7ebecb287b0f25e5bf510c10779c4ddf

--- tests/unit/test_rootwrap.py
+++ tests/unit/test_rootwrap.py
@@ -14,10 +14,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import ConfigParser
 import logging
 import logging.handlers
 import os
+from six.moves import configparser  # noqa
 import subprocess
 import uuid
 
@@ -334,10 +334,10 @@ class RootwrapTestCase(utils.BaseTestCase):
         self.assertTrue(filtermatch is self.filters[-1])
 
     def test_RootwrapConfig(self):
-        raw = ConfigParser.RawConfigParser()
+        raw = configparser.RawConfigParser()
 
-        # Empty config should raise ConfigParser.Error
-        self.assertRaises(ConfigParser.Error, wrapper.RootwrapConfig, raw)
+        # Empty config should raise configparser.Error
+        self.assertRaises(configparser.Error, wrapper.RootwrapConfig, raw)
 
         # Check default values
         raw.set('DEFAULT', 'filters_path', '/a,/b')
