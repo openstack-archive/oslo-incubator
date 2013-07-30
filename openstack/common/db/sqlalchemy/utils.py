c3e7b9af13e5848665e0c09d30b4e828dbf854a9
Julien Danjou <julien@danjou.info>
2013-07-30 12:51:54 +0200

    exception: remove
    This patch drops the obsolete openstack.common.exception module.
    
    Change-Id: Ia8b2b29bd443233def324e97de7342c2634bccff

--- openstack/common/db/sqlalchemy/utils.py
+++ openstack/common/db/sqlalchemy/utils.py
@@ -38,7 +38,6 @@ from sqlalchemy.types import NullType
 
 from openstack.common.gettextutils import _  # noqa
 
-from openstack.common import exception
 from openstack.common import log as logging
 from openstack.common import timeutils
 
@@ -182,13 +181,13 @@ def _get_not_supported_column(col_name_col_instance, column_name):
         msg = _("Please specify column %s in col_name_col_instance "
                 "param. It is required because column has unsupported "
                 "type by sqlite).")
-        raise exception.OpenstackException(message=msg % column_name)
+        raise Exception(message=msg % column_name)
 
     if not isinstance(column, Column):
         msg = _("col_name_col_instance param has wrong type of "
                 "column instance for column %s It should be instance "
                 "of sqlalchemy.Column.")
-        raise exception.OpenstackException(message=msg % column_name)
+        raise Exception(message=msg % column_name)
     return column
 
 
@@ -286,7 +285,7 @@ def _get_default_deleted_value(table):
         return 0
     if isinstance(table.c.id.type, String):
         return ""
-    raise exception.OpenstackException(
+    raise Exception(
         message=_("Unsupported id columns type"))
 
 
