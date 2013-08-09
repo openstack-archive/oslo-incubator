2766bb9bd35aee0aa442b96a2878f5e6e7e568d1
Pádraig Brady <pbrady@redhat.com>
2013-08-09 12:52:30 +0100

    parameterize fileutils removal functions
    Currently delete_if_exists() and consequently
    remove_path_on_error() only support single files.
    So to handle dirs, or trees or higher level operations
    it's useful to be able to specify how to remove.
    
    * openstack/common/fileutils.py (delete_if_exists):
    Allow passing a removal function.
    (remove_path_on_error): Likewise.
    * tests/unit/test_fileutils.py (test_dir_present):
    Demonstrate/test removal of empty dirs.
    (test_remove_dir): Likewise.
    (test_remove): Ensure the remove param is honored.
    
    Change-Id: I0ea2498048f09af5f1b9c1bddba677c5813fb2f8

--- openstack/common/fileutils.py
+++ openstack/common/fileutils.py
@@ -69,14 +69,18 @@ def read_cached_file(filename, force_reload=False):
     return (reloaded, cache_info['data'])
 
 
-def delete_if_exists(path):
+def delete_if_exists(path, remove=None):
     """Delete a file, but ignore file not found error.
 
     :param path: File to delete
+    :param remove: Optional function to remove passed path
     """
 
+    if remove is Nonw:
+        remove = os.unlink()
+
     try:
-        os.unlink(path)
+        remove(path)
     except OSError as e:
         if e.errno == errno.ENOENT:
             return
@@ -85,17 +89,22 @@ def delete_if_exists(path):
 
 
 @contextlib.contextmanager
-def remove_path_on_error(path):
+def remove_path_on_error(path, remove=None):
     """Protect code that wants to operate on PATH atomically.
     Any exception will cause PATH to be removed.
 
     :param path: File to work with
+    :param remove: Optional function to remove passed path
     """
+
+    if remove is None:
+        remove = delete_if_exists
+
     try:
         yield
     except Exception:
         with excutils.save_and_reraise_exception():
-            delete_if_exists(path)
+            remove(path)
 
 
 def file_open(*args, **kwargs):
