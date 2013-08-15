8edbde3912c752f1336f7a0a0dac2bd3be238c95
Sergey Kraynev <skraynev@mirantis.com>
2013-08-15 04:52:36 -0400

    Add fixture for using create_tempfiles
    This methods is moved from old BaseTestCase class
    in tests.utils module.
    
    Blueprint: common-unit-tests
    Change-Id: Ia51d416b2828a95fbb60d019cc92c4c8288bccc3

---
+++ openstack/common/fixture/create_tempfiles.py
@@ -0,0 +1,41 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 Mirantis, Inc.
+# Copyright 2013 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import fixtures
+import os
+import tempfile
+
+
+class CreateTempfiles(fixtures.Fixture):
+
+    def setUp(self):
+        super(CreateTempfiles, self).setUp()
+
+    def create_tempfiles(self, files, ext='.conf'):
+        tempfiles = []
+        for (basename, contents) in files:
+            if not os.path.isabs(basename):
+                (fd, path) = tempfile.mkstemp(prefix=basename, suffix=ext)
+            else:
+                path = basename + ext
+                fd = os.open(path, os.O_CREAT | os.O_WRONLY)
+            tempfiles.append(path)
+            try:
+                os.write(fd, contents)
+            finally:
+                os.close(fd)
+        return tempfiles
