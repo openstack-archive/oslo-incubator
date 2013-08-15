20f025edc2d1a83eb08dadf0f2511405970f10d4
Sergey Kraynev <skraynev@mirantis.com>
2013-08-15 04:52:36 -0400

    Add fixture for using create_tempfiles
    This methods is moved from old BaseTestCase class
    in tests.utils module.
    
    Blueprint: common-unit-tests
    Change-Id: Ia51d416b2828a95fbb60d019cc92c4c8288bccc3

---
+++ tests/unit/fixture/test_create_tempfiles.py
@@ -0,0 +1,64 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 Mirantis, Inc.
+# Copyright 2013 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os
+
+from openstack.common.fixture import create_tempfiles
+from tests.utils import BaseTestCase
+
+
+class CreateTempfilesTestCase(BaseTestCase):
+    def setUp(self):
+        super(CreateTempfilesTestCase, self).setUp()
+        tempfiles_fixture = self.useFixture(create_tempfiles.CreateTempfiles())
+        self.create_tempfiles = tempfiles_fixture.create_tempfiles
+
+    def test_files_with_abs_path(self):
+        files = [('/tmp/test1', 'testing123'),
+                 ('/tmp/test2', 'testing567')]
+        res = self.create_tempfiles(files)
+        self.assertTrue(os.path.exists(res[0]))
+        self.assertTrue(os.path.exists(res[1]))
+
+        for num in xrange(2):
+            fd = open(res[num], 'r')
+            ans = fd.read()
+            self.assertEquals(files[num][1], ans)
+            fd.close()
+
+    def test_files_without_abs_path(self):
+        files = [('test3', 'testing123'),
+                 ('test4', 'testing567')]
+        res = self.create_tempfiles(files)
+        self.assertTrue(os.path.exists(res[0]))
+        self.assertTrue(os.path.exists(res[1]))
+
+        for num in xrange(2):
+            fd = open(res[num], 'r')
+            ans = fd.read()
+            self.assertEquals(files[num][1], ans)
+            fd.close()
+
+    def test_files_with_other_extension(self):
+        files = [('test5', 'testing123')]
+        res = self.create_tempfiles(files, ext='.txt')
+        self.assertTrue(os.path.exists(res[0]))
+
+        fd = open(res[0], 'r')
+        ans = fd.read()
+        self.assertEquals(files[0][1], ans)
+        fd.close()
