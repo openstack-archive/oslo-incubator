8961ddb928559d0fa0d5fabdabf8c78dc1b809ba
Julien Danjou <julien@danjou.info>
2013-08-23 15:45:42 +0200

    excutils: replace unicode by six.u
    This makes it more Python 3 compatible.
    
    Change-Id: If640e551905836124657c79ab624e5717a0033a6

--- openstack/common/excutils.py
+++ openstack/common/excutils.py
@@ -78,7 +78,7 @@ def forever_retry_uncaught_exceptions(infunc):
             try:
                 return infunc(*args, **kwargs)
             except Exception as exc:
-                this_exc_message = unicode(exc)
+                this_exc_message = six.u(exc)
                 if this_exc_message == last_exc_message:
                     exc_count += 1
                 else:
