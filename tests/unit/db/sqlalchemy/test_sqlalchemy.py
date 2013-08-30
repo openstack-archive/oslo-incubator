3eaff44302847dfb71a4d4e16ca2ffc337f43421
Sergey Kraynev <skraynev@mirantis.com>
2013-08-30 10:55:52 -0400

    Replace using tests.utils part3
    It is the last step to replace using tests.utils with
    stack.common.test.
    In this patch database tests is changed for using
    openstack.common.test and added needed fixtures in test classes.
    
    Blueprint: common-unit-tests
    Change-Id: I9b360f9a8dc72bc3fb697e571555775bde10ef61

--- tests/unit/db/sqlalchemy/test_sqlalchemy.py
+++ tests/unit/db/sqlalchemy/test_sqlalchemy.py
@@ -25,8 +25,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from openstack.common.db import exception as db_exc
 from openstack.common.db.sqlalchemy import models
 from openstack.common.db.sqlalchemy import session
+from openstack.common import fileutils
+from openstack.common.fixture import config
+from openstack.common import test
 from tests.unit.db.sqlalchemy import base as test_base
-from tests import utils as test_utils
 
 
 BASE = declarative_base()
@@ -41,8 +43,14 @@ class TmpTable(BASE, models.ModelBase):
 
 class SessionParametersTestCase(test_base.DbTestCase):
 
+    def setUp(self):
+        super(SessionParametersTestCase, self).setUp()
+        configfixture = self.useFixture(config.Config())
+        self.conf = configfixture.conf
+        self.write_to_tempfile = fileutils.write_to_tempfile
+
     def test_deprecated_session_parameters(self):
-        paths = self.create_tempfiles([('test', """[DEFAULT]
+        path = self.write_to_tempfile("""[DEFAULT]
 sql_connection=x://y.z
 sql_min_pool_size=10
 sql_max_pool_size=20
@@ -51,8 +59,8 @@ sql_retry_interval=40
 sql_max_overflow=50
 sql_connection_debug=60
 sql_connection_trace=True
-""")])
-        self.conf(['--config-file', paths[0]])
+""")
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'x://y.z')
         self.assertEqual(self.conf.database.min_pool_size, 10)
         self.assertEqual(self.conf.database.max_pool_size, 20)
@@ -63,7 +71,7 @@ sql_connection_trace=True
         self.assertEqual(self.conf.database.connection_trace, True)
 
     def test_session_parameters(self):
-        paths = self.create_tempfiles([('test', """[database]
+        path = self.write_to_tempfile("""[database]
 connection=x://y.z
 min_pool_size=10
 max_pool_size=20
@@ -73,8 +81,8 @@ max_overflow=50
 connection_debug=60
 connection_trace=True
 pool_timeout=7
-""")])
-        self.conf(['--config-file', paths[0]])
+""")
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'x://y.z')
         self.assertEqual(self.conf.database.min_pool_size, 10)
         self.assertEqual(self.conf.database.max_pool_size, 20)
@@ -86,18 +94,17 @@ pool_timeout=7
         self.assertEqual(self.conf.database.pool_timeout, 7)
 
     def test_dbapi_database_deprecated_parameters(self):
-        paths = self.create_tempfiles([('test',
-                                        '[DATABASE]\n'
-                                        'sql_connection=fake_connection\n'
-                                        'sql_idle_timeout=100\n'
-                                        'sql_min_pool_size=99\n'
-                                        'sql_max_pool_size=199\n'
-                                        'sql_max_retries=22\n'
-                                        'reconnect_interval=17\n'
-                                        'sqlalchemy_max_overflow=101\n'
-                                        'sqlalchemy_pool_timeout=5\n'
-                                        )])
-        self.conf(['--config-file', paths[0]])
+        path = self.write_to_tempfile('[DATABASE]\n'
+                                      'sql_connection=fake_connection\n'
+                                      'sql_idle_timeout=100\n'
+                                      'sql_min_pool_size=99\n'
+                                      'sql_max_pool_size=199\n'
+                                      'sql_max_retries=22\n'
+                                      'reconnect_interval=17\n'
+                                      'sqlalchemy_max_overflow=101\n'
+                                      'sqlalchemy_pool_timeout=5\n'
+                                      )
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'fake_connection')
         self.assertEqual(self.conf.database.idle_timeout, 100)
         self.assertEqual(self.conf.database.min_pool_size, 99)
@@ -202,7 +209,7 @@ class RegexpFilterTestCase(test_base.DbTestCase):
         self._test_regexp_filter(u'♦', [])
 
 
-class SlaveBackendTestCase(test_utils.BaseTestCase):
+class SlaveBackendTestCase(test.BaseTestCase):
 
     def test_slave_engine_nomatch(self):
         default = session.CONF.database.connection
