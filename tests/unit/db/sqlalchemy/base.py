a5c177c997d06d706cedcb677d434a761985f07f
Pekelny Ilya <ipekelny@mirantis.com>
2013-09-13 18:39:06 +0300

    Database hook enabling traditional mode at MySQL
    Specific MySQL backend bug. The string greater than the declared on
    column silently truncated. Bug fixed — DBError raises when user trying to
    insert an overly long string.
    
    Fixes: bug 1224898
    Change-Id: I2960435c81d21f5a65ea7fa4dc5c100880cf6aef

--- tests/unit/db/sqlalchemy/base.py
+++ tests/unit/db/sqlalchemy/base.py
@@ -21,8 +21,13 @@ from openstack.common.db.sqlalchemy import session
 from tests import utils as test_utils
 
 
-class SqliteInMemoryFixture(fixtures.Fixture):
-    """SQLite in-memory DB recreated for each test case."""
+class DBFixture(fixtures.Fixture):
+
+    CONNECTION = None
+    DRIVER = None
+    USERNAME = None
+    DBNAME = None
+    PASSWORD = None
 
     def __init__(self):
         self.conf = cfg.CONF
@@ -30,18 +35,55 @@ class SqliteInMemoryFixture(fixtures.Fixture):
                              'openstack.common.db.sqlalchemy.session',
                              group='database')
 
-    def setUp(self):
-        super(SqliteInMemoryFixture, self).setUp()
+        conn_tmpl = '%(driver)s://%(usrname)s:%(passwd)s@localhost/%(dbname)s'
+        if not self.CONNECTION:
+            self.CONNECTION = conn_tmpl % {
+                    'driver': self.DRIVER,
+                    'usrname': self.USERNAME,
+                    'passwd': self.PASSWORD,
+                    'dbname': self.DBNAME
+                    }
 
-        self.conf.set_default('connection', "sqlite://", group='database')
+    def setUp(self):
+        super(DBFixture, self).setUp()
+
+        self.conf.set_default('connection', self.CONNECTION, group='database')
         self.addCleanup(self.conf.reset)
         self.addCleanup(session.cleanup)
 
 
+class SqliteInMemoryFixture(DBFixture):
+    """SQLite in-memory DB recreated for each test case."""
+
+    CONNECTION = "sqlite://"
+
+
+class MySQLFixture(DBFixture):
+    """MySQL specific fixture."""
+
+    DRIVER = 'mysql'
+    DBNAME = PASSWORD = USERNAME = 'openstack_citest'
+
+    def __init__(self):
+        super(MySQLFixture, self).__init__()
+
+        self.conf.import_opt('traditional_mode',
+                             'openstack.common.db.sqlalchemy.session',
+                             group='database')
+        self.conf.set_default('traditional_mode', True, group='database')
+
+
 class DbTestCase(test_utils.BaseTestCase):
-    """Base class for testing of DB code (uses in-memory SQLite DB fixture)."""
+    """Base class for testing of DB code."""
+
+    FIXTURE = SqliteInMemoryFixture
 
     def setUp(self):
         super(DbTestCase, self).setUp()
+        self.useFixture(self.FIXTURE())
 
-        self.useFixture(SqliteInMemoryFixture())
+
+class MySQLdbTestCase(DbTestCase):
+    """Test case to test MySQL specific features."""
+
+    FIXTURE = MySQLFixture
