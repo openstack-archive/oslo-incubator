a2d050a8fae589a8f330110889e87ffe8ab0bf11
Pekelny Ilya <ipekelny@mirantis.com>
2013-09-13 18:39:06 +0300

    Database hook enabling traditional mode at MySQL
    Specific MySQL backend bug. The string greater than the declared on
    column silently truncated. Bug fixed — DBError raises when user trying to
    insert an overly long string.
    
    Fixes: bug 1224898
    Change-Id: I2960435c81d21f5a65ea7fa4dc5c100880cf6aef

--- tests/unit/db/sqlalchemy/base.py
+++ tests/unit/db/sqlalchemy/base.py
@@ -15,14 +15,21 @@
 
 
 import fixtures
-from oslo.config import cfg
 
 from openstack.common.db.sqlalchemy import session
+from openstack.common.db.sqlalchemy.test_migrations import _is_backend_avail
+from oslo.config import cfg
 from tests import utils as test_utils
 
 
-class SqliteInMemoryFixture(fixtures.Fixture):
-    """SQLite in-memory DB recreated for each test case."""
+class DBFixture(fixtures.Fixture):
+    """Base database fixture. Allows to use various backend connection uri."""
+
+    CONNECTION = None
+    DRIVER = None
+    USERNAME = None
+    DBNAME = None
+    PASSWORD = None
 
     def __init__(self):
         self.conf = cfg.CONF
@@ -30,18 +37,76 @@ class SqliteInMemoryFixture(fixtures.Fixture):
                              'openstack.common.db.sqlalchemy.session',
                              group='database')
 
-    def setUp(self):
-        super(SqliteInMemoryFixture, self).setUp()
+        conn_tmpl = ('%(driver)s://%(username)s:'
+                     '%(passwd)s@localhost/%(dbname)s')
+        if not self.CONNECTION:
+            self.CONNECTION = conn_tmpl % {
+                'driver': self.DRIVER,
+                'username': self.USERNAME,
+                'passwd': self.PASSWORD,
+                'dbname': self.DBNAME
+            }
 
-        self.conf.set_default('connection', "sqlite://", group='database')
+    def setUp(self):
+        super(DBFixture, self).setUp()
+
+        # To prevent existance of prepared engine with wrong connection
+        session.cleanup()
+
+        self.conf.set_default('connection', self.CONNECTION, group='database')
         self.addCleanup(self.conf.reset)
         self.addCleanup(session.cleanup)
 
 
+class SqliteInMemoryFixture(DBFixture):
+    """SQLite in-memory DB recreated for each test case."""
+
+    CONNECTION = "sqlite://"
+
+
+class MySQLFixture(DBFixture):
+    """MySQL specific fixture
+
+    Be careful and use this fixture to run only MySQL specific
+    tests because create/drop and other actions can take extremly long time.
+    Please note, until test database does not dropped all of a tables and an
+    entries remain in the database. And database will be dropped only after
+    all the test cases will be completed. Those the cleanup process is
+    BaseTestCase subclasses business:
+
+    class FooTestCase(VariousBackendFixture):
+        def setUp(self):
+            ...
+            self.test_table.create()
+            self.sddCleanp(self.test_table.drop)
+    """
+
+    DRIVER = 'mysql'
+    DBNAME = PASSWORD = USERNAME = 'openstack_citest'
+
+
 class DbTestCase(test_utils.BaseTestCase):
-    """Base class for testing of DB code (uses in-memory SQLite DB fixture)."""
+    """Base class for testing of DB code."""
+
+    FIXTURE = SqliteInMemoryFixture
 
     def setUp(self):
         super(DbTestCase, self).setUp()
 
-        self.useFixture(SqliteInMemoryFixture())
+        credantials = (
+            self.FIXTURE.DRIVER,
+            self.FIXTURE.USERNAME,
+            self.FIXTURE.PASSWORD,
+            self.FIXTURE.DBNAME)
+
+        if not _is_backend_avail(*credantials):
+            msg = '%s backend is not available.' % self.FIXTURE.DRIVER
+            return self.skip(msg)
+
+        self.useFixture(self.FIXTURE())
+
+
+class MySQLdbTestCase(DbTestCase):
+    """Test case to test MySQL specific features."""
+
+    FIXTURE = MySQLFixture
