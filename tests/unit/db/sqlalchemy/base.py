046e4a91a8f01aeb5dc6b261d318161bc867569f
Pekelny Ilya <ipekelny@mirantis.com>
2013-09-16 19:15:12 +0300

    The ability to run tests at various backend
    Added test functionality to use different db backends. For correct
    operation required an environment variable contains root user
    db access uri.
    
    Blueprint: tests-given-db-backend
    Change-Id: Ic5d3cfe8764b3c04affe79088473255c17535e54

--- tests/unit/db/sqlalchemy/base.py
+++ tests/unit/db/sqlalchemy/base.py
@@ -13,35 +13,69 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from functools import wraps
+import os
 
 import fixtures
-from oslo.config import cfg
 
 from openstack.common.db.sqlalchemy import session
+from oslo.config import cfg
 from tests import utils as test_utils
 
 
-class SqliteInMemoryFixture(fixtures.Fixture):
-    """SQLite in-memory DB recreated for each test case."""
+class DbFixture(fixtures.Fixture):
+    """Database fixture.
+    Allows to run tests on various db backends, such as SQLite, MySQL and
+    PostgreSQL.
+    When using real backends please note: Create/drop and other actions can
+    take extremly long time.
+    """
 
     def __init__(self):
         self.conf = cfg.CONF
         self.conf.import_opt('connection',
                              'openstack.common.db.sqlalchemy.session',
                              group='database')
+        self.uri = os.getenv('OS_TEST_DBAPI_CONNECTION', 'sqlite://')
 
     def setUp(self):
-        super(SqliteInMemoryFixture, self).setUp()
+        super(DbFixture, self).setUp()
+        # To prevent existance of prepared engine with wrong connection
+        session.cleanup()
 
-        self.conf.set_default('connection', "sqlite://", group='database')
+        self.conf.set_default('connection', self.uri, group='database')
         self.addCleanup(self.conf.reset)
         self.addCleanup(session.cleanup)
 
 
 class DbTestCase(test_utils.BaseTestCase):
-    """Base class for testing of DB code (uses in-memory SQLite DB fixture)."""
+    """Base class for testing of DB code.
+    To use backend other than sqlite: override USE_BACKEND attribute
+    accordingly to env variable OS_TEST_DBAPI_ADMIN_CONNECTION
+    defined in tox.ini file. Can't be used outside of tox.
+    """
+
+    def __init__(self, *args):
+        super(DbTestCase, self).__init__(*args)
+        self.conf = cfg.CONF
 
     def setUp(self):
         super(DbTestCase, self).setUp()
+        self.useFixture(DbFixture())
 
-        self.useFixture(SqliteInMemoryFixture())
+
+def backend_specific(*dialects):
+    """Decorator to skip backend specific tests on inappropriate engines."""
+    def wrap(f):
+        @wraps(f)
+        def ins_wrap(self):
+            engine = session.get_engine()
+            if engine.name not in dialects:
+                msg = ' '.join(('The test "%s" can be run only on %s.',
+                                'Current engine is %s.'))
+                args = (f.__name__, ' '.join(dialects), engine.name)
+                self.skip(msg % args)
+            else:
+                return f(self)
+        return ins_wrap
+    return wrap
