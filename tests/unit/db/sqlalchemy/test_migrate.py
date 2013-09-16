608c2e3302e0b9941071d2bc7dcdd04f74f159ca
Pekelny Ilya <ipekelny@mirantis.com>
2013-09-16 19:15:12 +0300

    The ability to run tests at various backend
    Added testing functionality for using different db backends. Сurrently
    exists ability to define backend-specific tests along with regular
    tests. Assuming that the tests will be run under a variety of backend
    there exists opportunity to skip backend-specific tests at
    inappropriate backends. For the correct functioning of the tests required to
    export environment variable with a root access database uri.
    
    Blueprint: tests-given-db-backend
    Change-Id: Ic5d3cfe8764b3c04affe79088473255c17535e54

--- tests/unit/db/sqlalchemy/test_migrate.py
+++ tests/unit/db/sqlalchemy/test_migrate.py
@@ -64,6 +64,7 @@ class TestSqliteUniqueConstraints(test_base.DbTestCase):
 
         self.addCleanup(test_table.drop)
 
+    @test_base.backend_specific('sqlite')
     def test_get_unique_constraints(self):
         table = self.reflected_table
 
@@ -74,6 +75,7 @@ class TestSqliteUniqueConstraints(test_base.DbTestCase):
         )
         self.assertEqual(should_be, existing)
 
+    @test_base.backend_specific('sqlite')
     def test_add_unique_constraint(self):
         table = self.reflected_table
         UniqueConstraint(table.c.a, table.c.c, name='unique_a_c').create()
@@ -86,6 +88,7 @@ class TestSqliteUniqueConstraints(test_base.DbTestCase):
         )
         self.assertEqual(should_be, existing)
 
+    @test_base.backend_specific('sqlite')
     def test_drop_unique_constraint(self):
         table = self.reflected_table
         UniqueConstraint(table.c.a, table.c.b, name='unique_a_b').drop()
