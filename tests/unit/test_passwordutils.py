28ed7f2d8738342f67939283b56d10f1f311e9e6
daniel-a-nguyen <daniel.a.nguyen@hp.com>
2013-09-23 17:04:04 -0700

    Added util to generate passwords from nova
    Change-Id: I30905928a7f9bc359f09d7ee8d99cb7fcc9d570c
    Implements: blueprint move-password-generator-method-to-common-util

---
+++ tests/unit/test_passwordutils.py
@@ -0,0 +1,54 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from openstack.common import passwordutils
+from openstack.common import test
+from openstack.common.processutils import InvalidArgumentError
+
+
+class TestPasswordUtils(test.BaseTestCase):
+
+    def test_generate_password(self):
+        password = passwordutils.generate_password()
+        self.assertEqual(len(password), passwordutils.DEFAULT_PASSWORD_LENGTH)
+
+    def test_generate_password_length(self):
+        password = passwordutils.generate_password(16)
+        self.assertEqual(len(password), 16)
+
+    def test_generate_password_uniqueness(self):
+        password_list = []
+        for i in xrange(20):
+            password_list.append(passwordutils.generate_password())
+            self.assertTrue(password_list[i] is not None)
+            self.assertEqual(len(password_list[i]),
+                             passwordutils.DEFAULT_PASSWORD_LENGTH)
+
+        self.assertEqual(len(password_list), len(set(password_list)))
+
+    def test_raises_invalid_argument_error(self):
+        # DEFAULT_PASSWORD_SYMBOLS length is 3
+        self.assertRaises(InvalidArgumentError,
+                          passwordutils.generate_password, 0)
+
+        self.assertRaises(InvalidArgumentError,
+                          passwordutils.generate_password, 1)
+
+        self.assertRaises(InvalidArgumentError,
+                          passwordutils.generate_password, 2)
+
+        self.assertRaises(InvalidArgumentError,
+                          passwordutils.generate_password, 3)
