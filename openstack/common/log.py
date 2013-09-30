16ac9142acf84f61e433e35c0c570518225fd5bc
DennyZhang <denny@unitedstack.com>
2013-09-30 08:44:46 -0500

    _get_log_file_path is incorrect, when logfile and logdire unset
    Some change for tempest/openstack/common/log.py:_get_log_file_path:
     return None, when logfile and logdir are both unset
    
    Change-Id: I85176b9ba6334d694c8a71901ad358f487d6bc88
    Closes-Bug: #1232884

--- openstack/common/log.py
+++ openstack/common/log.py
@@ -207,6 +207,8 @@ def _get_log_file_path(binary=None):
         binary = binary or _get_binary_name()
         return '%s.log' % (os.path.join(logdir, binary),)
 
+    return None
+
 
 class BaseLoggerAdapter(logging.LoggerAdapter):
 
