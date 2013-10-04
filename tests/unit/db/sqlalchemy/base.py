openstack/oslo.db

Replace using tests.utils part3
It is the last step to replace using tests.utils with
stack.common.test.
In this patch database tests is changed for using
openstack.common.test and added needed fixtures in test classes.

Blueprint: common-unit-tests
Change-Id: I9b360f9a8dc72bc3fb697e571555775bde10ef61

tests/unit/db/sqlalchemy/base.py
@@ -15,30 +15,28 @@
 
 
 import fixtures
-from oslo.config import cfg
 
 from openstack.common.db.sqlalchemy import session
-from tests import utils as test_utils
+from openstack.common.fixture import config
+from openstack.common import test
 
 
 class SqliteInMemoryFixture(fixtures.Fixture):
     """SQLite in-memory DB recreated for each test case."""
 
-    def __init__(self):
-        self.conf = cfg.CONF
+    def setUp(self):
+        super(SqliteInMemoryFixture, self).setUp()
+        config_fixture = self.useFixture(config.Config())
+        self.conf = config_fixture.conf
         self.conf.import_opt('connection',
                              'openstack.common.db.sqlalchemy.session',
                              group='database')
 
-    def setUp(self):
-        super(SqliteInMemoryFixture, self).setUp()
-
         self.conf.set_default('connection', "sqlite://", group='database')
-        self.addCleanup(self.conf.reset)
         self.addCleanup(session.cleanup)
 
 
-class DbTestCase(test_utils.BaseTestCase):
+class DbTestCase(test.BaseTestCase):
     """Base class for testing of DB code (uses in-memory SQLite DB fixture)."""
 
     def setUp(self):

tests/unit/db/sqlalchemy/test_sqlalchemy.py
@@ -25,8 +25,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from openstack.common.db import exception as db_exc
 from openstack.common.db.sqlalchemy import models
 from openstack.common.db.sqlalchemy import session
+from openstack.common import fileutils
+from openstack.common.fixture import config
+from openstack.common import test
 from tests.unit.db.sqlalchemy import base as test_base
-from tests import utils as test_utils
 
 
 BASE = declarative_base()
@@ -41,8 +43,14 @@ class TmpTable(BASE, models.ModelBase):
 
 class SessionParametersTestCase(test_base.DbTestCase):
 
+    def setUp(self):
+        super(SessionParametersTestCase, self).setUp()
+        config_fixture = self.useFixture(config.Config())
+        self.conf = config_fixture.conf
+        self.write_to_tempfile = fileutils.write_to_tempfile
+
     def test_deprecated_session_parameters(self):
-        paths = self.create_tempfiles([('test', """[DEFAULT]
+        path = self.write_to_tempfile("""[DEFAULT]
 sql_connection=x://y.z
 sql_min_pool_size=10
 sql_max_pool_size=20
@@ -51,8 +59,8 @@ sql_retry_interval=40
 sql_max_overflow=50
 sql_connection_debug=60
 sql_connection_trace=True
-""")])
-        self.conf(['--config-file', paths[0]])
+""")
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'x://y.z')
         self.assertEqual(self.conf.database.min_pool_size, 10)
         self.assertEqual(self.conf.database.max_pool_size, 20)
@@ -63,7 +71,7 @@ sql_connection_trace=True
         self.assertEqual(self.conf.database.connection_trace, True)
 
     def test_session_parameters(self):
-        paths = self.create_tempfiles([('test', """[database]
+        path = self.write_to_tempfile("""[database]
 connection=x://y.z
 min_pool_size=10
 max_pool_size=20
@@ -73,8 +81,8 @@ max_overflow=50
 connection_debug=60
 connection_trace=True
 pool_timeout=7
-""")])
-        self.conf(['--config-file', paths[0]])
+""")
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'x://y.z')
         self.assertEqual(self.conf.database.min_pool_size, 10)
         self.assertEqual(self.conf.database.max_pool_size, 20)
@@ -86,18 +94,17 @@ pool_timeout=7
         self.assertEqual(self.conf.database.pool_timeout, 7)
 
     def test_dbapi_database_deprecated_parameters(self):
-        paths = self.create_tempfiles([('test',
-                                        '[DATABASE]\n'
-                                        'sql_connection=fake_connection\n'
-                                        'sql_idle_timeout=100\n'
-                                        'sql_min_pool_size=99\n'
-                                        'sql_max_pool_size=199\n'
-                                        'sql_max_retries=22\n'
-                                        'reconnect_interval=17\n'
-                                        'sqlalchemy_max_overflow=101\n'
-                                        'sqlalchemy_pool_timeout=5\n'
-                                        )])
-        self.conf(['--config-file', paths[0]])
+        path = self.write_to_tempfile('[DATABASE]\n'
+                                      'sql_connection=fake_connection\n'
+                                      'sql_idle_timeout=100\n'
+                                      'sql_min_pool_size=99\n'
+                                      'sql_max_pool_size=199\n'
+                                      'sql_max_retries=22\n'
+                                      'reconnect_interval=17\n'
+                                      'sqlalchemy_max_overflow=101\n'
+                                      'sqlalchemy_pool_timeout=5\n'
+                                      )
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'fake_connection')
         self.assertEqual(self.conf.database.idle_timeout, 100)
         self.assertEqual(self.conf.database.min_pool_size, 99)
@@ -108,11 +115,10 @@ pool_timeout=7
         self.assertEqual(self.conf.database.pool_timeout, 5)
 
     def test_dbapi_database_deprecated_parameters_sql_connection(self):
-        paths = self.create_tempfiles([('test',
-                                        '[sql]\n'
-                                        'connection=test_sql_connection\n'
-                                        )])
-        self.conf(['--config-file', paths[0]])
+        path = self.write_to_tempfile('[sql]\n'
+                                      'connection=test_sql_connection\n'
+                                      )
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'test_sql_connection')
 
 
@@ -202,7 +208,7 @@ class RegexpFilterTestCase(test_base.DbTestCase):
         self._test_regexp_filter(u'♦', [])
 
 
-class SlaveBackendTestCase(test_utils.BaseTestCase):
+class SlaveBackendTestCase(test.BaseTestCase):
 
     def test_slave_engine_nomatch(self):
         default = session.CONF.database.connection

tests/unit/db/sqlalchemy/test_utils.py
@@ -30,10 +30,11 @@ from sqlalchemy.types import UserDefinedType, NullType
 from openstack.common.db.sqlalchemy import migration
 from openstack.common.db.sqlalchemy import test_migrations
 from openstack.common.db.sqlalchemy import utils
-from tests import utils as testutils
+from openstack.common.fixture import moxstubout
+from openstack.common import test
 
 
-class TestSanitizeDbUrl(testutils.BaseTestCase):
+class TestSanitizeDbUrl(test.BaseTestCase):
 
     def test_url_with_cred(self):
         db_url = 'myproto://johndoe:secret@localhost/myschema'
@@ -74,9 +75,11 @@ class FakeModel(object):
         return '<FakeModel: %s>' % self.values
 
 
-class TestPaginateQuery(testutils.BaseTestCase):
+class TestPaginateQuery(test.BaseTestCase):
     def setUp(self):
         super(TestPaginateQuery, self).setUp()
+        mox_fixture = self.useFixture(moxstubout.MoxStubout())
+        self.mox = mox_fixture.mox
         self.query = self.mox.CreateMockAnything()
         self.mox.StubOutWithMock(sqlalchemy, 'asc')
         self.mox.StubOutWithMock(sqlalchemy, 'desc')

tests/unit/db/test_api.py
@@ -19,7 +19,10 @@
 from eventlet import tpool
 
 from openstack.common.db import api
-from tests import utils as test_utils
+from openstack.common import fileutils
+from openstack.common.fixture import config
+from openstack.common.fixture import moxstubout
+from openstack.common import test
 
 
 def get_backend():
@@ -31,27 +34,35 @@ class DBAPI(object):
         return args, kwargs
 
 
-class DBAPITestCase(test_utils.BaseTestCase):
+class DBAPITestCase(test.BaseTestCase):
+
+    def setUp(self):
+        super(DBAPITestCase, self).setUp()
+        config_fixture = self.useFixture(config.Config())
+        self.conf = config_fixture.conf
+        self.config = config_fixture.config
+
+        mox_fixture = self.useFixture(moxstubout.MoxStubout())
+        self.write_to_tempfile = fileutils.write_to_tempfile
+        self.stubs = mox_fixture.stubs
 
     def test_deprecated_dbapi_parameters(self):
-        paths = self.create_tempfiles([('test',
-                                        '[DEFAULT]\n'
-                                        'db_backend=test_123\n'
-                                        'dbapi_use_tpool=True\n'
-                                        )])
+        path = self.write_to_tempfile('[DEFAULT]\n'
+                                      'db_backend=test_123\n'
+                                      'dbapi_use_tpool=True\n'
+                                      )
 
-        self.conf(['--config-file', paths[0]])
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.backend, 'test_123')
         self.assertEqual(self.conf.database.use_tpool, True)
 
     def test_dbapi_parameters(self):
-        paths = self.create_tempfiles([('test',
-                                        '[database]\n'
-                                        'backend=test_123\n'
-                                        'use_tpool=True\n'
-                                        )])
+        path = self.write_to_tempfile('[database]\n'
+                                      'backend=test_123\n'
+                                      'use_tpool=True\n'
+                                      )
 
-        self.conf(['--config-file', paths[0]])
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.backend, 'test_123')
         self.assertEqual(self.conf.database.use_tpool, True)
 

openstack-archive/oslo-incubator

tests/unit/db/sqlalchemy/base.py
@@ -15,30 +15,28 @@
 
 
 import fixtures
-from oslo.config import cfg
 
 from openstack.common.db.sqlalchemy import session
-from tests import utils as test_utils
+from openstack.common.fixture import config
+from openstack.common import test
 
 
 class SqliteInMemoryFixture(fixtures.Fixture):
     """SQLite in-memory DB recreated for each test case."""
 
-    def __init__(self):
-        self.conf = cfg.CONF
+    def setUp(self):
+        super(SqliteInMemoryFixture, self).setUp()
+        configfixture = self.useFixture(config.Config())
+        self.conf = configfixture.conf
         self.conf.import_opt('connection',
                              'openstack.common.db.sqlalchemy.session',
                              group='database')
 
-    def setUp(self):
-        super(SqliteInMemoryFixture, self).setUp()
-
         self.conf.set_default('connection', "sqlite://", group='database')
-        self.addCleanup(self.conf.reset)
         self.addCleanup(session.cleanup)
 
 
-class DbTestCase(test_utils.BaseTestCase):
+class DbTestCase(test.BaseTestCase):
     """Base class for testing of DB code (uses in-memory SQLite DB fixture)."""
 
     def setUp(self):

tests/unit/db/sqlalchemy/test_sqlalchemy.py
@@ -25,8 +25,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from openstack.common.db import exception as db_exc
 from openstack.common.db.sqlalchemy import models
 from openstack.common.db.sqlalchemy import session
+from openstack.common import fileutils
+from openstack.common.fixture import config
+from openstack.common import test
 from tests.unit.db.sqlalchemy import base as test_base
-from tests import utils as test_utils
 
 
 BASE = declarative_base()
@@ -41,8 +43,14 @@ class TmpTable(BASE, models.ModelBase):
 
 class SessionParametersTestCase(test_base.DbTestCase):
 
+    def setUp(self):
+        super(SessionParametersTestCase, self).setUp()
+        configfixture = self.useFixture(config.Config())
+        self.conf = configfixture.conf
+        self.write_to_tempfile = fileutils.write_to_tempfile
+
     def test_deprecated_session_parameters(self):
-        paths = self.create_tempfiles([('test', """[DEFAULT]
+        path = self.write_to_tempfile("""[DEFAULT]
 sql_connection=x://y.z
 sql_min_pool_size=10
 sql_max_pool_size=20
@@ -51,8 +59,8 @@ sql_retry_interval=40
 sql_max_overflow=50
 sql_connection_debug=60
 sql_connection_trace=True
-""")])
-        self.conf(['--config-file', paths[0]])
+""")
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'x://y.z')
         self.assertEqual(self.conf.database.min_pool_size, 10)
         self.assertEqual(self.conf.database.max_pool_size, 20)
@@ -63,7 +71,7 @@ sql_connection_trace=True
         self.assertEqual(self.conf.database.connection_trace, True)
 
     def test_session_parameters(self):
-        paths = self.create_tempfiles([('test', """[database]
+        path = self.write_to_tempfile("""[database]
 connection=x://y.z
 min_pool_size=10
 max_pool_size=20
@@ -73,8 +81,8 @@ max_overflow=50
 connection_debug=60
 connection_trace=True
 pool_timeout=7
-""")])
-        self.conf(['--config-file', paths[0]])
+""")
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'x://y.z')
         self.assertEqual(self.conf.database.min_pool_size, 10)
         self.assertEqual(self.conf.database.max_pool_size, 20)
@@ -86,18 +94,17 @@ pool_timeout=7
         self.assertEqual(self.conf.database.pool_timeout, 7)
 
     def test_dbapi_database_deprecated_parameters(self):
-        paths = self.create_tempfiles([('test',
-                                        '[DATABASE]\n'
-                                        'sql_connection=fake_connection\n'
-                                        'sql_idle_timeout=100\n'
-                                        'sql_min_pool_size=99\n'
-                                        'sql_max_pool_size=199\n'
-                                        'sql_max_retries=22\n'
-                                        'reconnect_interval=17\n'
-                                        'sqlalchemy_max_overflow=101\n'
-                                        'sqlalchemy_pool_timeout=5\n'
-                                        )])
-        self.conf(['--config-file', paths[0]])
+        path = self.write_to_tempfile('[DATABASE]\n'
+                                      'sql_connection=fake_connection\n'
+                                      'sql_idle_timeout=100\n'
+                                      'sql_min_pool_size=99\n'
+                                      'sql_max_pool_size=199\n'
+                                      'sql_max_retries=22\n'
+                                      'reconnect_interval=17\n'
+                                      'sqlalchemy_max_overflow=101\n'
+                                      'sqlalchemy_pool_timeout=5\n'
+                                      )
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.connection, 'fake_connection')
         self.assertEqual(self.conf.database.idle_timeout, 100)
         self.assertEqual(self.conf.database.min_pool_size, 99)
@@ -194,7 +201,7 @@ class RegexpFilterTestCase(test_base.DbTestCase):
         self._test_regexp_filter(u'♦', [])
 
 
-class SlaveBackendTestCase(test_utils.BaseTestCase):
+class SlaveBackendTestCase(test.BaseTestCase):
 
     def test_slave_engine_nomatch(self):
         default = session.CONF.database.connection

tests/unit/db/sqlalchemy/test_utils.py
@@ -30,10 +30,11 @@ from sqlalchemy.types import UserDefinedType, NullType
 from openstack.common.db.sqlalchemy import migration
 from openstack.common.db.sqlalchemy import test_migrations
 from openstack.common.db.sqlalchemy import utils
-from tests import utils as testutils
+from openstack.common.fixture import moxstubout
+from openstack.common import test
 
 
-class TestSanitizeDbUrl(testutils.BaseTestCase):
+class TestSanitizeDbUrl(test.BaseTestCase):
 
     def test_url_with_cred(self):
         db_url = 'myproto://johndoe:secret@localhost/myschema'
@@ -74,9 +75,11 @@ class FakeModel(object):
         return '<FakeModel: %s>' % self.values
 
 
-class TestPaginateQuery(testutils.BaseTestCase):
+class TestPaginateQuery(test.BaseTestCase):
     def setUp(self):
         super(TestPaginateQuery, self).setUp()
+        moxfixture = self.useFixture(moxstubout.MoxStubout())
+        self.mox = moxfixture.mox
         self.query = self.mox.CreateMockAnything()
         self.mox.StubOutWithMock(sqlalchemy, 'asc')
         self.mox.StubOutWithMock(sqlalchemy, 'desc')

tests/unit/db/test_api.py
@@ -19,7 +19,10 @@
 from eventlet import tpool
 
 from openstack.common.db import api
-from tests import utils as test_utils
+from openstack.common import fileutils
+from openstack.common.fixture import config
+from openstack.common.fixture import moxstubout
+from openstack.common import test
 
 
 def get_backend():
@@ -31,27 +34,35 @@ class DBAPI(object):
         return args, kwargs
 
 
-class DBAPITestCase(test_utils.BaseTestCase):
+class DBAPITestCase(test.BaseTestCase):
+
+    def setUp(self):
+        super(DBAPITestCase, self).setUp()
+        configfixture = self.useFixture(config.Config())
+        self.conf = configfixture.conf
+        self.config = configfixture.config
+
+        moxfixture = self.useFixture(moxstubout.MoxStubout())
+        self.write_to_tempfile = fileutils.write_to_tempfile
+        self.stubs = moxfixture.stubs
 
     def test_deprecated_dbapi_parameters(self):
-        paths = self.create_tempfiles([('test',
-                                        '[DEFAULT]\n'
-                                        'db_backend=test_123\n'
-                                        'dbapi_use_tpool=True\n'
-                                        )])
+        path = self.write_to_tempfile('[DEFAULT]\n'
+                                      'db_backend=test_123\n'
+                                      'dbapi_use_tpool=True\n'
+                                      )
 
-        self.conf(['--config-file', paths[0]])
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.backend, 'test_123')
         self.assertEqual(self.conf.database.use_tpool, True)
 
     def test_dbapi_parameters(self):
-        paths = self.create_tempfiles([('test',
-                                        '[database]\n'
-                                        'backend=test_123\n'
-                                        'use_tpool=True\n'
-                                        )])
+        path = self.write_to_tempfile('[database]\n'
+                                      'backend=test_123\n'
+                                      'use_tpool=True\n'
+                                      )
 
-        self.conf(['--config-file', paths[0]])
+        self.conf(['--config-file', path])
         self.assertEqual(self.conf.database.backend, 'test_123')
         self.assertEqual(self.conf.database.use_tpool, True)
 