496a14c6733b156acea9ef9fa5003f821cf5caa2
Matthew Treinish <treinish@linux.vnet.ibm.com>
2013-10-03 22:42:55 -0400

    Add LockFixture to lockutils
    This commit adds a fixture class to be used instead of external
    locking with the synchronized decorator. When running tests in
    parallel the synchronized decorator won't synchronize the tearDown
    and addCleanup methods for a test. By using the fixture which does
    LockFixture the unlocking is done by using addCleanup() ensuring that
    tearDown and later addCleanup methods are run inside the lock
    context.
    
    Closes-Bug: #1229437
    
    Change-Id: I61af5e9d385bb7ef2e6402366fb8c5c09f749986

--- tests/unit/test_lockutils.py
+++ tests/unit/test_lockutils.py
@@ -314,3 +314,30 @@ class LockTestCase(test.BaseTestCase):
             pass
 
         self.assertRaises(cfg.RequiredOptError, foo)
+
+
+class TestLockFixture(test.BaseTestCase):
+
+    def setUp(self):
+        super(TestLockFixture, self).setUp()
+        self.config = self.useFixture(config.Config()).config
+        self.tempdir = tempfile.mkdtemp()
+
+    def _check_in_lock(self):
+        # Check that the lock file exists during teardown
+        lock_path = os.path.join(self.tempdir, 'test-lock')
+        self.assertTrue(os.path.exists(lock_path))
+
+    def tearDown(self):
+        self._check_in_lock()
+        super(TestLockFixture, self).tearDown()
+
+    def _cleanup_tempdir(self):
+        if os.path.exists(self.tempdir):
+            shutil.rmtree(self.tempdir)
+
+    def test_lock_fixture(self):
+        # Setup lock fixture to test that teardown is inside the lock
+        self.config(lock_path=self.tempdir)
+        self.addCleanup(self._cleanup_tempdir)
+        self.useFixture(lockutils.LockFixture('test-lock'))
