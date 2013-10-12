5d2ef356c953e359b82572e0c399f9a77210dcc7
Chang Bo Guo <guochbo@cn.ibm.com>
2013-10-11 19:03:01 -0700

    Add byte unit constants
    A lots of numeric expressions are needed to caculate byte number
    across all projects, such as 1024*1024 , 1024**3 etc. This patch
    add byte unit constants and will sync into projects, replace
    nummeric experssions with byte unit constants. Use these constants
    like: root_disk_gb =  5 * unit.Gi
    
    blueprint byte-unit-clean
    
    Change-Id: I5093244f607d8c3a61a5f6b7ecbe657e5574b793

---
+++ tests/unit/test_units.py
@@ -0,0 +1,31 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 IBM Corp
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack.common import test
+from openstack.common import units
+
+
+class UnitTest(test.BaseTestCase):
+    def test_byte_unit(self):
+        self.assertEqual(units.Ki, 1024)
+        self.assertEqual(units.Mi, 1024 ** 2)
+        self.assertEqual(units.Gi, 1024 ** 3)
+        self.assertEqual(units.Ti, 1024 ** 4)
+        self.assertEqual(units.Pi, 1024 ** 5)
+        self.assertEqual(units.Ei, 1024 ** 6)
+        self.assertEqual(units.Zi, 1024 ** 7)
+        self.assertEqual(units.Yi, 1024 ** 8)
