f651fc10a506a95eecf27509eddb9b2a9d11aa45
Yuriy Taraday <yorik.sar@gmail.com>
2013-10-18 15:30:15 +0400

    Allow to use lockutils without fixtures installed
    Current version of lockutils unconditionnaly requires fixtures. This is
    not good for projects that use lockutils not only for tests but alos in
    runtime.
    
    This change provides a way to provide the fixture only if fixtures
    module is available.
    
    Change-Id: Ic5b97939075b83956097a8fc017c07a95bba6981

--- openstack/common/lockutils.py
+++ openstack/common/lockutils.py
@@ -24,7 +24,10 @@ import threading
 import time
 import weakref
 
-import fixtures
+try:
+    import fixtures
+except ImportError:
+    fixtures = False
 from oslo.config import cfg
 
 from openstack.common import fileutils
@@ -279,34 +282,39 @@ def synchronized_with_prefix(lock_file_prefix):
     return functools.partial(synchronized, lock_file_prefix=lock_file_prefix)
 
 
-class LockFixture(fixtures.Fixture):
-    """External locking fixture.
+if fixtures:
+    class LockFixture(fixtures.Fixture):
+        """External locking fixture.
 
-    This fixture is basically an alternative to the synchronized decorator with
-    the external flag so that tearDowns and addCleanups will be included in
-    the lock context for locking between tests. The fixture is recommended to
-    be the first line in a test method, like so::
+        This fixture is basically an alternative to the synchronized decorator
+        with the external flag so that tearDowns and addCleanups will be
+        included in the lock context for locking between tests. The fixture is
+        recommended to be the first line in a test method, like so::
 
-        def test_method(self):
-            self.useFixture(LockFixture)
-                ...
-
-    or the first line in setUp if all the test methods in the class are
-    required to be serialized. Something like::
-
-        class TestCase(testtools.testcase):
-            def setUp(self):
+            def test_method(self):
                 self.useFixture(LockFixture)
-                super(TestCase, self).setUp()
                     ...
 
-    This is because addCleanups are put on a LIFO queue that gets run after the
-    test method exits. (either by completing or raising an exception)
-    """
-    def __init__(self, name, lock_file_prefix=None):
-        self.mgr = lock(name, lock_file_prefix, True)
+        or the first line in setUp if all the test methods in the class are
+        required to be serialized. Something like::
 
-    def setUp(self):
-        super(LockFixture, self).setUp()
-        self.addCleanup(self.mgr.__exit__, None, None, None)
-        self.mgr.__enter__()
+            class TestCase(testtools.testcase):
+                def setUp(self):
+                    self.useFixture(LockFixture)
+                    super(TestCase, self).setUp()
+                        ...
+
+        This is because addCleanups are put on a LIFO queue that gets run after
+        the test method exits. (either by completing or raising an exception)
+        """
+        def __init__(self, name, lock_file_prefix=None):
+            self.mgr = lock(name, lock_file_prefix, True)
+
+        def setUp(self):
+            super(LockFixture, self).setUp()
+            self.addCleanup(self.mgr.__exit__, None, None, None)
+            self.mgr.__enter__()
+else:
+    class LockFixture(object):
+        def __init__(self, *args, **kwargs):
+            raise ImportError('fixtures')
