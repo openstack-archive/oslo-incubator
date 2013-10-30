fd93d4fbcc11285448394f8fbbe6204dbcba388c
David Stanek <dstanek@dstanek.com>
2013-10-30 17:42:01 +0000

    Adds decorator to deprecate functions and methods
    The deprecated decorator logs a deprecation message when the callable it
    decorates is used. The message will include the release where the callable
    was deprecated, the release where is may be removed and possibly an
    optional replacement.
    
    Change-Id: Ic6260ce9d8950afa248c0f7842817e7312a4d5a7

--- tests/unit/test_versionutils.py
+++ tests/unit/test_versionutils.py
@@ -15,8 +15,135 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from testtools import matchers
+
 from openstack.common import test
 from openstack.common import versionutils
+
+
+class DeprecatedTestCase(test.BaseTestCase):
+
+    def setUp(self):
+        super(DeprecatedTestCase, self).setUp()
+        self.orig_logger = versionutils.LOG
+        versionutils.LOG = self
+
+    def tearDown(self):
+        super(DeprecatedTestCase, self).tearDown()
+        versionutils.LOG = self.orig_logger
+
+    def deprecated(self, message, details):
+        self.deprecated_message = message % details
+
+    def test_deprecating_a_function_returns_correct_value(self):
+
+        @versionutils.deprecated(as_of=versionutils.deprecated.ICEHOUSE)
+        def do_outdated_stuff(data):
+            return data
+
+        expected_rv = 'expected return value'
+        retval = do_outdated_stuff(expected_rv)
+
+        self.assertThat(retval, matchers.Equals(expected_rv))
+
+    def test_deprecating_a_method_returns_correct_value(self):
+
+        class C(object):
+            @versionutils.deprecated(as_of=versionutils.deprecated.ICEHOUSE)
+            def outdated_method(self, *args):
+                return args
+
+        retval = C().outdated_method(1, 'of anything')
+
+        self.assertThat(retval, matchers.Equals((1, 'of anything')))
+
+    def test_deprecated_with_unknown_future_release(self):
+
+        @versionutils.deprecated(as_of=versionutils.deprecated.ICEHOUSE,
+                                 in_favor_of='different_stuff()')
+        def do_outdated_stuff():
+            return
+
+        do_outdated_stuff()
+
+        expected = ('do_outdated_stuff() is deprecated as of Icehouse '
+                    'in favor of different_stuff() and may be removed in K.')
+        self.assertThat(self.deprecated_message,
+                        matchers.Contains(expected))
+
+    def test_deprecated_with_known_future_release(self):
+
+        @versionutils.deprecated(as_of=versionutils.deprecated.GRIZZLY,
+                                 in_favor_of='different_stuff()')
+        def do_outdated_stuff():
+            return
+
+        do_outdated_stuff()
+
+        expected = ('do_outdated_stuff() is deprecated as of Grizzly '
+                    'in favor of different_stuff() and may be removed in '
+                    'Icehouse.')
+        self.assertThat(self.deprecated_message,
+                        matchers.Contains(expected))
+
+    def test_deprecated_without_replacement(self):
+
+        @versionutils.deprecated(as_of=versionutils.deprecated.GRIZZLY)
+        def do_outdated_stuff():
+            return
+
+        do_outdated_stuff()
+
+        expected = ('do_outdated_stuff() is deprecated as of Grizzly '
+                    'and may be removed in Icehouse. It will not be '
+                    'superseded.')
+        self.assertThat(self.deprecated_message,
+                        matchers.Contains(expected))
+
+    def test_deprecated_with_custom_what(self):
+
+        @versionutils.deprecated(as_of=versionutils.deprecated.GRIZZLY,
+                                 what='v2.0 API',
+                                 in_favor_of='v3 API')
+        def do_outdated_stuff():
+            return
+
+        do_outdated_stuff()
+
+        expected = ('v2.0 API is deprecated as of Grizzly in favor of '
+                    'v3 API and may be removed in Icehouse.')
+        self.assertThat(self.deprecated_message,
+                        matchers.Contains(expected))
+
+    def test_deprecated_with_removed_next_release(self):
+
+        @versionutils.deprecated(as_of=versionutils.deprecated.GRIZZLY,
+                                 remove_in=1)
+        def do_outdated_stuff():
+            return
+
+        do_outdated_stuff()
+
+        expected = ('do_outdated_stuff() is deprecated as of Grizzly '
+                    'and may be removed in Havana. It will not be '
+                    'superseded.')
+        self.assertThat(self.deprecated_message,
+                        matchers.Contains(expected))
+
+    def test_deprecated_with_removed_plus_3(self):
+
+        @versionutils.deprecated(as_of=versionutils.deprecated.GRIZZLY,
+                                 remove_in=+3)
+        def do_outdated_stuff():
+            return
+
+        do_outdated_stuff()
+
+        expected = ('do_outdated_stuff() is deprecated as of Grizzly '
+                    'and may be removed in J. It will not '
+                    'be superseded.')
+        self.assertThat(self.deprecated_message,
+                        matchers.Contains(expected))
 
 
 class IsCompatibleTestCase(test.BaseTestCase):
