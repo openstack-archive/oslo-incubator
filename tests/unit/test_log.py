a6f40aca82632313ceec6732e51110ef536adc33
Davanum Srinivas <dims@linux.vnet.ibm.com>
2013-10-31 08:57:36 -0400

    Don't log to stdout when log_dir is set
    In addition to checking if log_file is set, we should check
    for log_dir as well. Previously, If log_file is not set, oslo
    will output logs to stdout even if log_dir is set. The only way
    for example the Heat folks avoided logging to stdout was to
    set log_file, but then all the heat processes ended up logging
    to the same file. So a check for both log_dir and log_file
    should avoid any output to stdout. Please note that use_stderr
    should be set to false as well to avoid any output to console
    
    Change-Id: Ie2886da679daedea0197ee3b3963ebedb8f11a0b
    Closes-Bug: #1226287

--- tests/unit/test_log.py
+++ tests/unit/test_log.py
@@ -17,6 +17,7 @@
 import logging
 import os
 import sys
+import tempfile
 
 import mock
 from oslo.config import cfg
@@ -436,6 +437,16 @@ class LogConfigOptsTestCase(test.BaseTestCase):
         log_file = '/some/path/foo-bar.log'
         self.CONF(['--log-file', log_file])
         self.assertEqual(self.CONF.log_file, log_file)
+
+    def test_log_dir_handlers(self):
+        log_dir = tempfile.mkdtemp()
+        self.CONF(['--log-dir', log_dir])
+        self.CONF.set_default('use_stderr', False)
+        log._setup_logging_from_conf()
+        logger = log._loggers[None].logger
+        self.assertEqual(1, len(logger.handlers))
+        self.assertIsInstance(logger.handlers[0],
+                              logging.handlers.WatchedFileHandler)
 
     def test_logfile_deprecated(self):
         logfile = '/some/other/path/foo-bar.log'
