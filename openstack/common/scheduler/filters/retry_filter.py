540e2de1445ec567d33fcb7403d80041a4b6f36a
Jay Lau <liugya@cn.ibm.com>
2013-11-13 19:17:42 +0800

    Add retry filter to oslo
    Implements part of bp oslo-more-filters
    
    Change-Id: Ia355810b106fee14a55f48081301a310979befac

---
+++ openstack/common/scheduler/filters/retry_filter.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2013 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack.common.gettextutils import _
+from openstack.common import log as logging
+from openstack.common.scheduler import filters
+
+LOG = logging.getLogger(__name__)
+
+
+class RetryFilter(filters.BaseHostFilter):
+    """Filter out nodes that have already been attempted for scheduling
+    purposes, host passes if it's not in the list of previously attempted
+    hosts, previously attempted hosts need to be updated to retry hosts
+    in scheduler.
+    """
+
+    def host_passes(self, host_state, filter_properties):
+        """Skip nodes that have already been attempted."""
+        retry = filter_properties.get('retry', None)
+        if not retry:
+            # Re-scheduling is disabled
+            LOG.debug(_("Re-scheduling is disabled"))
+            return True
+
+        hosts = retry.get('hosts', [])
+        host = host_state.host
+
+        passes = host not in hosts
+        pass_msg = "passes" if passes else "fails"
+
+        LOG.debug(_("Host %(host)s %(pass_msg)s.  Previously tried hosts: "
+                    "%(hosts)s") % {'host': host,
+                                    'pass_msg': pass_msg,
+                                    'hosts': hosts})
+        return passes
