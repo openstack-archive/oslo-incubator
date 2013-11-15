4dd3fb1f2bc0abbc69130657fdf1b65430196844
Ilya Pekelny <ipekelny@mirantis.com>
2013-11-15 13:52:45 +0200

    SQLite behavior independent DB test cases
    Currently, cleanups in DB test cases rely on the fact, that SQLite
    in-memory DB is always used. To be able to run those test cases on RDBMS
    like MySQL and PostgreSQL we must make sure that all created tables are
    dropped and that test cases use a newly created SQLAlchemy engine
    instance. Currently used a kind of Singleton. An instance of sqlalchemy
    engine declared globally and reusing until it will be cleaned. Thus
    session.cleanup() call is required to destroy engine where it was
    created.
    
    Blueprint: tests-given-db-backend
    Change-Id: I81d1ac2804dc7434f7ed1f36206f39f777480398

--- tests/unit/db/sqlalchemy/test_sqlalchemy.py
+++ tests/unit/db/sqlalchemy/test_sqlalchemy.py
@@ -137,6 +137,7 @@ class SessionErrorWrapperTestCase(test_base.DbTestCase):
                            Column('foo', Integer),
                            UniqueConstraint('foo', name='uniq_foo'))
         test_table.create()
+        self.addCleanup(test_table.drop)
 
     def test_flush_wrapper(self):
         tbl = TmpTable()
@@ -182,6 +183,7 @@ class RegexpFilterTestCase(test_base.DbTestCase):
                                   nullable=False),
                            Column('bar', String(255)))
         test_table.create()
+        self.addCleanup(test_table.drop)
 
     def _test_regexp_filter(self, regexp, expected):
         _session = session.get_session()
@@ -209,7 +211,6 @@ class RegexpFilterTestCase(test_base.DbTestCase):
 
 
 class SlaveBackendTestCase(test.BaseTestCase):
-
     def test_slave_engine_nomatch(self):
         default = session.CONF.database.connection
         session.CONF.database.slave_connection = default
