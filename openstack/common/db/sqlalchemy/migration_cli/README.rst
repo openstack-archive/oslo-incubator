144e4ef89f6b60c5d638347b6cea25230d9dd289
Dima Shulyak <dshulyak@mirantis.com>
2013-12-02 17:29:01 +0200

    Transition from migrate to alembic
    Toolkit for smooth transitioning from migrate tool to alembic.
    Check README for some description
    
    POC: Integration with nova-manage https://review.openstack.org/#/c/60517/
    
    Blueprint: oslo-migration-toolkit
    Change-Id: Icfa7d8736a84da1a4890eb417d48bab4c73e1f89

---
+++ openstack/common/db/sqlalchemy/migration_cli/README.rst
@@ -0,0 +1,9 @@
+This module could be used either for:
+1. Smooth transition from migrate tool to alembic
+2. As standalone alembic tool
+
+Core points:
+1. Upgrade/downgrade database with usage of alembic/migrate migrations
+or both
+2. Compatibility with oslo.config
+3. The way to autogenerate new revisions or stamps
---
+++ openstack/common/db/sqlalchemy/migration_cli/ext_alembic.py
@@ -0,0 +1,61 @@
+# Copyright 2013 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+import alembic
+from alembic import config as alembic_config
+import alembic.migration as alembic_migration
+
+from openstack.common.db.sqlalchemy.migration_cli import ext_base
+from openstack.common.db.sqlalchemy import session as db_session
+
+
+class AlembicExtension(ext_base.MigrationExtensionBase):
+
+    order = 2
+
+    @classmethod
+    def check_available(cls, migration_config):
+        return os.path.exists(migration_config.get('alembic_ini_path', ''))
+
+    def __init__(self, migration_config):
+        self.config = alembic_config.Config(
+            migration_config.get('alembic_ini_path', ''))
+        # option should be used if script is not in default directory
+        repo_path = migration_config.get('alembic_repo_path')
+        if repo_path:
+            self.config.set_main_option('script_location', repo_path)
+
+    def upgrade(self, version):
+        return alembic.command.upgrade(self.config, version or 'head')
+
+    def downgrade(self, version):
+        if isinstance(version, int) or version is None or version.isdigit():
+            version = 'base'
+        return alembic.command.downgrade(self.config, version)
+
+    def version(self):
+        engine = db_session.get_engine()
+        with engine.connect() as conn:
+            context = alembic_migration.MigrationContext.configure(conn)
+            return context.get_current_revision()
+
+    def revision(self, message=None, autogenerate=False):
+        return alembic.command.revision(self.config, message=message,
+                                        autogenerate=autogenerate)
+
+    def stamp(self, revision):
+        return alembic.command.stamp(self.config, revision=revision)
