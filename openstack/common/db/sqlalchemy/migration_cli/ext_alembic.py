a003a3d253e05036d547235920311e60f53a1721
Dima Shulyak <dshulyak@mirantis.com>
2013-12-02 17:29:01 +0200

    Transition from migrate to alembic
    Toolkit for smooth transitioning from migrate tool to alembic.
    Check README for some description
    
    Blueprint: oslo-migration-toolkit
    Change-Id: Icfa7d8736a84da1a4890eb417d48bab4c73e1f89

---
+++ openstack/common/db/sqlalchemy/migration_cli/ext_alembic.py
@@ -0,0 +1,61 @@
+# Copyright 2013 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from oslo.config import cfg
+
+import alembic
+from alembic import config as alembic_config
+import alembic.migration as alembic_migration
+
+from openstack.common.db.sqlalchemy.migration_cli import ext_base
+from openstack.common.db.sqlalchemy import session as db_session
+from openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+class AlembicExtension(ext_base.MigrationExtensionBase):
+
+    order = 2
+
+    @classmethod
+    def check_available(cls):
+        return os.path.exists(cfg.CONF.database.alembic_ini_path)
+
+    def __init__(self):
+        self.config = alembic_config.Config(cfg.CONF.database.alembic_ini_path)
+
+    def upgrade(self, version):
+        return alembic.command.upgrade(self.config, version or 'head')
+
+    def downgrade(self, version):
+        version = ('base' if isinstance(version, int)
+                   or version is None else version)
+        return alembic.command.downgrade(self.config, version)
+
+    def version(self):
+        conn = db_session.get_engine().connect()
+        context = alembic_migration.MigrationContext.configure(conn)
+        return context.get_current_revision()
+
+    def revision(self, message=None, autogenerate=False):
+        return alembic.command.revision(self.config, message=message,
+                                        autogenerate=autogenerate)
+
+    def stamp(self, revision):
+        return alembic.command.stamp(self.config, revision=revision)
