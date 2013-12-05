9ea8466af3faa0cb3bd64ddef31eb5926a49212e
Sean Dague <sean.dague@samsung.com>
2013-12-05 11:20:23 -0500

    provide a global default for lock dir
    this is based on one proposed upgrade path for cinder on the
    openstack-dev mailing list. Instead of making oslo lockutils error
    if the user has not added a config variable for lock path in their
    service (which they never needed to do before) use tempdir instead.
    
    in order to address the security concerns of file truncation attacks,
    open the files for locking in 'a' mode, which will make them non
    destructive to the underlying files. Add tests to ensure that we
    don't truncate underlying files.
    
    also throw an error message to the user that this isn't a safe
    mode to run in. That will hopefully mean they will only use this
    for the small window of rolling their cluster forward, then
    configure it to something more sane.
    
    (note: this patch could use tests for that log message, comments
    about how to mock that correctly in oslo would be appreciated.)
    
    Change-Id: Ia96a8926741464cf0d47d2c25c77b9c746c1b334

--- openstack/common/lockutils.py
+++ openstack/common/lockutils.py
@@ -41,7 +41,8 @@ util_opts = [
     cfg.BoolOpt('disable_process_locking', default=False,
                 help='Whether to disable inter-process locks'),
     cfg.StrOpt('lock_path',
-               default=os.environ.get("OSLO_LOCK_PATH"),
+               default=os.environ.get("OSLO_LOCK_PATH",
+                                      tempfile.gettempdir()),
                help=('Directory to use for lock files.'))
 ]
 
@@ -77,7 +78,7 @@ class _InterProcessLock(object):
         self.fname = name
 
     def __enter__(self):
-        self.lockfile = open(self.fname, 'w')
+        self.lockfile = open(self.fname, 'a')
 
         while True:
             try:
@@ -179,6 +180,11 @@ def lock(name, lock_file_prefix=None, external=False, lock_path=None):
 
                 # We need a copy of lock_path because it is non-local
                 local_lock_path = lock_path or CONF.lock_path
+                if local_lock_path == tempfile.gettempdir():
+                    LOG.error(_(
+                        'Unsafe default lock_path "%s" set, please '
+                        'update your configuration.') % local_lock_path)
+
                 if not local_lock_path:
                     raise cfg.RequiredOptError('lock_path')
 
