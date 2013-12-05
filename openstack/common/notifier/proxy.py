771e76d845e42cfe5894ece5ba85e2bc9c678e09
Sandy Walsh <sandy.walsh@rackspace.com>
2013-12-05 18:31:43 +0000

    Notifier Proxy
    Proxy for making notification calls in manner safe for conversion to
    oslo.messaging.
    
    Change-Id: I046f0f5383ecec7d95e1a253d5abc125350fed95

---
+++ openstack/common/notifier/proxy.py
@@ -0,0 +1,55 @@
+# Copyright 2013 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Proxy openstack.common.notifier.api.notify() to look like an
+   oslo.messaging.notify Driver"""
+
+from oslo.config import cfg
+
+from openstack.common.notifier import api
+
+CONF = cfg.CONF
+
+
+class NotifierProxy(object):
+    def __init__(self, publisher_id):
+        self.publisher_id = publisher_id
+
+    def audit(self, ctxt, event_type, payload):
+        # No audit in old notifier.
+        api.notify(ctxt, self.publisher_id, event_type, payload, 'INFO')
+
+    def debug(self, ctxt, event_type, payload):
+        api.notify(ctxt, self.publisher_id, event_type, payload, 'DEBUG')
+
+    def info(self, ctxt, event_type, payload):
+        api.notify(ctxt, self.publisher_id, event_type, payload, 'INFO')
+
+    def warn(self, ctxt, event_type, payload):
+        api.notify(ctxt, self.publisher_id, event_type, payload, 'WARN')
+
+    warning = warn
+
+    def error(self, ctxt, event_type, payload):
+        api.notify(ctxt, self.publisher_id, event_type, payload, 'ERROR')
+
+    def critical(self, ctxt, event_type, payload):
+        api.notify(ctxt, self.publisher_id, event_type, payload, 'CRITICAL')
+
+
+def get_notifier(service=None, host=None, publisher_id=None):
+    if not publisher_id:
+        publisher_id = "%s.%s" % (service, host or CONF.host)
+    return NotifierProxy(publisher_id)
