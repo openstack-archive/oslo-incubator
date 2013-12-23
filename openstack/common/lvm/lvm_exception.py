38986ac28bffbc8059a83e80eaf04f1cc4d03f34
john-griffith <john.griffith@solidfire.com>
2013-12-23 15:28:43 -0700

    Add LVM module to OpenStack Common
    This patch adds a simple LVM wrapper/interface to the
    OpenStack-Common modules.  Currently we use LVM in a couple
    of places, this would allow us to remove the duplication of
    that code, particularly between Nova and Cinder as well as
    up and coming projects like Manilla.
    
    Implements Blueprint: add-lvm-module
    
    Change-Id: I58dd70937110e50b5b5c49232e66f1c7ea82cbe6

---
+++ openstack/common/lvm/lvm_exception.py
@@ -0,0 +1,70 @@
+# Copyright 2013 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Exceptions for the LVM library."""
+
+from openstack.common.gettextutils import _
+from openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+class LVMException(Exception):
+    """Base Brick Exception
+
+    To correctly use this class, inherit from it and define
+    a 'msg_fmt' property. That msg_fmt will get printf'd
+    with the keyword arguments provided to the constructor.
+    """
+    msg_fmt = _("An unknown exception occurred.")
+    code = 500
+    headers = {}
+    safe = False
+
+    def __init__(self, message=None, **kwargs):
+        self.kwargs = kwargs
+
+        if 'code' not in self.kwargs:
+            try:
+                self.kwargs['code'] = self.code
+            except AttributeError:
+                pass
+
+        if not message:
+            try:
+                message = self.msg_fmt % kwargs
+
+            except Exception:
+                # kwargs doesn't match a variable in the message
+                # log the issue and the kwargs
+                msg = (_("Exception in string format operation.  msg='%s'")
+                       % self.msg_fmt)
+                LOG.exception(msg)
+                for name, value in kwargs.iteritems():
+                    LOG.error("%s: %s" % (name, value))
+
+                # at least get the core message out if something happened
+                message = self.msg_fmt
+
+        super(LVMException, self).__init__(message)
+
+
+class VolumeGroupNotFound(LVMException):
+    message = _('Unable to find Volume Group: %(vg_name)s')
+
+
+class VolumeGroupCreationFailed(LVMException):
+    message = _('Failed to create Volume Group: %(vg_name)s')
