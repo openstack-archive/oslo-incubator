0d9e9740023797cd885587d37283563c6e554c48
Artem Yasakov <ayasakov@mirantis.com>
2013-12-27 20:23:51 +0400

    Add class ModelsObjectComparatorMixin
    Move class ModelsObjectComparatorMixin from nova.tests.db.test_db_api
    to oslo-incubator, so we could reuse it in other projects.
    
    The class provides helper methods for checking equality of objects
    (ModelBase instances) and lists of objects.
    
    Change-Id: I56745128a6d5409b78eac616afe08a426c99d5cd

--- openstack/common/test.py
+++ openstack/common/test.py
@@ -18,6 +18,7 @@
 import os
 
 import fixtures
+import six
 import testtools
 
 _TRUE_VALUES = ('True', 'true', '1', 'yes')
@@ -50,3 +51,58 @@ class BaseTestCase(testtools.TestCase):
         if os.environ.get('OS_STDERR_CAPTURE') in _TRUE_VALUES:
             stderr = self.useFixture(fixtures.StringStream('stderr')).stream
             self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
+
+
+class ModelsObjectComparatorMixin(object):
+    def _dict_from_object(self, obj, ignored_keys):
+        if ignored_keys is None:
+            ignored_keys = []
+        return dict(
+            [(k, v) for k, v in six.iteritems(obj) if k not in ignored_keys]
+        )
+
+    def assertEqualObjects(self, obj1, obj2, ignored_keys=None):
+        """Check equal of the two objects
+
+        :param obj1: First object. An 'object' is an instance of
+            ModelBase
+        :param obj2: Second objects
+        :param ignored_keys: Set of keys that will be ignored, when
+            comparing
+        :type ignored_keys: set or None
+        """
+        obj1 = self._dict_from_object(obj1, ignored_keys)
+        obj2 = self._dict_from_object(obj2, ignored_keys)
+
+        self.assertEqual(
+            len(obj1),
+            len(obj2),
+            "Keys mismatch: %s" % str(set(obj1.keys()) ^ set(obj2.keys()))
+        )
+        for key, value in six.iteritems(obj1):
+            self.assertEqual(value, obj2[key])
+
+    def assertEqualListsOfObjects(self, objs1, objs2, ignored_keys=None):
+        """Check equal of the two lists. List's object is objects.
+
+        :param objs1: First set of object. An 'object' is an instance of
+            ModelBase
+        :param objs2: Second set of objects
+        :param ignored_keys: Set of keys that will be ignored, when
+            comparing
+        :type ignored_keys: set or None
+        """
+        obj_to_dict = lambda o: self._dict_from_object(o, ignored_keys)
+        sort_key = lambda d: [d[k] for k in sorted(d)]
+        conv_and_sort = lambda obj: sorted(map(obj_to_dict, obj), key=sort_key)
+
+        self.assertEqual(conv_and_sort(objs1), conv_and_sort(objs2))
+
+    def assertEqualListsOfPrimitivesAsSets(self, primitives1, primitives2):
+        """Check equal of the two built-in collections(list, tuple)."""
+        self.assertEqual(len(primitives1), len(primitives2))
+        for primitive in primitives1:
+            self.assertIn(primitive, primitives2)
+
+        for primitive in primitives2:
+            self.assertIn(primitive, primitives1)
