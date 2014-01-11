367a0f09d887a8a2f15a9cff48e5008876ee8c55
Doug Hellmann <doug.hellmann@dreamhost.com>
2014-01-11 00:10:20 +0000

    Enforce alphabetical ordering in requirements file
    Change-Id: Iaaa52acaa6005d9fbcd17b23523f61cea16e5a30

---
+++ tools/requirements_style_check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Enforce the requirement that dependencies are listed in the input
+# files in alphabetical order.
+
+# FIXME(dhellmann): This doesn't deal with URL requirements very
+# well. We should probably sort those on the egg-name, rather than the
+# full line.
+
+function check_file() {
+    typeset f=$1
+
+    # We don't care about comment lines.
+    grep -v '^#' $f > ${f}.unsorted
+    sort -i ${f}.unsorted > ${f}.sorted
+    diff -q ${f}.unsorted ${f}.sorted >/dev/null
+    rc=$?
+    rm -f ${f}.sorted ${f}.unsorted
+    return $rc
+}
+
+for filename in $@
+do
+    check_file $filename
+    if [ $? -ne 0 ]
+    then
+        echo "Please list requirements in $filename in alphabetical order" 1>&2
+        exit 1
+    fi
+done
