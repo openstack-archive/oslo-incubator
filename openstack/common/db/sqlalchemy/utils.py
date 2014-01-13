12b8662be952ecd04e8596f6da2be8be9e7add2b
Julia Varlamova <jvarlamova@mirantis.com>
2014-01-13 15:51:21 +0400

    Add index modifying methods
    Add to openstack/common/db/sqlalchemy/utils.py
    methods for modifying indexes:
    - add_index
    - drop_index
    - change_index_columns
    - index_exists
    - column_exists
    
    Add tests for these methods in UtilsOpportunisticTestCase
    to tests/unit/db/sqlalchemy/test_utils.py
    
    Change-Id: Ib7b2a1a8169fff803a925326703e71c014230a68

--- openstack/common/db/sqlalchemy/utils.py
+++ openstack/common/db/sqlalchemy/utils.py
@@ -497,3 +497,45 @@ def _change_deleted_column_type_to_id_type_sqlite(migrate_engine, table_name,
         where(new_table.c.deleted == deleted).\
         values(deleted=default_deleted_value).\
         execute()
+
+
+def _create_index(index_name, table, idx_columns):
+    return Index(
+        index_name, *[getattr(table.c, col) for col in idx_columns]
+    )
+
+
+def index_exists(migrate_engine, table_name, index_name):
+    inspector = reflection.Inspector.from_engine(migrate_engine)
+    indexes = inspector.get_indexes(table_name)
+    index_names = [index['name'] for index in indexes]
+    return index_name in index_names
+
+
+def add_index(migrate_engine, table_name, index_name, idx_columns):
+    table = get_table(migrate_engine, table_name)
+    if not index_exists(migrate_engine, table_name, index_name):
+        index = _create_index(index_name, table, idx_columns)
+        index.create()
+    else:
+        raise ValueError("Index '%s' already exists!" % index_name)
+
+
+def drop_index(migrate_engine, table_name, index_name, idx_columns):
+    table = get_table(migrate_engine, table_name)
+    if index_exists(migrate_engine, table_name, index_name):
+        index = _create_index(index_name, table, idx_columns)
+        index.drop()
+    else:
+        raise ValueError("Index '%s' not found!" % index_name)
+
+
+def change_index_columns(migrate_engine, table_name, index_name,
+                         new_columns, old_columns):
+    drop_index(migrate_engine, table_name, index_name, old_columns)
+    add_index(migrate_engine, table_name, index_name, new_columns)
+
+
+def column_exists(engine, table_name, column):
+    t = get_table(engine, table_name)
+    return column in t.c
