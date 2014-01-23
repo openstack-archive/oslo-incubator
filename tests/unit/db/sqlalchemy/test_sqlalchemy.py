e669feb26d3333ddde41821bf3065e425518ec1a
Roman Podoliaka <rpodolyaka@mirantis.com>
2014-01-23 18:42:56 +0200

    Don't use cfg.CONF in oslo.db
    Using of a global config object is something we want to avoid when
    making libraries. Parameters must be passed as function/class methods
    arguments instead.
    
    In order not to duplicate options declaration in each project, they
    have been put into separate options module (which should not be a
    part of oslo.db when it's released; we'll probably want to find a
    place for it within oslo-incubator).
    
    Partial-Bug: #1263908
    
    Change-Id: Ib5a4e31b4e78e2b4cb807a8c88b8072f4207eb22

--- tests/unit/db/sqlalchemy/test_sqlalchemy.py
+++ tests/unit/db/sqlalchemy/test_sqlalchemy.py
@@ -28,7 +28,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from openstack.common.db import exception as db_exc
 from openstack.common.db.sqlalchemy import models
 from openstack.common.db.sqlalchemy import session
-from openstack.common.fixture import config
 from openstack.common import test
 from tests.unit.db.sqlalchemy import base as test_base
 
@@ -41,88 +40,6 @@ class TmpTable(BASE, models.ModelBase):
     __tablename__ = _TABLE_NAME
     id = Column(Integer, primary_key=True)
     foo = Column(Integer)
-
-
-class SessionParametersTestCase(test_base.DbTestCase):
-
-    def setUp(self):
-        super(SessionParametersTestCase, self).setUp()
-        config_fixture = self.useFixture(config.Config())
-        self.conf = config_fixture.conf
-
-    def test_deprecated_session_parameters(self):
-        path = self.create_tempfiles([["tmp", """[DEFAULT]
-sql_connection=x://y.z
-sql_min_pool_size=10
-sql_max_pool_size=20
-sql_max_retries=30
-sql_retry_interval=40
-sql_max_overflow=50
-sql_connection_debug=60
-sql_connection_trace=True
-"""]])[0]
-        self.conf(['--config-file', path])
-        self.assertEqual(self.conf.database.connection, 'x://y.z')
-        self.assertEqual(self.conf.database.min_pool_size, 10)
-        self.assertEqual(self.conf.database.max_pool_size, 20)
-        self.assertEqual(self.conf.database.max_retries, 30)
-        self.assertEqual(self.conf.database.retry_interval, 40)
-        self.assertEqual(self.conf.database.max_overflow, 50)
-        self.assertEqual(self.conf.database.connection_debug, 60)
-        self.assertEqual(self.conf.database.connection_trace, True)
-
-    def test_session_parameters(self):
-        path = self.create_tempfiles([["tmp", """[database]
-connection=x://y.z
-min_pool_size=10
-max_pool_size=20
-max_retries=30
-retry_interval=40
-max_overflow=50
-connection_debug=60
-connection_trace=True
-pool_timeout=7
-"""]])[0]
-        self.conf(['--config-file', path])
-        self.assertEqual(self.conf.database.connection, 'x://y.z')
-        self.assertEqual(self.conf.database.min_pool_size, 10)
-        self.assertEqual(self.conf.database.max_pool_size, 20)
-        self.assertEqual(self.conf.database.max_retries, 30)
-        self.assertEqual(self.conf.database.retry_interval, 40)
-        self.assertEqual(self.conf.database.max_overflow, 50)
-        self.assertEqual(self.conf.database.connection_debug, 60)
-        self.assertEqual(self.conf.database.connection_trace, True)
-        self.assertEqual(self.conf.database.pool_timeout, 7)
-
-    def test_dbapi_database_deprecated_parameters(self):
-        path = self.create_tempfiles([['tmp', '[DATABASE]\n'
-                                      'sql_connection=fake_connection\n'
-                                      'sql_idle_timeout=100\n'
-                                      'sql_min_pool_size=99\n'
-                                      'sql_max_pool_size=199\n'
-                                      'sql_max_retries=22\n'
-                                      'reconnect_interval=17\n'
-                                      'sqlalchemy_max_overflow=101\n'
-                                      'sqlalchemy_pool_timeout=5\n'
-                                       ]])[0]
-        self.conf(['--config-file', path])
-        self.assertEqual(self.conf.database.connection, 'fake_connection')
-        self.assertEqual(self.conf.database.idle_timeout, 100)
-        self.assertEqual(self.conf.database.min_pool_size, 99)
-        self.assertEqual(self.conf.database.max_pool_size, 199)
-        self.assertEqual(self.conf.database.max_retries, 22)
-        self.assertEqual(self.conf.database.retry_interval, 17)
-        self.assertEqual(self.conf.database.max_overflow, 101)
-        self.assertEqual(self.conf.database.pool_timeout, 5)
-
-    def test_dbapi_database_deprecated_parameters_sql(self):
-        path = self.create_tempfiles([['tmp', '[sql]\n'
-                                      'connection=test_sql_connection\n'
-                                      'idle_timeout=99\n'
-                                       ]])[0]
-        self.conf(['--config-file', path])
-        self.assertEqual(self.conf.database.connection, 'test_sql_connection')
-        self.assertEqual(self.conf.database.idle_timeout, 99)
 
 
 class SessionErrorWrapperTestCase(test_base.DbTestCase):
