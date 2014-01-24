64647f3a18b37f8ff96c9df7e995490ec9e818a6
Florian Haas <florian@hastexo.com>
2014-01-24 11:15:17 +0100

    Make TRADITIONAL the default SQL mode
    Rather than log a warning when TRADITIONAL mode is not enabled,
    just make it the default. If this causes INSERTs or UPDATEs from
    applications to break, it just exposes that they are inserting
    broken data to begin with.
    
    Change-Id: I84c1781454ed5f46c7a135c96bafc575f313f9bc

--- openstack/common/db/sqlalchemy/session.py
+++ openstack/common/db/sqlalchemy/session.py
@@ -317,9 +317,11 @@ database_opts = [
                help='The SQLAlchemy connection string used to connect to the '
                     'slave database'),
     cfg.StrOpt('mysql_sql_mode',
-               help='The SQL mode to be used for MySQL sessions '
-                    '(default is empty, meaning do not override '
-                    'any server-side SQL mode setting)'),
+               default='TRADITIONAL',
+               help='The SQL mode to be used for MySQL sessions. '
+                    'Default is TRADITIONAL, overriding any server-side SQL '
+                    'mode setting. To use whatever SQL mode is set by the '
+                    'server configuration, set this to the empty string.'),
     cfg.IntOpt('idle_timeout',
                default=3600,
                deprecated_opts=[cfg.DeprecatedOpt('sql_idle_timeout',
