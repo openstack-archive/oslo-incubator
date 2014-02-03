8cac6d3f6b5fcd65ad573b251426bdba3552e672
Ilya Pekelny <ipekelny@mirantis.com>
2014-02-03 17:21:19 +0200

    Handle exception messages with `gettextutils`
    Handle exception messages with `gettextutils` tools instead of using
    string message as is. This is necessary becase a message can contain
    non-ascii characters and it can cause the unicode error:
    "UnicodeError: Message objects do not support str() because they may
    contain non-ascii characters. Please use unicode() or translate()
    instead."
    
    Change-Id: I3312b75731c1ce49e2f1069c83e1a868805409d3
    Fixes-Bug: 1275686

--- openstack/common/db/exception.py
+++ openstack/common/db/exception.py
@@ -23,19 +23,19 @@ class DBError(Exception):
     """Wraps an implementation specific exception."""
     def __init__(self, inner_exception=None):
         self.inner_exception = inner_exception
-        super(DBError, self).__init__(str(inner_exception))
+        super(DBError, self).__init__(_("%s") % inner_exception)
 
 
 class DBDuplicateEntry(DBError):
     """Wraps an implementation specific exception."""
     def __init__(self, columns=[], inner_exception=None):
         self.columns = columns
-        super(DBDuplicateEntry, self).__init__(inner_exception)
+        super(DBDuplicateEntry, self).__init__(_("%s") % inner_exception)
 
 
 class DBDeadlock(DBError):
     def __init__(self, inner_exception=None):
-        super(DBDeadlock, self).__init__(inner_exception)
+        super(DBDeadlock, self).__init__(_("%s") % inner_exception)
 
 
 class DBInvalidUnicodeParameter(Exception):
@@ -46,7 +46,7 @@ class DBInvalidUnicodeParameter(Exception):
 class DbMigrationError(DBError):
     """Wraps migration specific exception."""
     def __init__(self, message=None):
-        super(DbMigrationError, self).__init__(str(message))
+        super(DbMigrationError, self).__init__(_("%s") % message)
 
 
 class DBConnectionError(DBError):
