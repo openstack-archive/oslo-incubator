b19483671ade1ce9f8aa4e443bc7d2c1f2675fee
Zhongyue Luo <zhongyue.nah@intel.com>
2014-02-08 16:29:35 +0800

    Isolate utcnow mocking in the ManagerTestCase
    The mock of utcnow in the ManagerTestCase is affecting PeriodicTasksTestCase
    that the test result of
    "tox -epy26 unit.test_periodic" and
    "tox -epy26 unit.test_periodic.PeriodicTasksTestCase" are different.
    
    This patch isolates the mocking by removing set_time_override and
    using the mock.patch decorator on each test cases which needs it.
    
    Also this bug caused "doit_with_kwargs_odd" to be executed accidentally
    eventhough the task spacing was 10 seconds. This was fixed
    by adding the run_immediately argument as True to the function signature.
    
    Change-Id: I3385c8d313d58cbaad5c21347b2b7ce4a8aae6a7
    Closes-bug: #1277823

--- tests/unit/test_periodic.py
+++ tests/unit/test_periodic.py
@@ -20,10 +20,11 @@ Unit Tests for periodic_task decorator and PeriodicTasks class.
 
 import datetime
 
+import mock
+
 from openstack.common.fixture import config
 from openstack.common import periodic_task
 from openstack.common import test
-from openstack.common import timeutils
 from testtools import matchers
 
 
@@ -45,7 +46,7 @@ class AService(periodic_task.PeriodicTasks):
         self.called['urg'] = True
         raise AnException('urg')
 
-    @periodic_task.periodic_task(spacing=10)
+    @periodic_task.periodic_task(spacing=10, run_immediately=True)
     def doit_with_kwargs_odd(self, context):
         self.called['ticks'] = True
 
@@ -56,6 +57,7 @@ class PeriodicTasksTestCase(test.BaseTestCase):
     def test_is_called(self):
         serv = AService()
         serv.run_periodic_tasks(None)
+        # NOTE(zyluo): expect doit and crashit each to be called once.
         self.assertTrue(serv.called['doit'])
         self.assertTrue(len(serv.called) == 2)
 
@@ -63,8 +65,8 @@ class PeriodicTasksTestCase(test.BaseTestCase):
         serv = AService()
         serv.run_periodic_tasks(None)
         serv.run_periodic_tasks(None)
-        # expect doit_with_kwargs to be called twice
-        # and doit_with_kwargs_odd to be called once.
+        # NOTE(zyluo): expect doit, crashit, and doit_with_kwargs_odd
+        # each to be called once.
         self.assertTrue(len(serv.called) == 3)
 
     def test_raises(self):
@@ -133,9 +135,10 @@ class ManagerTestCase(test.BaseTestCase):
         idle = m.run_periodic_tasks(None)
         self.assertAlmostEqual(60, idle, 1)
 
-    def test_periodic_tasks_idle_calculation(self):
+    @mock.patch('openstack.common.timeutils.utcnow')
+    def test_periodic_tasks_idle_calculation(self, mock_utcnow):
         fake_time = datetime.datetime(3000, 1, 1)
-        timeutils.set_time_override(fake_time)
+        mock_utcnow.return_value = fake_time
 
         class Manager(periodic_task.PeriodicTasks):
 
@@ -161,16 +164,17 @@ class ManagerTestCase(test.BaseTestCase):
         self.assertEqual(10, m._periodic_spacing[task_name])
         self.assertIsNotNone(m._periodic_last_run[task_name])
 
-        timeutils.advance_time_delta(datetime.timedelta(seconds=5))
+        mock_utcnow.return_value = fake_time + datetime.timedelta(seconds=5)
         m.run_periodic_tasks(None)
 
-        timeutils.advance_time_delta(datetime.timedelta(seconds=5))
+        mock_utcnow.return_value = fake_time + datetime.timedelta(seconds=10)
         idle = m.run_periodic_tasks(None)
         self.assertAlmostEqual(10, idle, 1)
 
-    def test_periodic_tasks_immediate_runs_now(self):
+    @mock.patch('openstack.common.timeutils.utcnow')
+    def test_periodic_tasks_immediate_runs_now(self, mock_utcnow):
         fake_time = datetime.datetime(3000, 1, 1)
-        timeutils.set_time_override(fake_time)
+        mock_utcnow.return_value = fake_time
 
         class Manager(periodic_task.PeriodicTasks):
 
@@ -201,7 +205,7 @@ class ManagerTestCase(test.BaseTestCase):
                          m._periodic_last_run[task_name])
         self.assertAlmostEqual(10, idle, 1)
 
-        timeutils.advance_time_delta(datetime.timedelta(seconds=5))
+        mock_utcnow.return_value = fake_time + datetime.timedelta(seconds=5)
         idle = m.run_periodic_tasks(None)
         self.assertAlmostEqual(5, idle, 1)
 
