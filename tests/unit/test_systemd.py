a3d3fa47e5cac80ad1d274f6fa2441a1c61a129e
Alan Pevec <apevec@redhat.com>
2014-02-11 22:36:00 +0100

    notify calling process we are ready to serve
    Systemd notification should be sent in-process, otherwise systemd might
    miss the subprocess sending notification.
    See systemd bug https://bugzilla.redhat.com/show_bug.cgi?id=820448
    
    Taken from keystone project commit
    abc06716d027d68f0da3b0f559fa7c85a21804d5
    
    Improvements from Keystone version:
    
        * add unset_environment parameter
        New parameter unset_environment was added to sd_notify
        http://www.freedesktop.org/software/systemd/man/sd_notify.html
        to ensure service readiness is sent only once.
    
        * add onready() method to simulate systemd environment
        For testing purposes and optional use with SysV initscripts.
    
        * unit test added
    
        * docstrings for notification methods
    
    Patch includes deployment in openstack.common.service.
    It does not have an effect when running the service outside
    the systemd environment.
    
    Implements: blueprint service-readiness
    Change-Id: I80f325c9be9c171c2dc8d5526570bf64f0f87c78

---
+++ tests/unit/test_systemd.py
@@ -0,0 +1,56 @@
+# Copyright 2014 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mox
+import os
+import socket
+
+
+from openstack.common.fixture import moxstubout
+from openstack.common import systemd
+from openstack.common import test
+
+
+class SystemdTestCase(test.BaseTestCase):
+    """Test case for Systemd service readiness."""
+    def setUp(self):
+        super(SystemdTestCase, self).setUp()
+        self.mox = self.useFixture(moxstubout.MoxStubout()).mox
+        self.stubs = self.useFixture(moxstubout.MoxStubout()).stubs
+
+
+    def test_sd_notify(self):
+        self.ready = False
+
+        def mock_getenv(key):
+            return '@fake_socket'
+
+        def mock_socket(cls, *args):
+
+            class FakeSocket():
+                def connect(fs, socket): pass
+                def close(fs): pass
+                def sendall(fs, data):
+                    if (data == 'READY=1'):
+                        self.ready = True
+
+            return FakeSocket()
+
+        self.stubs.Set(os, 'getenv', mock_getenv)
+        self.stubs.Set(socket, 'socket', mock_socket)
+
+        systemd.notify()
+
+        self.assertEqual(self.ready, True)
+
