3b4c4ebd8407c7124ff0b5b434e2b8e5fa32ede9
James Carey <jecarey@us.ibm.com>
2014-02-16 16:23:48 +0000

    Config generator fails with lazy messages
    Entry points from modules loaded from projects may have help text
    that is i18n lazy enabled, which causes a failure because
    gettextutils.Message (a subclass of six.text_type) explicitly
    does not support concatenation.  Change from using concatenation
    to using format (%) to allow six.text_type to do the right thing.
    
    Change-Id: Ie60c569ae6fe227e16eef4a3281cf042732addc3
    Closes-Bug: #1280826

---
+++ tests/testmods/i18n_opt.py
@@ -0,0 +1,27 @@
+# Copyright 2014 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack.common import gettextutils
+from oslo.config import cfg
+
+CONF = cfg.CONF
+
+# Note that this is using the Message class directly instead of using
+# gettextutils.enable_lazy() because this isolates the use of
+# the Message class to this one instance instead of turning it on
+# for all subsequent tests
+opt = cfg.StrOpt('i18n', default="i18n",
+                 help=gettextutils.Message('helpful message'))
+
+CONF.register_opt(opt, group='i18n')
