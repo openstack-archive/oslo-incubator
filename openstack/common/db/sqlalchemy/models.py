8c55546407ef79544f1c88d1af055c0c89c35dfa
Shuangtai Tian <shuangtai.tian@intel.com>
2014-02-21 15:41:32 +0800

    Python3: define a __next__() method for ModelBase
    In Python 3, __next__() has replaced next().
    
    Blueprint: make-python3-compatible
    Change-Id: I96a7a4602be92cf78d4926bc4afd66bda9789395

--- openstack/common/db/sqlalchemy/models.py
+++ openstack/common/db/sqlalchemy/models.py
@@ -82,6 +82,9 @@ class ModelBase(object):
         n = six.advance_iterator(self._i)
         return n, getattr(self, n)
 
+    # In Python 3, __next__() has replaced next().
+    __next__ = next
+
     def update(self, values):
         """Make the model object behave like a dict."""
         for k, v in six.iteritems(values):
