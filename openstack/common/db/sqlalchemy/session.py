openstack/oslo.db

Add from_config() method to EngineFacade
Add a helper that allows to create an EngineFacade instance given an
oslo.config instance. This helper is completely optional, but it was
requested by a few people syncing the latest oslo.db code to target
projects.

Note, that oslo.config is mocked in tests, so oslo.db still has no
runtime dependency on oslo.config.

Blueprint: oslo-db-lib

Change-Id: Icab476386b859be0b0e984a94cbfcf3d581ab6d2

openstack/common/db/sqlalchemy/session.py
@@ -845,3 +845,36 @@ class EngineFacade(object):
                 del kwargs[arg]
 
         return self._session_maker(**kwargs)
+
+    @classmethod
+    def from_config(cls, connection_string, conf,
+                    sqlite_fk=False, mysql_sql_mode=None,
+                    autocommit=True, expire_on_commit=False):
+        """Initialize EngineFacade using oslo.config config instance options.
+
+        :param connection_string: SQLAlchemy connection string
+        :type connection_string: string
+
+        :param conf: oslo.config config instance
+        :type conf: oslo.config.cfg.ConfigOpts
+
+        :param sqlite_fk: enable foreign keys in SQLite
+        :type sqlite_fk: bool
+
+        :param mysql_sql_mode: set SQL mode in MySQL
+        :type mysql_sql_mode: string
+
+        :param autocommit: use autocommit mode for created Session instances
+        :type autocommit: bool
+
+        :param expire_on_commit: expire session objects on commit
+        :type expire_on_commit: bool
+
+        """
+
+        return cls(sql_connection=connection_string,
+                   sqlite_fk=sqlite_fk,
+                   mysql_sql_mode=mysql_sql_mode,
+                   autocommit=autocommit,
+                   expire_on_commit=expire_on_commit,
+                   **dict(conf.database.items()))

tests/unit/db/sqlalchemy/test_sqlalchemy.py
@@ -345,6 +345,39 @@ class EngineFacadeTestCase(test.BaseTestCase):
         self.assertFalse(ses.autocommit)
         self.assertTrue(ses.expire_on_commit)
 
+    @mock.patch('openstack.common.db.sqlalchemy.session.get_maker')
+    @mock.patch('openstack.common.db.sqlalchemy.session.create_engine')
+    def test_creation_from_config(self, create_engine, get_maker):
+        conf = mock.MagicMock()
+        conf.database.items.return_value = [
+            ('connection_debug', 100),
+            ('max_pool_size', 10)
+        ]
+
+        session.EngineFacade.from_config('sqlite:///:memory:', conf,
+                                         autocommit=False,
+                                         expire_on_commit=True,
+                                         mysql_sql_mode='TRADITIONAL')
+
+        conf.database.items.assert_called_once_with()
+        create_engine.assert_called_once_with(
+            sql_connection='sqlite:///:memory:',
+            connection_debug=100,
+            max_pool_size=10,
+            mysql_sql_mode='TRADITIONAL',
+            sqlite_fk=False,
+            idle_timeout=mock.ANY,
+            retry_interval=mock.ANY,
+            max_retries=mock.ANY,
+            max_overflow=mock.ANY,
+            connection_trace=mock.ANY,
+            sqlite_synchronous=mock.ANY,
+            pool_timeout=mock.ANY,
+        )
+        get_maker.assert_called_once_with(engine=create_engine(),
+                                          autocommit=False,
+                                          expire_on_commit=True)
+
 
 class SetSQLModeTestCase(test_log.LogTestBase):
     def setUp(self):

openstack-archive/oslo-incubator

openstack/common/db/sqlalchemy/session.py
@@ -858,3 +858,36 @@ class EngineFacade(object):
                 del kwargs[arg]
 
         return self._session_maker(**kwargs)
+
+    @classmethod
+    def from_config(cls, connection_string, conf,
+                    sqlite_fk=False, mysql_sql_mode=None,
+                    autocommit=True, expire_on_commit=False):
+        """Initialize EngineFacade using oslo.config config instance options.
+
+        :param connection_string: SQLAlchemy connection string
+        :type connection_string: string
+
+        :param conf: oslo.config config instance
+        :type conf: oslo.config.cfg.ConfigOpts
+
+        :param sqlite_fk: enable foreign keys in SQLite
+        :type sqlite_fk: bool
+
+        :param mysql_sql_mode: set SQL mode in MySQL
+        :type mysql_sql_mode: string
+
+        :param autocommit: use autocommit mode for created Session instances
+        :type autocommit: bool
+
+        :param expire_on_commit: expire session objects on commit
+        :type expire_on_commit: bool
+
+        """
+
+        return cls(sql_connection=connection_string,
+                   sqlite_fk=sqlite_fk,
+                   mysql_sql_mode=mysql_sql_mode,
+                   autocommit=autocommit,
+                   expire_on_commit=expire_on_commit,
+                   **dict(conf.database.iteritems()))

tests/unit/db/sqlalchemy/test_sqlalchemy.py
@@ -304,6 +304,39 @@ class EngineFacadeTestCase(test.BaseTestCase):
         self.assertFalse(ses.autocommit)
         self.assertTrue(ses.expire_on_commit)
 
+    @mock.patch('openstack.common.db.sqlalchemy.session.get_maker')
+    @mock.patch('openstack.common.db.sqlalchemy.session.create_engine')
+    def test_creation_from_config(self, create_engine, get_maker):
+        conf = mock.MagicMock()
+        conf.database.iteritems.return_value = [
+            ('connection_debug', 100),
+            ('max_pool_size', 10)
+        ]
+
+        session.EngineFacade.from_config('sqlite:///:memory:', conf,
+                                         autocommit=False,
+                                         expire_on_commit=True,
+                                         mysql_sql_mode='TRADITIONAL')
+
+        conf.database.iteritems.assert_called_once_with()
+        create_engine.assert_called_once_with(
+            sql_connection='sqlite:///:memory:',
+            connection_debug=100,
+            max_pool_size=10,
+            mysql_sql_mode='TRADITIONAL',
+            sqlite_fk=False,
+            idle_timeout=mock.ANY,
+            retry_interval=mock.ANY,
+            max_retries=mock.ANY,
+            max_overflow=mock.ANY,
+            connection_trace=mock.ANY,
+            sqlite_synchronous=mock.ANY,
+            pool_timeout=mock.ANY,
+        )
+        get_maker.assert_called_once_with(engine=create_engine(),
+                                          autocommit=False,
+                                          expire_on_commit=True)
+
 
 class SetSQLModeTestCase(test_log.LogTestBase):
     def setUp(self):