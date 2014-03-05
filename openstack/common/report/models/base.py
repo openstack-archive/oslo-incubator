487ef3efbca95bb967de7c8e8dfa8bc732bf46ef
Solly Ross <sross@redhat.com>
2014-03-05 18:40:49 -0500

    Report: Propagate view_type through collections
    Previously, if a model contained a property with a
    collection (sequence or mapping) of models, set_current_view_type
    would not propagate the current_view_type to those models.
    
    This change allows the set_current_view_type to descend recursively
    throught not just models, but also arbitrary sequences and mappings
    to set the view type.
    
    Change-Id: I3701b7b33195038ccc6b57456bfd0e732c32bbda

--- openstack/common/report/models/base.py
+++ openstack/common/report/models/base.py
@@ -24,6 +24,8 @@ the report serialization process.
 import collections as col
 import copy
 
+import six
+
 
 class ReportModel(col.MutableMapping):
     """A Report Data Model
@@ -119,14 +121,36 @@ class ReportModel(col.MutableMapping):
 
         This method attempts to set the current view
         type for this model and all submodels by calling
-        itself recursively on all values (and ignoring the
-        ones that are not themselves models)
+        itself recursively on all values, traversing
+        intervening sequences and mappings when possible,
+        and ignoring all other objects.
 
         :param tp: the type of the view ('text', 'json', 'xml', etc)
         """
 
-        for key in self:
-            try:
-                self[key].set_current_view_type(tp)
-            except AttributeError:
-                pass
+        memo = {}
+
+        def traverse_obj(obj):
+            oid = id(obj)
+
+            # don't die on recursive structures,
+            # and don't treat strings like sequences
+            if not memo.get(oid) or not isinstance(obj, six.string_types):
+                if isinstance(obj, col.Sequence):
+                    memo[oid] = True
+                    for item in obj:
+                        try:
+                            item.set_current_view_type(tp)
+                        except AttributeError:
+                            traverse_obj(item)
+                elif isinstance(obj, col.Mapping):
+                    memo[oid] = True
+                    for key in obj:
+                        try:
+                            obj[key].set_current_view_type(tp)
+                        except AttributeError:
+                            traverse_obj(obj[key])
+
+                # otherwise, do nothing
+
+        traverse_obj(self)
