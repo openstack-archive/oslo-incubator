3304b03c9aa3f7d90f76e2eb54bd4436901d301d
Solly Ross <sross@redhat.com>
2014-03-05 15:47:30 -0500

    Report: Support Sequences and Mappings as Data
    Previously, the ReportModel class assumed that data passed in
    was a dict, without actually checking.  This could lead to errors
    if anything else was passed in.  As it was likely one would want
    to pass in arbitrary sequences (like lists) and mappings, support
    was added.
    
    It works as such:
    - a MutableMapping is set directly as model.data
    - a Mapping is wrapped with a COWMapping, which does copy-on-write
      to make a MutableMapping
    - a MutableSequence is wrapped with a MutableSequenceToMappingProxy,
      which uses sequence indicies as keys
    - a Sequence is wrapped with a SequenceToMappingProxy, which uses
      sequence indicies as key, and that is then wrapped with a
      COWMapping (as described above)
    
    Finally, instances of manually converting an array to a dict were
    removed.
    
    Change-Id: Ibcf7d01d69ac21f9ad8c3dcf604733e88122c8f4

--- openstack/common/report/utils.py
+++ openstack/common/report/utils.py
@@ -18,6 +18,8 @@ This module includes various utilities
 used in generating reports.
 """
 
+import collections as col
+import copy
 import gc
 
 
@@ -26,6 +28,116 @@ class StringWithAttrs(str):
     """
 
     pass
+
+
+class SequenceToMappingProxy(col.Mapping):
+    """A proxy from a sequence to a mapping using indicies as keys."""
+
+    def __init__(self, underlying_seq):
+        self.seq = underlying_seq
+
+    def __getitem__(self, key):
+        try:
+            return self.seq[key]
+        except TypeError:
+            raise KeyError(key)
+
+    def __len__(self):
+        return len(self.seq)
+
+    def __iter__(self):
+        i = 0
+        while i < len(self.seq):
+            yield i
+            i += 1
+
+    def __repr__(self):
+        return '<SequenceToMappingProxy %s>' % repr(self.seq)
+
+    def __deepcopy__(self, memodict):
+        return dict((k, copy.deepcopy(self[k], memodict)) for k in self)
+
+    def __copy__(self):
+        return SequenceToMappingProxy(self.seq)
+
+
+class MutableSequenceToMappingProxy(SequenceToMappingProxy,
+                                    col.MutableMapping):
+    """A proxy from a mutable sequence to a mutable mapping."""
+    def __setitem__(self, key, value):
+        if not isinstance(key, int):
+            raise TypeError(('list indicies must be integers, '
+                             ' not %s') % type(key).__name__)
+
+        self.seq[key] = value
+
+    def __delitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError(('list indicies must be integers, '
+                             ' not %s') % type(key).__name__)
+
+        del self.seq[key]
+
+
+class COWMapping(col.MutableMapping):
+    """A Copy-On-Write mapping."""
+
+    def __init__(self, underlying_mapping):
+        self.old_data = underlying_mapping
+        self.new_data = {}
+        self.deleted_items = {}
+
+    def __getitem__(self, key):
+        try:
+            return self.new_data[key]
+        except KeyError:
+            return self.old_data[key]
+
+    def __setitem__(self, key, value):
+        self.new_data[key] = value
+        try:
+            del self.deleted_items[key]
+        except KeyError:
+            pass
+
+    def __delitem__(self, key):
+        try:
+            del self.new_data[key]
+        except KeyError:
+            if not self.old_data.has_item(key):
+                raise KeyError(key)
+
+            self.deleted_items[key] = 1
+
+    def __len__(self):
+        union_len = len(dict(self.old_items, **self.new_items))
+        return union_len - len(self.deleted_items)
+
+    def __iter__(self):
+        for k in self.old_data.keys():
+            if k in self.deleted_items:
+                next
+            yield k
+        for k in self.new_data.keys():
+            if k not in self.old_data:
+                yield k
+
+    def __repr__(self):
+        res = '<COWMapping {'
+        res += ', '.join(('%s: %s' % (k, v)) for k, v in self)
+        res += '}>'
+        return res
+
+    def __copy__(self):
+        res = COWMapping(self.old_data)
+        res.new_data = self.new_data
+        res.deleted_items = self.deleted_items
+        return res
+
+    def __deepcopy__(self, memodict):
+        return dict(
+            (copy.deepcopy(k, memodict), copy.deepcopy(self[k], memodict))
+            for k, v in self.items())
 
 
 def _find_objects(t):
