f57b1f3fc699f65c3527728fc20489b66a01d97b
Solly Ross <sross@redhat.com>
2014-03-05 18:51:26 -0500

    Report: Add basic processes section
    This commit adds a section that shows basic information about the
    current processes and all subprocesses (recursively).  Process
    information is collected using the psutil library, a cross-platform
    library for collection process information.
    
    Change-Id: I8d0f828bcd85f4b04f99653a712061757cbdda86

--- openstack/common/report/guru_meditation_report.py
+++ openstack/common/report/guru_meditation_report.py
@@ -55,6 +55,7 @@ import signal
 import sys
 
 from openstack.common.report.generators import conf as cgen
+from openstack.common.report.generators import process as prgen
 from openstack.common.report.generators import threading as tgen
 from openstack.common.report.generators import version as pgen
 from openstack.common.report import report
@@ -147,6 +148,9 @@ class GuruMeditation(object):
         self.add_section('Green Threads',
                          tgen.GreenThreadReportGenerator())
 
+        self.add_section('Processes',
+                         prgen.ProcessReportGenerator())
+
         self.add_section('Configuration',
                          cgen.ConfigReportGenerator())
 
@@ -175,6 +179,8 @@ class TextGuruMeditation(GuruMeditation, report.TextReport):
     - Threads List
 
     - Green Threads List
+
+    - Process List
 
     - Configuration Options
 
