f14e1c06a9a5afe60631085a8818772cac02d5fa
Brant Knudson <bknudson@us.ibm.com>
2014-03-05 16:45:36 -0600

    Fix excessive logging from db.sqlalchemy.session
    A message about the MySQL server mode was logged at the info level
    every time a connection was checked out. This happens often, and
    logging every time is unnecessary.
    
    Here's an example of the message when using the default
    configuration:
    
     MySQL server mode set to STRICT_TRANS_TABLES,STRICT_ALL_TABLES,...
    
    The fix is to send the message once per engine creation.
    
    Closes-Bug: #1288443
    
    Change-Id: I13b24d04c28e69a8534aaa8d3a4463ca1d65570f

--- openstack/common/db/sqlalchemy/session.py
+++ openstack/common/db/sqlalchemy/session.py
@@ -538,15 +538,34 @@ def _set_session_sql_mode(dbapi_con, connection_rec,
     if sql_mode is not None:
         cursor.execute("SET SESSION sql_mode = %s", [sql_mode])
 
-    # Check against the real effective SQL mode. Even when unset by
+
+def _mysql_get_effective_sql_mode(engine):
+    """Returns the effective SQL mode for connections from the engine pool.
+
+    Returns ``None`` if the mode isn't available, otherwise returns the mode.
+
+    """
+    dbapi_con = engine.pool.connect()
+    cursor = dbapi_con.cursor()
+
+    # Get the real effective SQL mode. Even when unset by
     # our own config, the server may still be operating in a specific
     # SQL mode as set by the server configuration
     cursor.execute("SHOW VARIABLES LIKE 'sql_mode'")
     row = cursor.fetchone()
     if row is None:
+        return
+    return row[1]
+
+
+def _mysql_check_effective_sql_mode(engine):
+    """Logs a message based on the effective SQL mode for MySQL connections."""
+    realmode = _mysql_get_effective_sql_mode(engine)
+
+    if realmode is None:
         LOG.warning(_LW('Unable to detect effective SQL mode'))
         return
-    realmode = row[1]
+
     LOG.info(_LI('MySQL server mode set to %s') % realmode)
     # 'TRADITIONAL' mode enables several other modes, so
     # we need a substring match here
@@ -555,6 +574,13 @@ def _set_session_sql_mode(dbapi_con, connection_rec,
         LOG.warning(_LW("MySQL SQL mode is '%s', "
                         "consider enabling TRADITIONAL or STRICT_ALL_TABLES")
                     % realmode)
+
+
+def _mysql_set_callback(engine, sql_mode):
+    mode_callback = functools.partial(_set_session_sql_mode,
+                                      sql_mode=sql_mode)
+    sqlalchemy.event.listen(engine, 'checkout', mode_callback)
+    _mysql_check_effective_sql_mode(engine)
 
 
 def _is_db_connection_error(args):
@@ -632,9 +658,7 @@ def create_engine(sql_connection, sqlite_fk=False, mysql_sql_mode=None,
             if mysql_traditional_mode:
                 mysql_sql_mode = 'TRADITIONAL'
             if mysql_sql_mode:
-                mode_callback = functools.partial(_set_session_sql_mode,
-                                                  sql_mode=mysql_sql_mode)
-                sqlalchemy.event.listen(engine, 'checkout', mode_callback)
+                _mysql_set_callback(engine, mysql_sql_mode)
     elif 'sqlite' in connection_dict.drivername:
         if not sqlite_synchronous:
             sqlalchemy.event.listen(engine, 'connect',
