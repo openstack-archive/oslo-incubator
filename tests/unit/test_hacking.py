205d14d0ac9f567ad1b688d8e8df04750d9fff39
ChangBo Guo(gcb) <eric.guo@easystack.cn>
2014-05-02 17:06:11 +0800

    Add check import style hacking in gate
    update.py scans 'from openstack.common.*' to replace base name while
    syncing, currently only module report uses another import style
    'import openstack.common.*'. As discussed, we changed the import style
    of module report in Ife98dc96b012d6aa7b5bd10d09a733506072fa1c. This
    patch adds local hacking and hacking unit test.
    
    Related-Bug: #1304774
    Implements blueprint oslo-local-hacking
    Change-Id: I29caef5faff2ec7b2eebd5c61bcf39f91e6e9265

---
+++ tests/unit/test_hacking.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2014 EasyStack, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslotest import base as test_base
+
+from openstack.hacking import checks
+
+
+class HackingTestCase(test_base.BaseTestCase):
+
+    def test_no_import_from(self):
+        self.assertEqual([], list(checks.no_import_from(
+            "from openstack.common.apiclient import exceptions")))
+
+        self.assertEqual([], list(checks.no_import_from(
+            "from openstack.common import log as logging")))
+
+        self.assertEqual(1, len(list(checks.no_import_from(
+            "import openstack.common.report.generators.conf as cgen"))))
