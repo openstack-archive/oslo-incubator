e51cb5d5dcda4a266f61ed84a43505706f30137c
Doug Hellmann <doug.hellmann@dreamhost.com>
2014-05-07 07:19:49 -0700

    Add script to build gerrit queries
    We have a few queries in the wiki for finding reviews in gerrit [1].
    Keeping the links up to date as we add new repositories is a bit
    of a chore, which this script makes easier.
    
    1: https://wiki.openstack.org/wiki/Oslo#Review_Links
    
    Change-Id: I65968cfd6b70bef7ded8384e317c17dbd293d8c7

---
+++ tools/make_review_links.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Build useful gerrit query links for the wiki page.
+"""
+
+from __future__ import print_function
+
+import argparse
+
+import yaml
+
+BASE_URL = 'https://review.openstack.org/#'
+
+#label:Code-Review-1
+OPEN = 'status:open'
+VERIFIED = 'label:Verified=1'
+NO_MINUS = '-label:Code-Review-1+-label:Code-Review-2+-label:Workflow-1'
+PLUS_2 = 'label:Code-Review=2'
+NO_PLUS = '-label:Code-Review%253D2+-label:Code-Review%253D1'
+
+LINKS = [
+    ('Potential+Merges', [OPEN, VERIFIED, NO_MINUS, PLUS_2]),
+    ('No+Objections', [OPEN, VERIFIED, NO_MINUS]),
+    ('No+Reviews', [OPEN, VERIFIED, NO_MINUS, NO_PLUS])
+]
+
+
+def make_title(s, level):
+    p = '=' * level
+    return p + ' ' + s + ' ' + p
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--infile',
+        default='../governance/reference/programs.yaml',
+        help='programs.yaml file',
+    )
+    parser.add_argument(
+        '--title-level',
+        type=int,
+        default=3,
+        help='wiki title level',
+    )
+    args = parser.parse_args()
+
+    programs = yaml.load(open(args.infile, 'r'))
+    program = programs['Common Libraries']
+
+    all_repos = [pr['repo'] for pr in program['projects']]
+
+    # Get the list of repos that do not match the ^.*oslo.* pattern
+    repos = [r for r in all_repos if 'oslo' not in pr['repo']]
+    # use a regex for the ones we can, to reduce URL length
+    repos.append('^.*oslo.*')
+
+    print(make_title('Review Links', args.title_level))
+    print()
+
+    sections = [
+        '%s=%s' % (
+            title,
+            (
+                '+'.join(extra) +
+                '+(' + '+OR+'.join('project:' + p for p in repos) + ')'
+            )
+        )
+        for title, extra in LINKS
+    ]
+    url = BASE_URL + '/dashboard/?title=Oslo+Dashboard&' + '&'.join(sections)
+    print('* [%(url)s Oslo Review Dashboard]' % {
+        'url': url,
+    })
+
+    url = BASE_URL + '/dashboard/?title=Personal+Oslo+Dashboard&' + '&'.join(
+        '%s+-owner:self' % s for s in sections
+    )
+    print('* [%(url)s Personal Oslo Review Dashboard]' % {
+        'url': url,
+    })
+
+    print('* Open Reviews by Project')
+    for r in all_repos:
+        url = BASE_URL + '/q/' + OPEN + '+' + 'project:' + r + ',n,z'
+        print('** [%(url)s %(repo)s]' % {
+            'url': url,
+            'repo': r,
+        })
+
+if __name__ == '__main__':
+    main()
