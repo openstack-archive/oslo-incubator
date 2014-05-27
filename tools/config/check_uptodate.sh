1f848ca3fb167e767bbeef543399d42264278db6
Matthew Treinish <mtreinish@kortar.org>
2014-05-27 09:35:43 -0400

    Make script to run in error msg configurable
    This commit adds new variables to the check_uptodate.sh script to set
    the error message if the sample is out of date. One of the new
    variables SCRIPT_PATH can be set as an env variable to specify
    alternate scripts to run to reset the config sample.
    
    This is needed by Tempest for example because it wraps the oslo
    version of generate_sample.sh to set env variables needed to load all
    the config options.
    
    Change-Id: Ibdeaf981364b3e63f5e23c2de2c3717821f0ee8f

--- tools/config/check_uptodate.sh
+++ tools/config/check_uptodate.sh
@@ -2,6 +2,9 @@
 
 PROJECT_NAME=${PROJECT_NAME:-oslo}
 CFGFILE_NAME=${PROJECT_NAME}.conf.sample
+ERROR_MSG_START="${0##*/}: ${PROJECT_NAME}.conf.sample is not up to date."
+SCRIPT_PATH=${SCRIPT_PATH:-"${0%%${0##*/}}generate_sample.sh."}
+ERROR_MSG=$ERROR_MSG_START"${0##*/}: Please run "$SCRIPT_PATH
 
 if [ -e etc/${PROJECT_NAME}/${CFGFILE_NAME} ]; then
     CFGFILE=etc/${PROJECT_NAME}/${CFGFILE_NAME}
@@ -19,7 +22,6 @@ tools/config/generate_sample.sh -b ./ -p ${PROJECT_NAME} -o ${TEMPDIR}
 
 if ! diff -u ${TEMPDIR}/${CFGFILE_NAME} ${CFGFILE}
 then
-   echo "${0##*/}: ${PROJECT_NAME}.conf.sample is not up to date."
-   echo "${0##*/}: Please run ${0%%${0##*/}}generate_sample.sh."
+   echo $ERROR_MSG
    exit 1
 fi
