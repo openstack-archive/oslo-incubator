6db41966c9bff3e1d59874c6782a2d0928e6cf24
Ken'ichi Ohmichi <oomichi@mxs.nes.nec.co.jp>
2014-06-06 14:00:10 +0900

    Add URI validation method
    There is the library 'rfc3987' which can validate URI data format, but
    the library cannot be used for OpenStack due to the license.
    This patch adds URI validation method based on rfc3986 and the method
    will be used in jsonschema validation in some OpenStack projects.
    
    Change-Id: I36b5c7c03348359eb7b99c331d24a5e439f2d149
    Related-Bug: #1326599

---
+++ tests/unit/test_uriutils.py
@@ -0,0 +1,60 @@
+# Copyright 2014 NEC Corporation. All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslotest import base as test_base
+
+from openstack.common import uriutils
+
+
+class UriUtilsTest(test_base.BaseTestCase):
+
+    def test_is_valid_uri_with_host_only(self):
+        self.assertTrue(uriutils.is_valid_uri('http://localhost'))
+        self.assertTrue(uriutils.is_valid_uri('ftp://localhost'))
+        self.assertTrue(uriutils.is_valid_uri('http://192.168.0.100'))
+        self.assertTrue(uriutils.is_valid_uri('http://[2001:db8::9abc]'))
+
+    def test_is_valid_uri_with_port(self):
+        self.assertTrue(uriutils.is_valid_uri('http://localhost:8080'))
+        self.assertTrue(uriutils.is_valid_uri('http://localhost:65535'))
+
+    def test_is_valid_uri_with_userinfo(self):
+        self.assertTrue(uriutils.is_valid_uri('http://foo@localhost'))
+
+    def test_is_valid_uri_with_path(self):
+        self.assertTrue(uriutils.is_valid_uri('http://localhost/'))
+        self.assertTrue(uriutils.is_valid_uri('http://localhost/foo'))
+        self.assertTrue(uriutils.is_valid_uri('http://localhost/foo/bar'))
+
+    def test_is_valid_uri_with_query(self):
+        self.assertTrue(uriutils.is_valid_uri('http://localhost?foo=bar'))
+
+    def test_is_valid_uri_with_fragment(self):
+        self.assertTrue(uriutils.is_valid_uri('http://localhost#nose'))
+
+    def test_is_valid_uri_with_all(self):
+        uri = 'http://user@localhost:8080/foo/bar?foo=bar#nose'
+        self.assertTrue(uriutils.is_valid_uri(uri))
+        uri = 'urn:example:animal:ferret:nose'
+        self.assertTrue(uriutils.is_valid_uri(uri))
+
+    def test_is_valid_uri_with_invalid_scheme(self):
+        self.assertFalse(uriutils.is_valid_uri('0ttp://localhost'))
+
+    def test_is_valid_uri_with_invalid_host(self):
+        self.assertFalse(uriutils.is_valid_uri('http://local\host'))
+        self.assertFalse(uriutils.is_valid_uri('http://localhost:80:80:80'))
+
+    def test_is_valid_uri_with_invalid_port(self):
+        self.assertFalse(uriutils.is_valid_uri('http://localhost:65536'))
