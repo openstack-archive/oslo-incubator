4835768bb5a91c359ca46e2ebb7119115ebf0c02
Thomas Herve <thomas.herve@enovance.com>
2014-06-11 17:44:45 +0200

    Add a RequestContext.from_dict method
    This adds a new methods allowing the creation of a RequestContext from a
    dict, useful when passing it serialized.
    
    Change-Id: Id66aba0c600896521a4a236e0a97859735419357

--- tests/unit/test_context.py
+++ tests/unit/test_context.py
@@ -27,3 +27,31 @@ class ContextTest(test_base.BaseTestCase):
     def test_admin_context_show_deleted_flag_default(self):
         ctx = context.get_admin_context()
         self.assertFalse(ctx.show_deleted)
+
+    def test_from_dict(self):
+        dct = {
+            "auth_token": "token1",
+            "user": "user1",
+            "tenant": "tenant1",
+            "domain": "domain1",
+            "user_domain": "user_domain1",
+            "project_domain": "project_domain1",
+            "is_admin": True,
+            "read_only": True,
+            "show_deleted": True,
+            "request_id": "request1",
+            "instance_uuid": "instance1",
+            "extra_data": "foo"
+        }
+        ctx = context.RequestContext.from_dict(dct)
+        self.assertEqual("token1", ctx.auth_token)
+        self.assertEqual("user1", ctx.user)
+        self.assertEqual("tenant1", ctx.tenant)
+        self.assertEqual("domain1", ctx.domain)
+        self.assertEqual("user_domain1", ctx.user_domain)
+        self.assertEqual("project_domain1", ctx.project_domain)
+        self.assertTrue(ctx.is_admin)
+        self.assertTrue(ctx.read_only)
+        self.assertTrue(ctx.show_deleted)
+        self.assertEqual("request1", ctx.request_id)
+        self.assertEqual("instance1", ctx.instance_uuid)
