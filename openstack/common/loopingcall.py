32565dec301669279962991914ed062825e0ea70
Zhongyue Luo <zhongyue.nah@intel.com>
2014-06-30 15:39:44 +0800

    Use timestamp in loopingcall
    All time related data in loopingcall are in seconds.
    Therefore creating datetime objects only adds overhead to the module.
    This patch replaces all datetime objects with utc timestamps in loopingcall.
    A lambda function returning time.time() was placed due to mock collisions
    with time.time() called in the logging module.
    Changes to test scripts were made accordingly.
    
    Change-Id: I38749f3c7bfff23164481b2341d56154ba737765

--- openstack/common/loopingcall.py
+++ openstack/common/loopingcall.py
@@ -16,15 +16,17 @@
 #    under the License.
 
 import sys
+import time
 
 from eventlet import event
 from eventlet import greenthread
 
 from openstack.common.gettextutils import _LE, _LW
 from openstack.common import log as logging
-from openstack.common import timeutils
 
 LOG = logging.getLogger(__name__)
+
+_ts = lambda: time.time()
 
 
 class LoopingCallDone(Exception):
@@ -72,15 +74,15 @@ class FixedIntervalLoopingCall(LoopingCallBase):
 
             try:
                 while self._running:
-                    start = timeutils.utcnow()
+                    start = _ts()
                     self.f(*self.args, **self.kw)
-                    end = timeutils.utcnow()
+                    end = _ts()
                     if not self._running:
                         break
-                    delay = interval - timeutils.delta_seconds(start, end)
+                    delay = interval + start - end
                     if delay <= 0:
                         LOG.warn(_LW('task %(func_name)s run outlasted '
-                                     'interval by %(delay)s sec'),
+                                     'interval by %(delay).2f sec'),
                                  {'func_name': repr(self.f), 'delay': -delay})
                     greenthread.sleep(delay if delay > 0 else 0)
             except LoopingCallDone as e:
