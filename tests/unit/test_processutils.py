72bd40c1d5b32c087a9c45fbc03ddbfe07a23953
Amrith Kumar <amrith@tesora.com>
2014-07-24 21:36:30 -0400

    Log stdout, stderr and command on execute() error
    It would help debugging considerably if a failure in execute()
    generated a message (at a client specified logging level). Such a
    message, generated by execute() would only occur if the user requested
    it by specifying a parameter (log_errors), and execute() were to throw
    an exception. As an example, this change would address the issue being
    addressed in bug 1340124 for the Trove project only.
    
    DocImpact: New parameter added to processutils.execute().  The
    possible values of the log_errors parameter are 'all' or 'final'. The
    default value of None implies no logging. If it is set to 'final',
    stdout and stderr are logged on the final failure. If it is set to
    'all', stdout and stderr are logged on every failure.
    
    DocImpact:
    Change-Id: If8180e042e1a05374ef705f5873cc0981243619f
    Closes-Bug: #1342857
    Suggested-By: Peter Stachowski <peter@tesora.com>
    Reported-By: Ionuț Arțăriși <iartarisi@suse.cz>

--- tests/unit/test_processutils.py
+++ tests/unit/test_processutils.py
@@ -28,6 +28,8 @@ from oslotest import base as test_base
 import six
 
 from openstack.common import processutils
+PROCESS_EXECUTION_ERROR_LOGGING_TEST = """#!/bin/bash
+exit 41"""
 
 TEST_EXCEPTION_AND_MASKING_SCRIPT = """#!/bin/bash
 # This is to test stdout and stderr
@@ -250,6 +252,65 @@ grep foo
                                             'password="***"',
                                             'something']))
         self.assertNotIn('secret', str(err))
+
+
+class ProcessExecutionErrorLoggingTest(test_base.BaseTestCase):
+    def setUp(self):
+        super(ProcessExecutionErrorLoggingTest, self).setUp()
+        self.tmpfilename = self.create_tempfiles(
+            [["process_execution_error_logging_test",
+              PROCESS_EXECUTION_ERROR_LOGGING_TEST]],
+            ext='bash')[0]
+
+        os.chmod(self.tmpfilename, (stat.S_IRWXU | stat.S_IRGRP |
+                                    stat.S_IXGRP | stat.S_IROTH |
+                                    stat.S_IXOTH))
+
+    def _test_and_check(self, log_errors=None, attempts=None):
+        fixture = self.useFixture(fixtures.FakeLogger(level=logging.DEBUG))
+        kwargs = {}
+
+        if log_errors:
+            kwargs.update({"log_errors": log_errors})
+
+        if attempts:
+            kwargs.update({"attempts": attempts})
+
+        err = self.assertRaises(processutils.ProcessExecutionError,
+                                processutils.execute,
+                                self.tmpfilename,
+                                **kwargs)
+
+        self.assertEqual(41, err.exit_code)
+        self.assertIn(self.tmpfilename, fixture.output)
+
+    def test_with_invalid_log_errors(self):
+        self.assertRaises(processutils.InvalidArgumentError,
+                          processutils.execute,
+                          self.tmpfilename,
+                          log_errors='invalid')
+
+    def test_with_log_errors_NONE(self):
+        self._test_and_check(log_errors=None, attempts=None)
+
+    def test_with_log_errors_final(self):
+        self._test_and_check(log_errors=processutils.LOG_FINAL_ERROR,
+                             attempts=None)
+
+    def test_with_log_errors_all(self):
+        self._test_and_check(log_errors=processutils.LOG_ALL_ERRORS,
+                             attempts=None)
+
+    def test_multiattempt_with_log_errors_NONE(self):
+        self._test_and_check(log_errors=None, attempts=3)
+
+    def test_multiattempt_with_log_errors_final(self):
+        self._test_and_check(log_errors=processutils.LOG_FINAL_ERROR,
+                             attempts=3)
+
+    def test_multiattempt_with_log_errors_all(self):
+        self._test_and_check(log_errors=processutils.LOG_ALL_ERRORS,
+                             attempts=3)
 
 
 def fake_execute(*cmd, **kwargs):
