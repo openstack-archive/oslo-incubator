aa744115b8834ff1ffc55166d7fc51c4b63cc5af
Julien Danjou <julien@danjou.info>
2014-08-19 10:40:25 +0200

    log: add missing space in error message
    Change-Id: I7113bbf329663f460419c059d311d6563b2612e4

--- openstack/common/log.py
+++ openstack/common/log.py
@@ -554,7 +554,7 @@ def _setup_logging_from_conf(project, version):
                 syslog = logging.handlers.SysLogHandler(facility=facility)
             log_root.addHandler(syslog)
         except socket.error:
-            log_root.error('Unable to add syslog handler. Verify that syslog'
+            log_root.error('Unable to add syslog handler. Verify that syslog '
                            'is running.')
 
 
