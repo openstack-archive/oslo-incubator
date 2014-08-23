ba1145128657b41416b34084eaf3d129652af873
Steve Martinelli <stevemar@ca.ibm.com>
2014-08-23 17:52:26 -0400

    Add pdb support to tox with debug helper shell script
    The Keystone team has been using a home brewed `debug_helper.sh`
    file to run tests with pdb support, it's now being also used by
    pycadf too. As noted by bnemec [1] we should move this to oslo.
    
    [1] https://bugs.launchpad.net/pycadf/+bug/1354192
    
    To run any test in particular, run tox with -e debug:
      `tox -e debug`
    It also supports passing in a specific test module, class or case.
    
    The shell file runs testtools underneath the covers to get pdb
    support.
    
    The hope is that other projects will adopt this when they sync
    oslo, and get support similar to the shell files used to create
    virtual environments.
    
    Co-Authored-By: Brant Knudson <bknudson@us.ibm.com>
    Change-Id: I5fce559d0e4522da06b3aabd9b39aa4518b1e0e7

---
+++ tools/debug_helper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# To utilize this file, add the following to tox.ini:
+#[testenv:debug]
+#commands = {toxinidir}/tools/debug_helper.sh {posargs}
+
+# To run with tox:
+#tox -e debug
+#tox -e debug test_notifications
+#tox -e debug test_notifications.NotificationsTestCase
+#tox -e debug test_notifications.NotificationsTestCase.test_send_notification
+
+TMP_DIR=`mktemp -d` || exit 1
+trap "rm -rf $TMP_DIR" EXIT
+
+ALL_TESTS=$TMP_DIR/all_tests
+TESTS_TO_RUN=$TMP_DIR/tests_to_run
+
+PACKAGENAME=$(python setup.py --name)
+
+python -m testtools.run discover -t ./ ./$PACKAGENAME/tests --list > $ALL_TESTS
+
+if [ "$1" ]; then
+    grep "$1" < $ALL_TESTS > $TESTS_TO_RUN
+else
+    mv $ALL_TESTS $TESTS_TO_RUN
+fi
