55ea9c74ea93958b447e0d540afb4264baa46bcf
Simon Pasquier <spasquier@mirantis.com>
2015-02-03 10:29:51 +0100

    Move instantiation of the Syslog handler
    Move the creation of the Syslog handler before setting up the log
    formatter and the log level. The issue has been introduced when fixing
    bug #1328501.
    
    Change-Id: I2528731d094e07bf13b8190a61ceeb024b32f607
    Closes-Bug: 1399088

--- openstack/common/log.py
+++ openstack/common/log.py
@@ -511,6 +511,20 @@ def _setup_logging_from_conf(project, version):
                 logging.ERROR)
         log_root.addHandler(handler)
 
+    if CONF.use_syslog:
+        try:
+            facility = _find_facility_from_conf()
+            # TODO(bogdando) use the format provided by RFCSysLogHandler
+            #   after existing syslog format deprecation in J
+            if CONF.use_syslog_rfc_format:
+                syslog = RFCSysLogHandler(facility=facility)
+            else:
+                syslog = logging.handlers.SysLogHandler(facility=facility)
+            log_root.addHandler(syslog)
+        except socket.error:
+            log_root.error('Unable to add syslog handler. Verify that syslog'
+                           'is running.')
+
     datefmt = CONF.log_date_format
     for handler in log_root.handlers:
         # NOTE(alaski): CONF.log_format overrides everything currently.  This
@@ -542,20 +556,6 @@ def _setup_logging_from_conf(project, version):
             logger.setLevel(level)
         else:
             logger.setLevel(level_name)
-
-    if CONF.use_syslog:
-        try:
-            facility = _find_facility_from_conf()
-            # TODO(bogdando) use the format provided by RFCSysLogHandler
-            #   after existing syslog format deprecation in J
-            if CONF.use_syslog_rfc_format:
-                syslog = RFCSysLogHandler(facility=facility)
-            else:
-                syslog = logging.handlers.SysLogHandler(facility=facility)
-            log_root.addHandler(syslog)
-        except socket.error:
-            log_root.error('Unable to add syslog handler. Verify that syslog'
-                           'is running.')
 
 
 _loggers = {}
