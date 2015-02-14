06c7336358df437361e6c5e8c69d39a5fd2fc555
Davanum Srinivas <davanum@gmail.com>
2015-02-14 09:54:31 -0500

    Reduce LOG for policy.d/ directory to debug
    We have a whole log of logstash hits:
    "Can not find policy directory: policy.d"
    
    We switch from LOG.info to LOG.debug to reduce logstash hits
    
    Closes-Bug: #1421863
    Depends-On: I3f54b1f9f5c216b9e5eca6297e120a3618535bc3
    Change-Id: I9f605129356ec6598c7bc00d1ab990c068f85fb2

--- openstack/common/policy.py
+++ openstack/common/policy.py
@@ -272,7 +272,7 @@ class Enforcer(object):
                 try:
                     path = self._get_policy_path(path)
                 except cfg.ConfigFilesNotFoundError:
-                    LOG.info(_LI("Can not find policy directory: %s"), path)
+                    LOG.debug("Can not find policy directory: %s", path)
                     continue
                 self._walk_through_policy_directory(path,
                                                     self._load_policy_file,
