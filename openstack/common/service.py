c175967beb29e9f956e49ecc51ca8f78ae75d88e
Elena Ezhova <eezhova@mirantis.com>
2015-03-17 18:13:29 +0300

    ProcessLauncher: reload config file in parent process on SIGHUP
    Currently, when a parent process receives SIGHUP it just sends
    SIGHUP to its children. While children reload their config files
    and call reset on their services, parent continues to run with
    old config.
    
    Add reloading of config file and calling reset for service
    on receiving SIGHUP.
    
    Closes-Bug: #1433142
    Change-Id: I132865fc5c3a12baf02f2991fc82701adfc7ed67

--- openstack/common/service.py
+++ openstack/common/service.py
@@ -209,6 +209,7 @@ class ProcessLauncher(object):
     def __init__(self):
         """Constructor."""
 
+        self.service = None
         self.children = {}
         self.sigcaught = None
         self.running = True
@@ -329,6 +330,7 @@ class ProcessLauncher(object):
         return pid
 
     def launch_service(self, service, workers=1):
+        self.service = service
         wrap = ServiceWrapper(service, workers)
 
         LOG.info(_LI('Starting %d workers'), wrap.workers)
@@ -393,6 +395,10 @@ class ProcessLauncher(object):
 
                 for pid in self.children:
                     os.kill(pid, signal.SIGHUP)
+
+                cfg.CONF.reload_config_files()
+                self.service.reset()
+
                 self.running = True
                 self.sigcaught = None
         except eventlet.greenlet.GreenletExit:
