f5646edc61b9653d7ff71ed0177ed77811bbdcd0
Elena Ezhova <eezhova@mirantis.com>
2015-04-07 17:46:16 +0300

    Revert "Revert "Optimization of waiting subprocesses in ProcessLauncher""
    This reverts commit d24b65890bca9568781d05c458720358a8edc07e.
    
    The sync of service.py code to neutron has merged and the work on
    fixing bug 1438321 is in progress. So we can add these changes back.
    
    Change-Id: Ib6a39d33657e63a4d2d8ef07c03ed0d00fa9b88a

--- openstack/common/service.py
+++ openstack/common/service.py
@@ -206,16 +206,12 @@ class ProcessLauncher(object):
         for handler in cls._signal_handlers_set:
             handler(*args, **kwargs)
 
-    def __init__(self, wait_interval=0.01):
-        """Constructor.
+    def __init__(self):
+        """Constructor."""
 
-        :param wait_interval: The interval to sleep for between checks
-                              of child process exit.
-        """
         self.children = {}
         self.sigcaught = None
         self.running = True
-        self.wait_interval = wait_interval
         rfd, self.writepipe = os.pipe()
         self.readpipe = eventlet.greenio.GreenPipe(rfd, 'r')
         self.handle_signal()
@@ -341,8 +337,8 @@ class ProcessLauncher(object):
 
     def _wait_child(self):
         try:
-            # Don't block if no child processes have exited
-            pid, status = os.waitpid(0, os.WNOHANG)
+            # Block while any of child processes have exited
+            pid, status = os.waitpid(0, 0)
             if not pid:
                 return None
         except OSError as exc:
@@ -371,10 +367,6 @@ class ProcessLauncher(object):
         while self.running:
             wrap = self._wait_child()
             if not wrap:
-                # Yield to other threads if no children have exited
-                # Sleep for a short time to avoid excessive CPU usage
-                # (see bug #1095346)
-                eventlet.greenthread.sleep(self.wait_interval)
                 continue
             while self.running and len(wrap.children) < wrap.workers:
                 self._start_child(wrap)
