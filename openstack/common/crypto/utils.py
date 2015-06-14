a3e55210e86abba167fbdd9d6755476e8a4ea0a1
Davanum Srinivas <davanum@gmail.com>
2015-06-14 13:57:34 -0400

    Just use os.urandom instead of Crypto.Random
    Crypto.Random needs someone to keep track of os.fork() and
    call Crypto.Random.atfork() in both the parent and the child
    Since this is too onerous a burden on the loosely coupled
    oslo code and the consuming project. It's just simpler to switch
    to os.urandom() to get the bytes we need.
    
    Closes-Bug: #1464037
    Change-Id: I09d7d2708dafdb3ac2514844e74445d93037412a

--- openstack/common/crypto/utils.py
+++ openstack/common/crypto/utils.py
@@ -26,9 +26,9 @@
 ########################################################################
 
 import base64
+import os
 
 from Crypto.Hash import HMAC
-from Crypto import Random
 from oslo_utils import importutils
 import six
 
@@ -125,7 +125,7 @@ class SymmetricCrypto(object):
         self.hashfn = importutils.import_module('Crypto.Hash.' + hashtype)
 
     def new_key(self, size):
-        return Random.new().read(size)
+        return os.urandom(size)
 
     def encrypt(self, key, msg, b64encode=True):
         """Encrypt the provided msg and returns the cyphertext optionally
@@ -142,7 +142,7 @@ class SymmetricCrypto(object):
 
         :returns enc: a block of encrypted data.
         """
-        iv = Random.new().read(self.cipher.block_size)
+        iv = os.urandom(self.cipher.block_size)
         cipher = self.cipher.new(key, self.cipher.MODE_CBC, iv)
 
         # CBC mode requires a fixed block size. Append padding and length of
