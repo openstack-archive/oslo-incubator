d5d6b4c79fee00c793744ac29716ea57337d3361
Doug Hellmann <doug@doughellmann.com>
2015-07-10 13:12:08 +0000

    Fix mock of open() for mock 1.1.0
    Mock out open() properly.
    
    Change-Id: I464de26fdbd53dd7138f8cb4ae362cd47e99609c

--- tests/unit/reports/test_views.py
+++ tests/unit/reports/test_views.py
@@ -393,7 +393,7 @@ class TestJinjaView(base.BaseTestCase):
         super(TestJinjaView, self).setUp()
         self.model = base_model.ReportModel(data={'int': 1, 'string': 'value'})
 
-    @mock.patch('six.moves.builtins.open', new=MM_OPEN)
+    @mock.mock_open(MM_OPEN)
     def test_load_from_file(self):
         self.model.attached_view = jv.JinjaView(path='a/b/c/d.jinja.txt')
 
